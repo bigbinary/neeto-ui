3a2ba38e09b35f76a1f264e8fefd064faea46a28
Vinay Chandran <vinaychandranvs@gmail.com>
2020-10-03 16:31:43 +0530

    - Added Tooltip, Label, Input components - Added Remixicons

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Button } from '../lib';
+import { Button, Input } from "../lib";
 
 const App = () => {
   return (
-    <div>
-      <Button label="Testing" />
+    <div className="grid grid-flow-row gap-8 p-6">
+      <div>
+        <Button label="Testing" />
+      </div>
+      <div className="w-1/5">
+        <Input placeholder="Sample field" helpText="I'm a sample field!" label="Testing" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
