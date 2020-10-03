ecb92fcfdd3bf2f7bd208da54e548433781cfaa6
Vinay Chandran <vinaychandranvs@gmail.com>
2020-10-03 11:08:32 +0530

    - Remove CRA - Build from scratch

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Test</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
