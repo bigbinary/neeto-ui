4f03c860ce1f6a9ffd99cde43743cd7030f1b6ed
Vinay Chandran <vinaychandranvs@gmail.com>
2020-10-09 22:37:12 +0530

    Add toastr

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Button, Input } from "../lib";
+import { Button, Input, showToastr, showErrorToastr, ToastContainer } from "../lib";
 
 const App = () => {
   return (
+    <>
+      <ToastContainer />
     <div className="grid grid-flow-row gap-8 p-6">
       <div>
-        <Button label="Testing" />
+        <Button label="Show toastr" onClick={() => {
+          showToastr("I'm a happy toastr."); 
+        }} />
       </div>
       <div className="w-1/5">
         <Input placeholder="Sample field" helpText="I'm a sample field!" label="Testing" />
       </div>
     </div>
+    </>
   );
 };
 
