b1c22621b88e41fe471ce2ea772926865e49ccbc
Edwin Babu <edwinbbu@gmail.com>
2020-10-26 16:22:33 +0530

    Added DateInput and DateRangeInput

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Avatar, Button, Input } from "../lib";
+import { Avatar, Button, Input, DateInput } from "../lib";
+import moment from "moment";
+import { Position } from "@blueprintjs/core";
 
 const App = () => {
   return (
@@ -14,6 +16,9 @@ const App = () => {
       </div>
       <div className="w-1/5">
         <Input placeholder="Sample field" helpText="I'm a sample field!" label="Testing" />
+        </div>
+        <div className="w-1/5">
+          <DateInput value={moment().toDate()} label="DateInput" popoverProps={{ position: Position.BOTTOM}}/>
       </div>
     </div>
     </>
