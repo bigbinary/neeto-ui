cae77f06d8e72d3ff36e8ca942fc1189cebead12
Vinay Chandran <vinaychandranvs@gmail.com>
2020-11-22 19:31:32 +0530

    Add IconPicker

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Avatar, Button, Input, DateInput } from "../lib";
+import { IconPicker } from "../lib";
 import moment from "moment";
 import { Position } from "@blueprintjs/core";
 
 const App = () => {
+  const [icon, seticon] = useState(null)
   return (
     <>
-    <div className="grid grid-flow-row gap-8 p-6">
-      <div>
-      <Avatar size={32} />
-        <Button label="Show toastr" onClick={() => {
-          showToastr("I'm a happy toastr."); 
-        }} />
-      </div>
-      <div className="w-1/5">
-        <Input placeholder="Sample field" helpText="I'm a sample field!" label="Testing" />
-        </div>
-        <div className="w-1/5">
-          <DateInput value={moment().toDate()} label="DateInput" popoverProps={{ position: Position.BOTTOM}}/>
-        </div>
-    </div>
+    Hello
+    <IconPicker value={icon} onChange={seticon}/>
     </>
   );
 };
