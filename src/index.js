c9e63474a830db585e6f7f8ee8beeb044065864c
Yedhin Kizhakkethara <yedhin1998@gmail.com>
2020-11-30 22:07:57 +0530

    Sync badge (#57)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { IconPicker } from "../lib";
-import moment from "moment";
-import { Position } from "@blueprintjs/core";
 
 const App = () => {
-  const [icon, seticon] = useState(null)
+  const [icon, setIcon] = useState(null)
   return (
     <>
-    Hello
-    <IconPicker value={icon} onChange={seticon}/>
+      Hello
+      <IconPicker value={icon} onChange={setIcon} />
     </>
   );
 };
