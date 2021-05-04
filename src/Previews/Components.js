6f1a2c6b0ec8c305ff61f67f171653b616849550
Edwin Babu <edwinbbu@gmail.com>
2021-05-04 13:15:42 +0530

    Fixed view more in card component

--- src/Previews/Components.js
+++ src/Previews/Components.js
@@ -11,20 +11,22 @@ import {
   ColorPicker,
   Accordion,
 } from "../../lib";
-import Header from '../Header';
+import Header from "../Header";
 
+const cardItems = ["Card Item 1", "Card Item 2", "Card Item 3"];
 const Components = () => {
-
   const [icon, setIcon] = useState(null);
   const [pageNo, setPageNo] = useState(1);
   const PAGE_SIZE = 5;
 
   return (
     <div className="w-full">
-      <Header title="Components"/>
+      <Header title="Components" />
       <div className="flex flex-col items-start justify-start p-6">
         <div className="mb-12">
-          <h4 className="mb-3 text-sm font-medium text-gray-800">Icon Picker</h4>
+          <h4 className="mb-3 text-sm font-medium text-gray-800">
+            Icon Picker
+          </h4>
           <IconPicker value={icon} onChange={setIcon} />
         </div>
         <div className="w-full mb-12">
@@ -35,14 +37,21 @@ const Components = () => {
             <p className="mb-4">Card Item 2</p>
             <p>Card Item 3</p>
           </Card>
+
+          {/* <Card rows={1} className="w-1/3">
+            <Card.Title>My Card Title 2.0</Card.Title>
+            {cardItems.map((item) => (
+              <p className="mb-4">{item}</p>
+            ))}
+          </Card> */}
         </div>
         <div className="mb-12">
           <h4 className="mb-3 text-sm font-medium text-gray-800">Callout</h4>
           <Callout icon={false} style="info">
             <span className="leading-relaxed">
               Press
-              <span className="font-semibold"> Shift + ?</span> on your keyboard at
-              any point.
+              <span className="font-semibold"> Shift + ?</span> on your keyboard
+              at any point.
             </span>
           </Callout>
         </div>
@@ -97,7 +106,9 @@ const Components = () => {
           </Tab>
         </div>
         <div className="mb-12">
-          <h4 className="mb-3 text-sm font-medium text-gray-800">Color Picker</h4>
+          <h4 className="mb-3 text-sm font-medium text-gray-800">
+            Color Picker
+          </h4>
           <ColorPicker
             onChange={(value) => {}}
             color="#fefefe"
@@ -112,17 +123,31 @@ const Components = () => {
           <div className="w-96">
             <Accordion className="mt-2" defaultActiveKey={0}>
               <Accordion.Item title="Opened Heading">
-                <p className="leading-relaxed text-gray-600">We got a right to pick a little fight, Bonanza! If anyone fights anyone of us, he's gotta fight with me! We're not a one to saddle up and run, Bonanza! Anyone of us who starts a little fuss knows he can count on me! One for four, four for one, this we guarantee. We got a right to pick a little fight, Bonanza! If anyone fights anyone of us he's gotta fight with me!</p>
+                <p className="leading-relaxed text-gray-600">
+                  We got a right to pick a little fight, Bonanza! If anyone
+                  fights anyone of us, he's gotta fight with me! We're not a one
+                  to saddle up and run, Bonanza! Anyone of us who starts a
+                  little fuss knows he can count on me! One for four, four for
+                  one, this we guarantee. We got a right to pick a little fight,
+                  Bonanza! If anyone fights anyone of us he's gotta fight with
+                  me!
+                </p>
               </Accordion.Item>
               <Accordion.Item title="Closed Heading">
-                <p className="leading-relaxed text-gray-600">It seems today that all you see is violence in movies and sex on tv, but where are those good old-fashioned values on which we used to rely? Lucky there's a family guy, lucky there's a man who positively can do all the things that make us laugh and cry - he's our family guy!</p>
+                <p className="leading-relaxed text-gray-600">
+                  It seems today that all you see is violence in movies and sex
+                  on tv, but where are those good old-fashioned values on which
+                  we used to rely? Lucky there's a family guy, lucky there's a
+                  man who positively can do all the things that make us laugh
+                  and cry - he's our family guy!
+                </p>
               </Accordion.Item>
             </Accordion>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Components;
