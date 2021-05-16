044264b1f2746870102bcf4b40f42274a9afcf5c
Goutham Subramanyam <gsn.goutham@gmail.com>
2021-05-16 05:52:09 +0530

    Added Badge colors to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,26 @@ module.exports = {
       "./lib/*.js",
     ],
     defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    options: {
+      safelist: [
+        'bg-gray-100',
+        'bg-red-100',
+        'bg-yellow-100',
+        'bg-green-100',
+        'bg-blue-100',
+        'bg-indigo-100',
+        'bg-purple-100',
+        'bg-pink-100',
+        'text-gray-800',
+        'text-red-800',
+        'text-yellow-800',
+        'text-green-800',
+        'text-blue-800',
+        'text-indigo-800',
+        'text-purple-800',
+        'text-pink-800',
+      ]
+    }
   },
   theme: {
     extend: {
