a79941754eb57cfd78e00a53fbb22b72fb09e2b9
Goutham Subramanyam N <gsn.goutham@gmail.com>
2021-05-18 17:02:35 +0530

    Updated ColorPaletter to receive colorList as props (#269)

--- src/Previews/Components.js
+++ src/Previews/Components.js
@@ -14,6 +14,17 @@ import {
 import Header from "../Header";
 
 const cardItems = ["Card Item 1", "Card Item 2", "Card Item 3"];
+
+const COLOR_PALETTER_LIST = [
+  { from: "purple-500", to: "purple-600" },
+  { from: "pink-500", to: "pink-600" },
+  { from: "green-500", to: "green-600" },
+  { from: "red-500", to: "red-600" },
+  { from: "blue-500", to: "blue-600" },
+  { from: "indigo-500", to: "indigo-600" },
+  { from: "yellow-500", to: "yellow-600" }
+];
+
 const Components = () => {
   const [icon, setIcon] = useState(null);
   const [pageNo, setPageNo] = useState(1);
@@ -110,10 +121,11 @@ const Components = () => {
             Color Picker
           </h4>
           <ColorPicker
-            onChange={(value) => {}}
+            onChange={() => {}}
             color="#fefefe"
             colorPaletteProps={{
               color: "#fefefe",
+              colorList: COLOR_PALETTER_LIST,
               onChange: () => {},
             }}
           />
