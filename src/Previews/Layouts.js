31e1ba30c6c22d1bfe19e1a0212e1a621f2d92de
Edwin Babu <edwinbbu@gmail.com>
2021-05-20 10:55:02 +0530

    Fixed margin in subheader component

--- src/Previews/Layouts.js
+++ src/Previews/Layouts.js
@@ -6,13 +6,28 @@ import {
   Container,
   Scrollable,
   MenuBar,
-  FilterBar
+  FilterBar,
 } from "../../lib/layouts";
 
+const noop = () => {};
 const LABELS = ["Misc", "Random", "Urgent"];
+const COLUMNFILTERS = {
+  email: { show: false, label: "Email" },
+  display_role: { show: true, label: "Role" },
+  groups: { show: false, label: "Groups" },
+  active_assigned_tickets_count: {
+    show: true,
+    label: "Assigned Tickets",
+  },
+  available_for_desk: {
+    show: true,
+    label: "Availability for Desk",
+  },
+};
 
 const Layouts = () => {
   const [currentLabel, setCurrentLabel] = useState(null);
+  const [searchString, setSearchString] = useState("");
   return (
     <MainLayout>
       {(showMenu, toggleMenu, showFilter, toggleFilter) => (
@@ -30,23 +45,36 @@ const Layouts = () => {
                 />
               ))}
             </div>
-        
           </MenuBar>
           <Container>
             <Header title="Layouts" toggleMenu={toggleMenu} />
-            <SubHeader toggleFilter={toggleFilter}/>
+            <SubHeader
+              toggleFilter={toggleFilter}
+              searchProps={{
+                value: searchString,
+                onChange: (e) => setSearchString(e.target.value),
+                clear: () => setSearchString(""),
+              }}
+              sortProps={{
+                options: [],
+                onClick: noop,
+              }}
+              columnFilterProps={{
+                columnFilters: COLUMNFILTERS,
+                handleColumnFiltering: noop,
+              }}
+            />
             <div className="flex flex-row items-start justify-start w-full">
               <Scrollable className="w-full p-4">
-                <div className="flex-grow h-full bg-gray-100 rounded">
-                </div>
+                <div className="flex-grow h-full bg-gray-100 rounded"></div>
               </Scrollable>
-              <FilterBar showFilter={showFilter}/>
+              <FilterBar showFilter={showFilter} />
             </div>
           </Container>
         </>
       )}
     </MainLayout>
-  )
-}
+  );
+};
 
 export default Layouts;
