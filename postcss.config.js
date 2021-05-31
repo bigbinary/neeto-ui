c8edaaf1a9dfcf8db528fb8d1c63818185da081f
Edwin Babu <edwinbbu@gmail.com>
2021-05-31 10:46:24 +0530

    Updated postcss config to support Safari browser.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-preset-env': {},
-    'tailwindcss': {},
-    'cssnano': {}
-  }
-}
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss")("./tailwind.config.js"),
+    require("postcss-preset-env")({
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 3,
+    }),
+    require("cssnano"),
+  ],
+};
