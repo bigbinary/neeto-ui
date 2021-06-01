ef6e81be459fa502eab1ce12636e90943276748a
Goutham Subramanyam <gsn.goutham@gmail.com>
2021-06-01 13:39:37 +0530

    Fixed Form elements border issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("tailwindcss")("./tailwind.config.js"),
+    require("tailwindcss"),
     require("postcss-preset-env")({
       autoprefixer: {
         flexbox: "no-2009",
