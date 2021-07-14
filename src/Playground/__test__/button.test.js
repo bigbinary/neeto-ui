6e4a1c8abe664829548191ee581d992a80275a21
Edwin Babu <edwinbbu@gmail.com>
2021-07-14 19:06:47 +0530

    Buttons expected to be in the document test added

--- src/Playground/__test__/button.test.js
+++ src/Playground/__test__/button.test.js
@@ -1,12 +1,32 @@
 import * as React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import App from "./Playground/index";
+import App from "../index";
 
 test("test buttons in playground", () => {
   render(<App />);
-  const buttonElement = screen.getByText(/Primary Button/i);
-  expect(buttonElement).toBeInTheDocument();
+
+  //Buttons
+  const primaryButton = screen.getByText("Primary Button");
+  const secondaryButton = screen.getByText("Secondary Button");
+  const warningButton = screen.getByText("Warning Button");
+  const dangerButton = screen.getByText("Danger Button");
+  const textButton = screen.getByText("Text Button");
+  const linkButton = screen.getByText("Link Button");
+
+  expect(primaryButton).toBeInTheDocument();
+  expect(secondaryButton).toBeInTheDocument();
+  expect(warningButton).toBeInTheDocument();
+  expect(dangerButton).toBeInTheDocument();
+  expect(textButton).toBeInTheDocument();
+  expect(linkButton).toBeInTheDocument();
+
+  //Dropdown
+  const dropdown1 = screen.getByText("Dropdown with Action Button");
+  const dropdown2 = screen.getByText("Action Dropdown");
+
+  expect(dropdown1).toBeInTheDocument();
+  expect(dropdown2).toBeInTheDocument();
 });
 
 // test("create a entry for cash-in", async () => {
