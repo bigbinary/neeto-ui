57652731b4f0ca07ecbe963d9f9bb0a160e52a62
Goutham Subramanyam N <gsn.goutham@gmail.com>
2021-07-29 10:35:14 +0530

    Added Textarea component to v2 (#377)
    * Input component v2 - Improvements
    
    * Added Textarea
    
    * Fixed sidebar warning
    
    * Error input and textarea component example added
    
    Co-authored-by: Edwin Babu <edwinbbu@gmail.com>

--- example/v2/InputFields.js
+++ example/v2/InputFields.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
-import { Input } from "../../lib/v2";
+import { Input, Textarea } from "../../lib/v2";
 import Header from "./Header";
 import { Favorite } from "@bigbinary/neeto-icons";
 
-const Buttons = () => {
+const InputField = () => {
   const [loading, setLoading] = useState(false);
   const [input, setInput] = useState("Text");
-  const toggle = () => {
-    setLoading(!loading);
-  };
+
   return (
     <div className="w-full">
       <Header title="Input Fields" />
@@ -16,117 +14,147 @@ const Buttons = () => {
         <div className="p-4 space-y-8 border border-indigo-500 border-dashed">
           <h2 className="text-xl">Input/Small/Default</h2>
           <div className="flex flex-row items-center justify-center space-x-8">
-            <Input placeHolder="Input Placeholder" />
+            <Input placeholder="Input Placeholder" />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               prefix={<Favorite size={16} />}
             />
-            <Input placeHolder="Input Placeholder" />
-            <Input placeHolder="Input Placeholder" />
+            <Input placeholder="Input Placeholder" />
+            <Input placeholder="Input Placeholder" />
           </div>
           <div className="flex flex-row items-center justify-center space-x-8">
-            <Input placeHolder="Input Placeholder" />
+            <Input placeholder="Input Placeholder" />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               prefix={<Favorite size={16} />}
             />
-            <Input placeHolder="Input Placeholder" />
-            <Input placeHolder="Input Placeholder" />
+            <Input placeholder="Input Placeholder" />
+            <Input placeholder="Input Placeholder" />
           </div>
           <div className="flex flex-row items-center justify-center space-x-8">
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               onChange={(e) => setInput(e.target.value)}
               value={input}
               autoFocus
             />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               onChange={(e) => setInput(e.target.value)}
               prefix={<Favorite size={16} />}
               value={input}
             />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               onChange={(e) => setInput(e.target.value)}
               value={input}
             />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               onChange={(e) => setInput(e.target.value)}
               value={input}
             />
           </div>
           <div className="flex flex-row items-center justify-center space-x-8">
-            <Input placeHolder="Input Placeholder" disabled />
+            <Input placeholder="Input Placeholder" disabled />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               prefix={<Favorite size={16} />}
               disabled
             />
-            <Input placeHolder="Input Placeholder" disabled />
-            <Input placeHolder="Input Placeholder" disabled />
+            <Input placeholder="Input Placeholder" disabled />
+            <Input placeholder="Input Placeholder" disabled />
+          </div>
+          <div className="flex flex-row items-center justify-center space-x-8">
+            <Input placeholder="Input Placeholder" error={true} />
+            <Input
+              placeholder="Input Placeholder"
+              prefix={<Favorite size={16} />}
+              error={true}
+            />
+            <Input placeholder="Input Placeholder" error={true} />
+            <Input placeholder="Input Placeholder" error={true} />
           </div>
         </div>
         <div className="p-4 space-y-8 border border-indigo-500 border-dashed">
           <h2 className="text-xl">Input/Large</h2>
           <div className="flex flex-row items-center justify-center space-x-8">
-            <Input placeHolder="Input Placeholder" size="large" />
+            <Input placeholder="Input Placeholder" size="large" />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               size="large"
               prefix={<Favorite size={16} />}
             />
-            <Input placeHolder="Input Placeholder" size="large" />
-            <Input placeHolder="Input Placeholder" size="large" />
+            <Input placeholder="Input Placeholder" size="large" />
+            <Input placeholder="Input Placeholder" size="large" />
           </div>
           <div className="flex flex-row items-center justify-center space-x-8">
-            <Input placeHolder="Input Placeholder" size="large" />
+            <Input placeholder="Input Placeholder" size="large" />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               size="large"
               prefix={<Favorite size={16} />}
             />
-            <Input placeHolder="Input Placeholder" size="large" />
-            <Input placeHolder="Input Placeholder" size="large" />
+            <Input placeholder="Input Placeholder" size="large" />
+            <Input placeholder="Input Placeholder" size="large" />
           </div>
           <div className="flex flex-row items-center justify-center space-x-8">
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               onChange={(e) => setInput(e.target.value)}
               value={input}
               size="large"
             />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               onChange={(e) => setInput(e.target.value)}
               size="large"
               value={input}
               prefix={<Favorite size={16} />}
             />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               onChange={(e) => setInput(e.target.value)}
               size="large"
               value={input}
             />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               onChange={(e) => setInput(e.target.value)}
               size="large"
               value={input}
             />
           </div>
           <div className="flex flex-row items-center justify-center space-x-8">
-            <Input placeHolder="Input Placeholder" size="large" disabled />
+            <Input placeholder="Input Placeholder" size="large" disabled />
             <Input
-              placeHolder="Input Placeholder"
+              placeholder="Input Placeholder"
               size="large"
               prefix={<Favorite size={16} />}
               disabled
             />
-            <Input placeHolder="Input Placeholder" size="large" disabled />
-            <Input placeHolder="Input Placeholder" size="large" disabled />
+            <Input placeholder="Input Placeholder" size="large" disabled />
+            <Input placeholder="Input Placeholder" size="large" disabled />
+          </div>
+          <div className="flex flex-row items-center justify-center space-x-8">
+            <Input placeholder="Input Placeholder" size="large" error={true} />
+            <Input
+              placeholder="Input Placeholder"
+              size="large"
+              prefix={<Favorite size={16} />}
+              error={true}
+            />
+            <Input placeholder="Input Placeholder" size="large" error={true} />
+            <Input placeholder="Input Placeholder" size="large" error={true} />
+          </div>
+        </div>
+        <div className="p-4 space-y-8 border border-indigo-500 border-dashed">
+          <h2 className="text-xl">TextArea</h2>
+          <div className="flex flex-row items-center justify-center space-x-8">
+            <Textarea />
+            <Textarea placeholder="Enter Text" />
+            <Textarea placeholder="Enter Text" disabled />
+            <Textarea placeholder="Enter Text" error={true} />
           </div>
         </div>
       </div>
@@ -134,4 +162,4 @@ const Buttons = () => {
   );
 };
 
-export default Buttons;
+export default InputField;
