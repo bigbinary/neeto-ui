0d4fdcfc3fabaa113ce8ae5e6059fbac44a53ff3
Goutham Subramanyam <gsn.goutham@gmail.com>
2021-07-29 16:03:51 +0530

    UI cleanup

--- example/v2/Pages/Dropdown.js
+++ example/v2/Pages/Dropdown.js
@@ -4,76 +4,59 @@ import Header from "../Header";
 
 const Dropdowns = () => {
   const [loading, setLoading] = useState(false);
-  const toggle = () => {
-    setLoading(!loading);
-  };
   const listItems = ["Option 1", "Option 2", "Option 3"];
   return (
     <div className="w-full">
       <Header title="Dropdowns" />
-      <div className="flex flex-row flex-wrap items-center justify-between h-32 p-4">
-        <Dropdown
-          label="Primary Dropdown"
-          buttonStyle="primary"
-          className="ml-3"
-          position="bottom-end"
-          closeOnSelect
-        >
-          {listItems.map((item, idx) => (
-            <li key={idx}>{item}</li>
-          ))}
-        </Dropdown>
-        <Dropdown
-          label="Secondary Dropdown"
-          buttonStyle="secondary"
-          className="ml-3"
-          position="bottom-end"
-          closeOnSelect
-        >
-          {listItems.map((item, idx) => (
-            <li key={idx}>{item}</li>
-          ))}
-        </Dropdown>
-        <Dropdown
-          label="Text Dropdown"
-          buttonStyle="text"
-          className="ml-3"
-          position="bottom-end"
-          closeOnSelect
-        >
-          {listItems.map((item, idx) => (
-            <li key={idx}>{item}</li>
-          ))}
-        </Dropdown>
-        <Dropdown
-          label="Dropdown with custom icon"
-          buttonStyle="primary"
-          className="ml-3"
-          position="bottom-end"
-          icon="ri-send-plane-line"
-          closeOnSelect
-        >
-          {listItems.map((item, idx) => (
-            <li key={idx}>{item}</li>
-          ))}
-        </Dropdown>
-        <Dropdown
-          label="Dropdown with Action Button"
-          actionButtonProps={{
-            label: "Add New Item",
-            style: "seconary",
-            icon: "ri-add-line",
-            onClick: () => alert("You clicked on action button in dropdown"),
-          }}
-          buttonStyle="secondary"
-          className="ml-3"
-          position="bottom-end"
-          closeOnSelect
-        >
-          {listItems.map((item, idx) => (
-            <li key={idx}>{item}</li>
-          ))}
-        </Dropdown>
+      <div className="p-6 space-y-6">
+        <div className="flex flex-row flex-wrap items-center justify-start p-4 space-x-6 border border-indigo-500 border-dashed">
+          <Dropdown
+            label="Primary Dropdown"
+            buttonStyle="primary"
+          >
+            {listItems.map((item, idx) => (
+              <li key={idx}>{item}</li>
+            ))}
+          </Dropdown>
+          <Dropdown
+            label="Secondary Dropdown"
+            buttonStyle="secondary"
+          >
+            {listItems.map((item, idx) => (
+              <li key={idx}>{item}</li>
+            ))}
+          </Dropdown>
+          <Dropdown
+            label="Text Dropdown"
+            buttonStyle="text"
+          >
+            {listItems.map((item, idx) => (
+              <li key={idx}>{item}</li>
+            ))}
+          </Dropdown>
+          <Dropdown
+            label="Dropdown with custom icon"
+            icon="ri-send-plane-line"
+          >
+            {listItems.map((item, idx) => (
+              <li key={idx}>{item}</li>
+            ))}
+          </Dropdown>
+          <Dropdown
+            label="Dropdown with Action Button"
+            actionButtonProps={{
+              label: "Add New Item",
+              style: "seconary",
+              icon: "ri-add-line",
+              onClick: () => alert("You clicked on action button in dropdown"),
+            }}
+            buttonStyle="secondary"
+          >
+            {listItems.map((item, idx) => (
+              <li key={idx}>{item}</li>
+            ))}
+          </Dropdown>
+        </div>
       </div>
     </div>
   );
