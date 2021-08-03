ed9effa93bcc3b1e2707dc85ceef2f22ce7a21e7
Edwin Babu <edwinbbu@gmail.com>
2021-08-04 00:34:32 +0530

    Tailwind v2 style fix

--- tailwind-v2.config.js
+++ tailwind-v2.config.js
@@ -1,24 +1,3 @@
-const safelist = [
-  "bg-gray-100",
-  "bg-red-100",
-  "bg-yellow-100",
-  "bg-green-100",
-  "bg-blue-100",
-  "bg-indigo-100",
-  "bg-purple-100",
-  "bg-pink-100",
-  "bg-orange-100",
-  "text-gray-800",
-  "text-red-800",
-  "text-yellow-800",
-  "text-green-800",
-  "text-blue-800",
-  "text-indigo-800",
-  "text-purple-800",
-  "text-pink-800",
-  "text-orange-800",
-];
-
 module.exports = {
   prefix: "tw-",
   purge: {
@@ -29,11 +8,9 @@ module.exports = {
       "./lib/**/**/**/*.js",
       "./lib/*.js",
       "./example/**/*.js",
+      "./example/**/**/*.js",
     ],
     defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    options: {
-      safelist,
-    },
   },
   theme: {
     extend: {
