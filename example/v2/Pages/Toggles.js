f1b4628d006cbf849ca3b62d412c1012ca9a3807
Goutham Subramanyam N <gsn.goutham@gmail.com>
2021-08-06 09:26:41 +0530

    Fixed Toggles UI Issues (#405)
    * Updated Tab style
    
    * Checkbox and Radio Improvements
    
    * Switch Improvements
    
    * Fixed Switch focus issue

--- example/v2/Pages/Toggles.js
+++ example/v2/Pages/Toggles.js
@@ -3,7 +3,7 @@ import { Switch, Radio, Checkbox } from "../../../lib/v2";
 import Header from "../Header";
 
 const Misc = () => {
-  const [checked, setChecked] = useState(false);
+  const [data, setData] = useState({});
   return (
     <div className="w-full">
       <Header title="Toggles" />
@@ -19,21 +19,28 @@ const Misc = () => {
             </div>
             <div className="flex space-x-6">
               <Checkbox
-                checked={checked}
-                onChange={() => setChecked(!checked)}
+                name="1"
+                checked={data["1"]}
+                onChange={(e) => setData({ ...data, "1" : e.target.checked })}
               />
               <Checkbox
+                name="2"
+                checked={data["2"]}
+                onChange={(e) => setData({ ...data, "2" : e.target.checked })}
                 disabled
-                checked={checked}
-                onChange={() => setChecked(!checked)}
               />
             </div>
             <div className="flex space-x-6">
-              <Switch checked={checked} onChange={() => setChecked(!checked)} />
               <Switch
+                name="3"
+                checked={data["3"]}
+                onChange={(e) => setData({ ...data, "3" : e.target.checked })}
+              />
+              <Switch
+                name="4"
                 disabled
-                checked={checked}
-                onChange={() => setChecked(!checked)}
+                checked={data["4"]}
+                onChange={(e) => setData({ ...data, "4" : e.target.checked })}
               />
             </div>
           </div>
@@ -45,43 +52,69 @@ const Misc = () => {
               </Radio>
             </div>
             <div className="flex space-x-6">
-              <Checkbox checked onChange={() => {}} />
-              <Checkbox checked disabled onChange={() => {}} />
+              <Checkbox
+                name="5"
+                checked={data["5"]}
+                onChange={(e) => setData({ ...data, "5" : e.target.checked })}
+                />
+              <Checkbox
+                name="6"
+                checked={data["6"]}
+                onChange={(e) => setData({ ...data, "6" : e.target.checked })}
+                disabled
+              />
             </div>
             <div className="flex space-x-6">
-              <Switch checked onChange={() => {}} />
-              <Switch checked disabled onChange={() => {}} />
+              <Switch
+                name="7"
+                checked={data["7"]}
+                onChange={(e) => setData({ ...data, "7" : e.target.checked })}
+                />
+              <Switch
+                name="8"
+                checked={data["8"]}
+                onChange={(e) => setData({ ...data, "8" : e.target.checked })}
+                disabled
+              />
             </div>
           </div>
-          <div className="flex flex-col items-start justify-start space-x-6">
+          <div className="flex flex-col items-start justify-start space-y-6">
             <h2 className="text-base">Checkbox with Label</h2>
-            <Checkbox checked onChange={() => {}} label="Checkbox" />
             <Checkbox
-              checked
+              name="9"
+              label="Checkbox"
+              checked={data["9"]}
+              onChange={(e) => setData({ ...data, "9" : e.target.checked })}
+              />
+            <Checkbox
+              name="10"
+              label="Checkbox Disabled"
+              checked={data["10"]}
+              onChange={(e) => setData({ ...data, "10" : e.target.checked })}
               disabled
-              onChange={() => {}}
-              label="Checkbox disabled"
             />
           </div>
-          <div className="flex flex-col items-start justify-start space-x-6">
+          <div className="flex flex-col items-start justify-start space-y-6">
             <h2 className="text-base">Radio with Label</h2>
             <Radio>
               <Radio.Item name="radio" label="Radio" />
               <Radio.Item name="radio" disabled label="Radio disabled" />
             </Radio>
           </div>
-          <div className="flex flex-col items-start justify-start space-x-6">
+          <div className="flex flex-col items-start justify-start space-y-6">
             <h2 className="text-base">Switch with Label</h2>
             <Switch
-              checked={checked}
-              onChange={() => setChecked(!checked)}
+              name="11"
               label="Switch"
-            />
+              checked={data["11"]}
+              onChange={(e) => setData({ ...data, "11" : e.target.checked })}
+              />
             <Switch
-              disabled
-              checked={checked}
-              onChange={() => setChecked(!checked)}
+              name="12"
               label="Switch Disabled"
+              checked={data["12"]}
+              onChange={(e) => setData({ ...data, "12" : e.target.checked })}
+              disabled
             />
           </div>
         </div>
