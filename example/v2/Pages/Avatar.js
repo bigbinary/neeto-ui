271f19899b03de8b3a343c4e1447aa62c623882c
Akkshay Lawrence <akkshay.lawrence@bigbinary.com>
2021-08-10 18:51:04 +0530

    Minor refactoring in CSS

--- example/v2/Pages/Avatar.js
+++ example/v2/Pages/Avatar.js
@@ -15,16 +15,16 @@ function Avatars() {
       <div className="p-6">
         <div className="flex p-4 space-x-4 border border-indigo-500 border-dashed">
           <div className="flex flex-col space-y-4 p-2">
-            <Avatar onClick={onClick} fallbackText="AB" size="small" imageUrl="" />
-            <Avatar onClick={onClick} fallbackText="CD" size="medium" imageUrl="" />
-            <Avatar onClick={onClick} fallbackText="EF" size="large" imageUrl="" />
-            <Avatar onClick={onClick} fallbackText="GH" size="xlarge" imageUrl="" />
+            <Avatar onClick={onClick} fallbackText="AB" size="small" />
+            <Avatar onClick={onClick} fallbackText="CD" size="medium" />
+            <Avatar onClick={onClick} fallbackText="EF" size="large" />
+            <Avatar onClick={onClick} fallbackText="GH" size="xlarge" />
           </div>
           <div className="flex flex-col space-y-4 p-2">
-            <Avatar onClick={onClick} fallbackText="IJ" size="small" imageUrl="" isRounded />
-            <Avatar onClick={onClick} fallbackText="KL" size="medium" imageUrl="" isRounded />
-            <Avatar onClick={onClick} fallbackText="MN" size="large" imageUrl="" isRounded />
-            <Avatar onClick={onClick} fallbackText="OP" size="xlarge" imageUrl="" isRounded />
+            <Avatar onClick={onClick} fallbackText="IJ" size="small" isRounded />
+            <Avatar onClick={onClick} fallbackText="KL" size="medium" isRounded />
+            <Avatar onClick={onClick} fallbackText="MN" size="large" isRounded />
+            <Avatar onClick={onClick} fallbackText="OP" size="xlarge" isRounded />
           </div>
           <div className="flex flex-col space-y-4 p-2">
             <Avatar onClick={onClick} fallbackText="WR" size="small" imageUrl="https://i.pravatar.cc/300" />
