4d09715a3c6c04cc7d0ab4fd930c5c7c5a6f8f96
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2021-08-12 15:55:51 +0530

    More refinement in CSS.

--- example/v2/Pages/Tag.js
+++ example/v2/Pages/Tag.js
@@ -7,7 +7,7 @@ const Tabs = () => {
 
   const Icon = () => <Favorite size={15} />;
 
-  const onClick = () => alert("onClick event!");
+  const onClose = () => alert("onClose celled!");
 
   return (
     <>
@@ -18,31 +18,31 @@ const Tabs = () => {
             <div className="flex space-x-4">
               <h3>Default: </h3>
               <Tag label="Small" />
-              <Tag type="outline" size="large" label="Large" />
-              <Tag type="solid" label="Small Solid" />
-              <Tag type="solid" size="large" label="Large Solid" />
+              <Tag type="outline" size="large" label="Label" />
+              <Tag type="solid" label="Label" />
+              <Tag type="solid" size="large" label="Label" />
             </div>
             <div className="flex space-x-4">
               <h3>Icons: </h3>
-              <Tag icon={Icon} label="Small" />
-              <Tag icon={Icon} size="large" label="Large" />
-              <Tag icon={Icon} type="solid" label="Small Solid" />
-              <Tag icon={Icon} size="large" type="solid" label="Large Solid" />
+              <Tag icon={Icon} label="Label" />
+              <Tag icon={Icon} size="large" label="Label" />
+              <Tag icon={Icon} type="solid" label="Label" />
+              <Tag icon={Icon} size="large" type="solid" label="Label" />
             </div>
             <div className="flex space-x-4">
               <h3>Clear button: </h3>
-              <Tag icon={Icon} onClick={onClick} label="Small" showClearOption />
-              <Tag icon={Icon} onClick={onClick} size="large" label="Large" showClearOption />
-              <Tag icon={Icon} onClick={onClick} type="solid" label="Small Solid" showClearOption />
-              <Tag icon={Icon} onClick={onClick} size="large" type="solid" label="Large Solid"
+              <Tag icon={Icon} onClose={onClose} label="Label" />
+              <Tag icon={Icon} onClose={onClose} size="large" label="Label" />
+              <Tag icon={Icon} onClose={onClose} type="solid" label="Label" />
+              <Tag icon={Icon} onClose={onClose} size="large" type="solid" label="Label"
                 showClearOption />
             </div>
             <div className="flex space-x-4">
               <h3>Color: </h3>
-              <Tag label="Green" type="color" indicatorColor="bg-green-500" />
-              <Tag label="Yellow" type="color" indicatorColor="bg-yellow-500" />
-              <Tag label="Blue" type="color" indicatorColor="bg-blue-500" />
-              <Tag label="Red" type="color" indicatorColor="bg-red-500" />
+              <Tag label="Label" type="color" indicatorColor="bg-green-500" />
+              <Tag label="Label" type="color" indicatorColor="bg-yellow-500" />
+              <Tag label="Label" type="color" indicatorColor="bg-blue-500" />
+              <Tag label="Label" type="color" indicatorColor="bg-red-500" />
             </div>
           </div>
         </div>
