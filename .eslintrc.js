d6c14f0318c5d3ede7c6cc89c9ff3872dead9df1
Edwin Babu <edwinbbu@gmail.com>
2021-08-12 14:12:14 +0530

    Pageloader added (#415)

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
     "no-console": "error",
     "import/prefer-default-export": "off",
     "react/prop-types": 0,
+    "react/no-unescaped-entities": 0,
     quotes: ["error", "double", { avoidEscape: true }],
   },
 };
