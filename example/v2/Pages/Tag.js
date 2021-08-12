91653e6ba4ddab6028a61387fdc3cfd6a096a759
Akkshay Lawrence <akkshay.lawrence@bigbinary.com>
2021-08-12 08:55:42 +0530

    Props update

--- example/v2/Pages/Tag.js
+++ example/v2/Pages/Tag.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Tag } from '../../../lib/v2';
 import Header from '../Header';
+import { Favorite } from '@bigbinary/neeto-icons';
 
 const Tabs = () => {
+
+  const Icon = () => <Favorite size={15} />;
 
   const onClick = () => {
     alert('onClick event!');
@@ -16,32 +19,32 @@ const Tabs = () => {
           <div className="flex flex-col space-y-4 p-2">
             <div className="flex space-x-4">
               <h3>Default: </h3>
-              <Tag content="Small" />
-              <Tag type="large" size="large" content="Large" />
-              <Tag type="solid" content="Small Solid" />
-              <Tag type="solid" size="large" content="Large Solid" />
+              <Tag label="Small" />
+              <Tag type="outline" size="large" label="Large" />
+              <Tag type="solid" label="Small Solid" />
+              <Tag type="solid" size="large" label="Large Solid" />
             </div>
             <div className="flex space-x-4">
               <h3>Icons: </h3>
-              <Tag icon="ri-pencil-line" content="Small" />
-              <Tag icon="ri-pencil-line" size="large" content="Large" />
-              <Tag icon="ri-pencil-line" type="solid" content="Small Solid" />
-              <Tag icon="ri-pencil-line" size="large" type="solid" content="Large Solid" />
+              <Tag icon={Icon} label="Small" />
+              <Tag icon={Icon} size="large" label="Large" />
+              <Tag icon={Icon} type="solid" label="Small Solid" />
+              <Tag icon={Icon} size="large" type="solid" label="Large Solid" />
             </div>
             <div className="flex space-x-4">
               <h3>Clear button: </h3>
-              <Tag icon="ri-pencil-line" onClick={onClick} content="Small" showClearOption />
-              <Tag icon="ri-pencil-line" onClick={onClick} size="large" content="Large" showClearOption />
-              <Tag icon="ri-pencil-line" onClick={onClick} type="solid" content="Small Solid" showClearOption />
-              <Tag icon="ri-pencil-line" onClick={onClick} size="large" type="solid" content="Large Solid"
+              <Tag icon={Icon} onClick={onClick} label="Small" showClearOption />
+              <Tag icon={Icon} onClick={onClick} size="large" label="Large" showClearOption />
+              <Tag icon={Icon} onClick={onClick} type="solid" label="Small Solid" showClearOption />
+              <Tag icon={Icon} onClick={onClick} size="large" type="solid" label="Large Solid"
                    showClearOption />
             </div>
             <div className="flex space-x-4">
               <h3>Color: </h3>
-              <Tag content="Green" type="color" color="#00BA88" />
-              <Tag content="Yellow" type="color" color="#F3CD82" />
-              <Tag content="Blue" type="color" color="#5E5CE6" />
-              <Tag content="Red" type="color" color="#F56A58" />
+              <Tag label="Green" type="color" indicatorColor="bg-red-500" />
+              <Tag label="Yellow" type="color" indicatorColor="bg-red-500" />
+              <Tag label="Blue" type="color" indicatorColor="bg-red-500" />
+              <Tag label="Red" type="color" indicatorColor="bg-red-500" />
             </div>
           </div>
         </div>
