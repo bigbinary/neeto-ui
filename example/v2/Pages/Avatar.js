57e4b5c62cb9a7364ae8c0627698eb5f712cf2df
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2021-08-16 15:56:59 +0530

    Updated avatar text logic.

--- example/v2/Pages/Avatar.js
+++ example/v2/Pages/Avatar.js
@@ -14,28 +14,30 @@ function Avatars() {
       <div className="p-6">
         <div className="flex p-4 space-x-4 border border-indigo-500 border-dashed">
           <div className="flex flex-col space-y-4 p-2">
-            <Avatar onClick={onClick} content={{ text: "AB" }} size="small" />
-            <Avatar onClick={onClick} content={{ text: "CD" }} size="medium" />
-            <Avatar onClick={onClick} content={{ text: "EF" }} size="large" />
-            <Avatar onClick={onClick} content={{ text: "GH" }} size="xlarge" />
+            <Avatar onClick={onClick} content={{ text: "Akkshay Lawrence" }} size="small" />
+            <Avatar onClick={onClick} content={{ text: "Vinay V" }} size="medium" />
+            <Avatar onClick={onClick} content={{ text: "Neeraj Singh" }} size="large" />
+            <Avatar onClick={onClick} content={{ text: "Goutham Subramanyam" }} size="xlarge" />
           </div>
           <div className="flex flex-col space-y-4 p-2">
-            <Avatar onClick={onClick} content={{ text: "IJ" }} size="small" status="online" isRounded />
-            <Avatar onClick={onClick} content={{ text: "KL" }} size="medium" status="idle" isRounded />
-            <Avatar onClick={onClick} content={{ text: "MN" }} size="large" status="offline" isRounded />
-            <Avatar onClick={onClick} content={{ text: "OP" }} size="xlarge" status="online" isRounded />
+            <Avatar onClick={onClick} content={{ text: "Akkshay Lawrence" }} size="small" status="online" isRounded />
+            <Avatar onClick={onClick} content={{ text: "Vinay V" }} size="medium" status="idle" isRounded />
+            <Avatar onClick={onClick} content={{ text: "Neeraj Singh" }} size="large" status="offline" isRounded />
+            <Avatar onClick={onClick} content={{ text: "Goutham Subramanyam" }} size="xlarge" status="online"
+              isRounded />
           </div>
           <div className="flex flex-col space-y-4 p-2">
-            <Avatar onClick={onClick} content={{ text: "WR", imageUrl }} status="online" size="small" />
-            <Avatar onClick={onClick} content={{ text: "ST", imageUrl }} status="idle" size="medium" />
-            <Avatar onClick={onClick} content={{ text: "UV", imageUrl }} status="offline" size="large" />
-            <Avatar onClick={onClick} content={{ text: "WX", imageUrl }} status="online" size="xlarge" />
+            <Avatar onClick={onClick} content={{ text: "Akkshay Lawrence", imageUrl }} status="online" size="small" />
+            <Avatar onClick={onClick} content={{ text: "Vinay V", imageUrl }} status="idle" size="medium" />
+            <Avatar onClick={onClick} content={{ text: "Neeraj Singh", imageUrl }} status="offline" size="large" />
+            <Avatar onClick={onClick} content={{ text: "Goutham Subramanyam", imageUrl }} status="online"
+              size="xlarge" />
           </div>
           <div className="flex flex-col space-y-4 p-2">
-            <Avatar onClick={onClick} content={{ text: "YZ", imageUrl }} size="small" isRounded />
-            <Avatar onClick={onClick} content={{ text: "AB", imageUrl }} size="medium" isRounded />
-            <Avatar onClick={onClick} content={{ text: "CD", imageUrl }} size="large" isRounded />
-            <Avatar onClick={onClick} content={{ text: "EF", imageUrl }} size="xlarge" isRounded />
+            <Avatar onClick={onClick} content={{ text: "Akkshay Lawrence", imageUrl }} size="small" isRounded />
+            <Avatar onClick={onClick} content={{ text: "Vinay V", imageUrl }} size="medium" isRounded />
+            <Avatar onClick={onClick} content={{ text: "Neeraj Singh", imageUrl }} size="large" isRounded />
+            <Avatar onClick={onClick} content={{ text: "Goutham Subramanyam", imageUrl }} size="xlarge" isRounded />
           </div>
         </div>
       </div>
