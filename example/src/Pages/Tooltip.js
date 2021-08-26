a9b495029de2f965bc8e030f29ac3934d0ff48f6
amaldinesh7 <amalkdinesh@gmail.com>
2021-08-27 02:39:38 +0530

    New examples and props added

--- example/src/Pages/Tooltip.js
+++ example/src/Pages/Tooltip.js
@@ -7,10 +7,44 @@ const Tooltips = () => {
     <div className="w-full">
       <Header title="Tooltip" />
       <div className="p-6 space-y-6">
-        <div className="w-1/2 space-y-8">
-          <div className="flex flex-row items-center justify-start space-x-6">
-            <Tooltip content={"Tooltip"}>
-              <Button label="Auto Placement" />
+        <div className="w-full space-y-8">
+          <div className="flex flex-row items-center justify-start space-x-20">
+            <Tooltip
+              placement={"top"}
+              followCursor={"horizontal"}
+              content={"Content is string"}
+            >
+              <Button label="Follow Cursor" />
+            </Tooltip>
+
+            <Tooltip
+              placement={"bottom"}
+              trigger={"click"}
+              content={
+                <span>
+                  Content is a <b>JSX</b> <u>Element</u>
+                </span>
+              }
+            >
+              <Button label="On Click" />
+            </Tooltip>
+
+            <Tooltip
+              placement={"right-start"}
+              interactive={true}
+              content={
+                <span>
+                  Go to{" "}
+                  <a
+                    href="https://www.bigbinary.com/"
+                    style={{ color: "white" }}
+                  >
+                    bigbinary.com
+                  </a>
+                </span>
+              }
+            >
+              <Button label="Interactive" />
             </Tooltip>
           </div>
         </div>
