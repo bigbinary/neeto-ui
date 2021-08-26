90246aae790042d8cf1e73b60771fb890d519676
Edwin Babu <edwinbbu@gmail.com>
2021-08-26 13:52:12 +0530

    Alert component added (#431)

--- example/v2/Pages/Modal.js
+++ example/v2/Pages/Modal.js
@@ -1,25 +1,24 @@
 import React, { useState } from "react";
 import { Check } from "@bigbinary/neeto-icons";
-import {
-  Button,
-  Modal,
-} from "../../../lib/components";
+import { Button, Modal, Alert } from "../../../lib/components";
 import Header from "../Header";
 
 const Modals = () => {
   const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
   return (
     <div className="w-full">
       <Header title="Modal" />
-      
+
       <div className="p-6 space-y-6">
         <div className="w-1/2 space-y-8">
           <div className="flex flex-row items-center justify-start space-x-6">
             <Button label="Show Modal" onClick={() => setShowModal(true)} />
+            <Button label="Show Alert" onClick={() => setShowAlert(true)} />
           </div>
         </div>
       </div>
-      
+
       <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
         <Modal.Header>
           <h2>Add Label</h2>
@@ -28,8 +27,9 @@ const Modals = () => {
           Somewhere out in space live The Herculoids! Zok, the laser-ray dragon!
           Igoo, the giant rock ape! Tundro, the tremendous! Gloop and Gleep, the
           formless, fearless wonders! With Zandor, their leader, and his wife,
-          Tara, and son, Dorno, they team up to protect their planet from sinister
-          invaders! All-strong! All-brave! All-heroes! They're The Herculoids!
+          Tara, and son, Dorno, they team up to protect their planet from
+          sinister invaders! All-strong! All-brave! All-heroes! They're The
+          Herculoids!
         </Modal.Body>
         <Modal.Footer className="space-x-4">
           <Button
@@ -44,6 +44,15 @@ const Modals = () => {
           />
         </Modal.Footer>
       </Modal>
+
+      <Alert
+        isOpen={showAlert}
+        onClose={() => setShowAlert(false)}
+        onSubmit={() => setShowAlert(false)}
+        message="Are you sure you want to continue? All of your unsaved changes will be
+          lost."
+        title="You have unsaved changes!"
+      />
     </div>
   );
 };
