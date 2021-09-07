b4703619d2fef24d812545101d4769ea09162405
Praveen Murali <itspraveenmurali@gmail.com>
2021-09-07 20:24:18 +0530

    tooltip placement and theme demos added

--- example/src/Pages/Tooltip.js
+++ example/src/Pages/Tooltip.js
@@ -14,11 +14,11 @@ const Tooltips = () => {
               followCursor={"horizontal"}
               content={"Content is string"}
             >
-              <Button label="Follow Cursor" />
+              <Button style="secondary" label="Follow Cursor" />
             </Tooltip>
 
             <Tooltip
-              placement={"bottom"}
+              placement={"top"}
               trigger={"click"}
               content={
                 <span>
@@ -26,7 +26,7 @@ const Tooltips = () => {
                 </span>
               }
             >
-              <Button label="On Click" />
+              <Button style="secondary" label="On Click" />
             </Tooltip>
 
             <Tooltip
@@ -44,8 +44,114 @@ const Tooltips = () => {
                 </span>
               }
             >
-              <Button label="Interactive" />
+              <Button style="secondary" label="Interactive" />
             </Tooltip>
+          </div>
+        </div>
+      </div>
+      <div className="p-6 space-y-6">
+        <div className="grid grid-cols-2 gap-6">
+          <div className="p-4 space-y-8 border border-indigo-500 border-dashed">
+            <h2 className="text-xl">Placement</h2>
+            <div className="flex items-center justify-start gap-8">
+              <Tooltip
+                placement={"top"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="top" />
+              </Tooltip>
+              <Tooltip
+                placement={"top-start"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="top-start" />
+              </Tooltip>
+              <Tooltip
+                placement={"top-end"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="top-end" />
+              </Tooltip>
+            </div>
+            <div className="flex items-center justify-start gap-8">
+              <Tooltip
+                placement={"right"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="right" />
+              </Tooltip>
+              <Tooltip
+                placement={"right-start"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="right-start" />
+              </Tooltip>
+              <Tooltip
+                placement={"right-end"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="right-end" />
+              </Tooltip>
+            </div>
+            <div className="flex items-center justify-start gap-8">
+              <Tooltip
+                placement={"bottom"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="bottom" />
+              </Tooltip>
+              <Tooltip
+                placement={"bottom-start"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="bottom-start" />
+              </Tooltip>
+              <Tooltip
+                placement={"bottom-end"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="bottom-end" />
+              </Tooltip>
+            </div>
+            <div className="flex items-center justify-start gap-8">
+              <Tooltip
+                placement={"left"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="left" />
+              </Tooltip>
+              <Tooltip
+                placement={"left-start"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="left-start" />
+              </Tooltip>
+              <Tooltip
+                placement={"left-end"}
+                content={"Tooltip"}
+              >
+                <Button style="secondary" label="left-end" />
+              </Tooltip>
+            </div>
+          </div>
+          <div className="p-4 space-y-8 border border-indigo-500 border-dashed">
+            <h2 className="text-xl">Theme</h2>
+            <div className="flex flex-row flex-wrap items-center justify-start gap-8">
+              <Tooltip
+                placement={"top"}
+                content={"Tooltip"}
+                theme={"dark"}
+              >
+                <Button style="secondary" label="dark" />
+              </Tooltip>
+              <Tooltip
+                placement={"top"}
+                content={"Tooltip"}
+                theme={"light"}
+              >
+                <Button style="secondary" label="light" />
+              </Tooltip>
+            </div>
           </div>
         </div>
       </div>
