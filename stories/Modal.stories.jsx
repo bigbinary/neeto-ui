53b004d234306bc32899824ce2f531580a57039d
Praveen Murali <itspraveenmurali@gmail.com>
2021-09-28 20:55:39 +0530

    UI clean-up Modal

--- stories/Modal.stories.jsx
+++ stories/Modal.stories.jsx
@@ -3,12 +3,11 @@ import { Check } from "@bigbinary/neeto-icons";
 
 import Button from "../lib/components/Button";
 import Modal from "../lib/components/Modal";
-import Alert from "../lib/components/Alert";
 
 export default {
   title: "Overlays/Modal",
   component: Modal,
-  subcomponents: { Button, Alert },
+  subcomponents: { Button },
   parameters: {
     layout: "padded",
     docs: {
@@ -19,16 +18,15 @@ export default {
   },
 };
 
-export const Modals = () => {
+export const Default = () => {
   const [showModal, setShowModal] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
+
   return (
     <div className="w-full">
       <div className="space-y-6">
         <div className="w-1/2 space-y-8">
           <div className="flex flex-row items-center justify-start space-x-6">
             <Button label="Show Modal" onClick={() => setShowModal(true)} />
-            <Button label="Show Alert" onClick={() => setShowAlert(true)} />
           </div>
         </div>
       </div>
@@ -50,23 +48,127 @@ export const Modals = () => {
             icon={Check}
             label="Continue"
             onClick={() => setShowModal(false)}
+            size="large"
           />
           <Button
             style="text"
             label="Cancel"
             onClick={() => setShowModal(false)}
+            size="large"
+          />
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export const ModalSizing = () => {
+  const [showModalExtraSmall, setShowModalExtraSmall] = useState(false);
+  const [showModalSmall, setShowModalSmall] = useState(false);
+  const [showModalMedium, setShowModalMedium] = useState(false);
+
+  return (
+    <div className="w-full">
+      <div className="space-y-6">
+        <div className="w-1/2 space-y-8">
+          <div className="flex flex-row items-center justify-start space-x-6">
+            <Button
+              label="Extra Small"
+              onClick={() => setShowModalExtraSmall(true)}
+            />
+            <Button label="Small" onClick={() => setShowModalSmall(true)} />
+            <Button label="Medium" onClick={() => setShowModalMedium(true)} />
+          </div>
+        </div>
+      </div>
+
+      <Modal
+        isOpen={showModalExtraSmall}
+        onClose={() => setShowModalExtraSmall(false)}
+        size="xs"
+      >
+        <Modal.Header>
+          <h2>They're creepy & they're kooky</h2>
+        </Modal.Header>
+        <Modal.Body>
+          Somewhere out in space live The Herculoids! Zok, the laser-ray dragon!
+          Igoo, the giant rock ape! Tundro, the tremendous!
+        </Modal.Body>
+        <Modal.Footer className="space-x-4">
+          <Button
+            icon={Check}
+            size="large"
+            label="Continue"
+            onClick={() => setShowModalExtraSmall(false)}
+          />
+          <Button
+            style="text"
+            size="large"
+            label="Cancel"
+            onClick={() => setShowModalExtraSmall(false)}
           />
         </Modal.Footer>
       </Modal>
 
-      <Alert
-        isOpen={showAlert}
-        onClose={() => setShowAlert(false)}
-        onSubmit={() => setShowAlert(false)}
-        message="Are you sure you want to continue? All of your unsaved changes will be
-            lost."
-        title="You have unsaved changes!"
-      />
+      <Modal
+        isOpen={showModalSmall}
+        onClose={() => setShowModalSmall(false)}
+        size="sm"
+      >
+        <Modal.Header>
+          <h2>They're creepy & they're kooky</h2>
+        </Modal.Header>
+        <Modal.Body>
+          Somewhere out in space live The Herculoids! Zok, the laser-ray dragon!
+          Igoo, the giant rock ape! Tundro, the tremendous!
+        </Modal.Body>
+        <Modal.Footer className="space-x-4">
+          <Button
+            icon={Check}
+            size="large"
+            label="Continue"
+            onClick={() => setShowModalSmall(false)}
+          />
+          <Button
+            style="text"
+            size="large"
+            label="Cancel"
+            onClick={() => setShowModalSmall(false)}
+          />
+        </Modal.Footer>
+      </Modal>
+
+      <Modal
+        isOpen={showModalMedium}
+        onClose={() => setShowModalMedium(false)}
+        size="md"
+      >
+        <Modal.Header>
+          <h2>Add Label</h2>
+        </Modal.Header>
+        <Modal.Body>
+          Somewhere out in space live The Herculoids! Zok, the laser-ray dragon!
+          Igoo, the giant rock ape! Tundro, the tremendous! Gloop and Gleep, the
+          formless, fearless wonders! With Zandor, their leader, and his wife,
+          Tara, and son, Dorno, they team up to protect their planet from
+          sinister invaders! All-strong! All-brave! All-heroes! They're The
+          Herculoids!
+        </Modal.Body>
+        <Modal.Footer className="space-x-4">
+          <Button
+            icon={Check}
+            size="large"
+            label="Continue"
+            onClick={() => setShowModalMedium(false)}
+          />
+          <Button
+            style="text"
+            size="large"
+            label="Cancel"
+            onClick={() => setShowModalMedium(false)}
+          />
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
