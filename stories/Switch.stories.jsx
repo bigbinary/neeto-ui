9cc70e2cc2f11b4b7bc0979cd33d5914a43f8264
Vipul Goel <58417652+vipul-goel@users.noreply.github.com>
2021-09-29 15:35:59 +0530

    Added documentations for Radio, Select, Switch, Textarea components (#484)

---
+++ stories/Switch.stories.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import Switch from "../lib/components/Switch";
+
+export default {
+  title: "Components/Switch",
+  component: Switch,
+  parameters: {
+    layout: "padded",
+    docs: {
+      description: {
+        component: '`import { Switch } from "@bigbinary/neetoui";`',
+      },
+    },
+  },
+};
+
+const Template = (args) => (
+  <div className="p-4">
+    <Switch {...args} />
+  </div>
+);
+
+export const CheckedState = Template.bind({});
+CheckedState.args = {
+  checked: true,
+};
+
+export const UncheckedState = Template.bind({});
+UncheckedState.args = {
+  checked: false,
+};
+
+export const DisabledState = Template.bind({});
+DisabledState.args = {
+  disabled: true,
+};
+
+export const WithLabel = Template.bind({});
+WithLabel.args = {
+  label: "Switch Label Example",
+};
+
+export const WithCustomLabel = Template.bind({});
+WithCustomLabel.args = {
+  label: <span className="ml-3 shadow-md">Switch Label Example</span>,
+};
+
+export const WithChangeListner = Template.bind({});
+WithChangeListner.args = {
+  onChange: (e) => alert("Callback invoked"),
+};
