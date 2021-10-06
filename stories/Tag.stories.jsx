4817e8ed39713047d86386776224d32aa7039991
Praveen Murali <itspraveenmurali@gmail.com>
2021-10-06 15:41:31 +0530

    Updated styles and doc for tags

--- stories/Tag.stories.jsx
+++ stories/Tag.stories.jsx
@@ -16,28 +16,50 @@ export default {
   },
 };
 
-export const Tags = () => {
+const Template = (args) => <Tag {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: "Label",
+};
+
+export const TagColorStory = (props) => (
+  <div className="p-6">
+    <Tag {...props} />
+  </div>
+);
+
+TagColorStory.storyName = "Tag with Color";
+TagColorStory.args = {
+  color: "green",
+  style: "outline",
+  label: "Label",
+  size: "large",
+  onClose: null,
+};
+
+export const AllVariants = () => {
   const onClose = () => alert("onClose Triggered!");
 
   return (
     <div className="p-6">
       <div className="p-4 space-y-8 border border-indigo-500 border-dashed">
-        <div className="flex flex-col space-y-6 p-2">
-          <div className="flex flex-row justify-start items-start space-x-4">
+        <div className="flex flex-col p-2 space-y-6">
+          <div className="flex flex-row items-start justify-start space-x-4">
             <h5>Outline Small: </h5>
             <Tag label="Label" />
             <Tag icon={Favorite} label="Label" />
             <Tag onClose={onClose} label="Label" />
             <Tag icon={Favorite} onClose={onClose} label="Label" />
           </div>
-          <div className="flex flex-row justify-start items-start space-x-4">
+          <div className="flex flex-row items-start justify-start space-x-4">
             <h5>Outline Large: </h5>
             <Tag size="large" label="Label" />
             <Tag size="large" icon={Favorite} label="Label" />
             <Tag size="large" onClose={onClose} label="Label" />
             <Tag size="large" icon={Favorite} onClose={onClose} label="Label" />
           </div>
-          <div className="flex flex-row justify-start items-start space-x-4">
+          <div className="flex flex-row items-start justify-start space-x-4">
             <h5>Colored Outline Large : </h5>
             <Tag
               style="outline"
@@ -71,7 +93,7 @@ export const Tags = () => {
               label="Label"
             />
           </div>
-          <div className="flex flex-row justify-start items-start space-x-4">
+          <div className="flex flex-row items-start justify-start space-x-4">
             <h5>Solid Small: </h5>
             <Tag style="solid" label="Label" />
             <Tag style="solid" icon={Favorite} label="Label" />
@@ -83,7 +105,7 @@ export const Tags = () => {
               label="Label"
             />
           </div>
-          <div className="flex flex-row justify-start items-start space-x-4">
+          <div className="flex flex-row items-start justify-start space-x-4">
             <h5>Solid Large : </h5>
             <Tag style="solid" size="large" label="Label" />
             <Tag style="solid" size="large" icon={Favorite} label="Label" />
@@ -96,7 +118,7 @@ export const Tags = () => {
               label="Label"
             />
           </div>
-          <div className="flex flex-row justify-start items-start space-x-4">
+          <div className="flex flex-row items-start justify-start space-x-4">
             <h5>Solid With Colors : </h5>
             <Tag size="large" style="solid" label="Label" color="grey" />
             <Tag size="large" style="solid" label="Label" color="red" />
@@ -104,14 +126,14 @@ export const Tags = () => {
             <Tag size="large" style="solid" label="Label" color="blue" />
             <Tag size="large" style="solid" label="Label" color="yellow" />
           </div>
-          <div className="flex flex-row justify-start items-start space-x-4">
+          <div className="flex flex-row items-start justify-start space-x-4">
             <h5>With Indicator : </h5>
             <Tag label="Label" indicator="bg-green-500" />
             <Tag label="Label" indicator="bg-yellow-500" />
             <Tag label="Label" indicator="bg-blue-500" />
             <Tag label="Label" indicator="bg-red-500" />
           </div>
-          <div className="flex flex-row justify-start items-start space-x-4">
+          <div className="flex flex-row items-start justify-start space-x-4">
             <h5>With Indicator Large : </h5>
             <Tag size="large" label="Label" indicator="bg-green-500" />
             <Tag size="large" label="Label" indicator="bg-yellow-500" />
@@ -122,19 +144,4 @@ export const Tags = () => {
       </div>
     </div>
   );
-};
-
-export const TagColorStory = (props) => (
-  <div className="flex items-center justify-center">
-    <Tag {...props} />
-  </div>
-);
-
-TagColorStory.storyName = "Tag with Color";
-TagColorStory.args = {
-  color: "green",
-  style: "outline",
-  label: "Label",
-  size: "large",
-  onClose: null,
 };
