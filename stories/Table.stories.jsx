7487415984b5e081304ef3d5fc93376ff48f1432
Edwin Babu <edwinbbu@gmail.com>
2021-10-20 18:43:30 +0530

    Updated controls for table in storybook

--- stories/Table.stories.jsx
+++ stories/Table.stories.jsx
@@ -53,17 +53,23 @@ const useSortTable = (items, config = null) => {
 };
 
 const Template = ({ isLoading, ...args }) => {
-  const { items, performSort, sortConfig } = useSortTable(TABLE_DATA, { key: "", direction: "" });
+  const { items, performSort, sortConfig } = useSortTable(TABLE_DATA, {
+    key: "",
+    direction: "",
+  });
   const { key, direction } = sortConfig;
-  const isSorted = (headerKey, sortDirection) => key === headerKey && direction === sortDirection;
+  const isSorted = (headerKey, sortDirection) =>
+    key === headerKey && direction === sortDirection;
   return (
     <>
       <Table hasActions hasCheckbox {...args}>
         <Table.Head>
           <Table.Tr>
-            <Table.Th>
-            <Checkbox name="all" />
-            </Table.Th>
+            {args.hasCheckbox && (
+              <Table.Th>
+                <Checkbox name="all" />
+              </Table.Th>
+            )}
             {TABLE_HEADERS.map(({ label, key }, idx) => (
               <Table.Th
                 key={idx}
@@ -77,26 +83,32 @@ const Template = ({ isLoading, ...args }) => {
             <Table.Th />
           </Table.Tr>
         </Table.Head>
-        <Table.Body
-          isLoading={isLoading}
-        >
+        <Table.Body isLoading={isLoading}>
           {items.map(({ name, email, phone_number, pass_year }, idx) => (
             <Table.Tr key={idx}>
-              <Table.Td>
-                <Checkbox name={idx} />
-              </Table.Td>
+              {args.hasCheckbox && (
+                <Table.Td>
+                  <Checkbox name={idx} />
+                </Table.Td>
+              )}
               <Table.Td center>{name}</Table.Td>
               <Table.Td center>{email}</Table.Td>
               <Table.Td center>{phone_number}</Table.Td>
               <Table.Td center>{pass_year}</Table.Td>
-              <Table.Td>
-                <div className="flex flex-row items-center justify-end space-x-3">
-                  <Dropdown icon={MenuHorizontal} buttonStyle="text" autoWidth>
-                    <li>Edit</li>
-                    <li>Delete</li>
-                  </Dropdown>
-                </div>
-              </Table.Td>
+              {args.hasActions && (
+                <Table.Td>
+                  <div className="flex flex-row items-center justify-end space-x-3">
+                    <Dropdown
+                      icon={MenuHorizontal}
+                      buttonStyle="text"
+                      autoWidth
+                    >
+                      <li>Edit</li>
+                      <li>Delete</li>
+                    </Dropdown>
+                  </div>
+                </Table.Td>
+              )}
             </Table.Tr>
           ))}
         </Table.Body>
@@ -105,7 +117,7 @@ const Template = ({ isLoading, ...args }) => {
   );
 };
 
-export const TableStory = Template.bind({})
+export const TableStory = Template.bind({});
 TableStory.storyName = "Table";
 TableStory.args = {
   hasActions: true,
@@ -113,6 +125,6 @@ TableStory.args = {
   isLoading: {
     loading: false,
     rowCount: 50,
-    columnCount: 6
-  }
-}
+    columnCount: 6,
+  },
+};
