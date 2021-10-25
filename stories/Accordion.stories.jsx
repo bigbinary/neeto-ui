f578d8b992c9297b7f21408951dbbb8dfec34b9d
Ajmal Noushad <ajufhn@gmail.com>
2021-10-25 13:29:02 +0530

    Created Accordion component (#569)
    * Accordion component created
    
    - Accordion component created
    - Added Story for Accordion
    
    * Added PropTypes to Accordion and Accordion.Item
    
    - Updated css classnames
    - Fixed jsconfig warning
    - Added story for defaultActiveKey
    
    * Added secondary style Accordion
    
    - Added secondary style Accordion
    - Removed border for single and last Accordion Item
    - Added padded prop to apply padding to Accordion container
    
    * Exported Accordion

---
+++ stories/Accordion.stories.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+import Accordion from "../lib/components/Accordion";
+
+export default {
+  title: "Components/Accordion",
+  component: Accordion,
+  subcomponents: {
+    'Accordion.Item': Accordion.Item
+  },
+  parameters: {
+    layout: "padded",
+    docs: {
+      description: {
+        component: '`import { Accordion } from "@bigbinary/neetoui/v2";`',
+      },
+    },
+  },
+};
+
+export const AccordionStory = (args) => {
+  return (
+    <Accordion {...args}>
+      <Accordion.Item title="Accordion 1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </Accordion.Item>
+    </Accordion>
+  );
+};
+
+AccordionStory.storyName = "Single Accordion"
+
+const Template = (args) => {
+  return (
+    <Accordion {...args}>
+      <Accordion.Item title="Accordion 1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </Accordion.Item>
+      <Accordion.Item title="Accordion 2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </Accordion.Item>
+      <Accordion.Item title="Accordion 3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </Accordion.Item>
+    </Accordion>
+  );
+};
+
+export const MultipleAccordionStory = Template.bind({});
+MultipleAccordionStory.storyName = "Multiple Accordions";
+
+export const DefaultActiveKeyStory = Template.bind({});
+DefaultActiveKeyStory.storyName = "Accordion with defaultActiveKey";
+DefaultActiveKeyStory.args = {
+  defaultActiveKey: 1
+}
+
+export const SecondaryAccordionWithPadding = (args) => {
+  return (
+    <Accordion {...args}>
+      <Accordion.Item title="Accordion 1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </Accordion.Item>
+      <Accordion.Item title="Accordion 2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </Accordion.Item>
+      <Accordion.Item title="Accordion 3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </Accordion.Item>
+    </Accordion>
+  );
+};
+
+SecondaryAccordionWithPadding.args = {
+  padded: true,
+  style: "secondary"
+}
+
+SecondaryAccordionWithPadding.parameters = {
+  layout: "default"
+}
