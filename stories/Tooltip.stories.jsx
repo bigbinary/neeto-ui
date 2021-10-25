400f4557198df5f2c0202619b56a7948567a7a53
Ajmal Noushad <ajufhn@gmail.com>
2021-10-25 22:11:47 +0530

    Added documentation for Tooltips on custom component

--- stories/Tooltip.stories.jsx
+++ stories/Tooltip.stories.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 import Button from "../lib/components/Button";
 import Tooltip from "../lib/components/Tooltip";
 import Typography from "../lib/components/Typography";
+import TooltipDocs from "!raw-loader!./TooltipDocs.mdx";
 
 export default {
   title: "Overlays/Tooltip",
@@ -135,3 +136,28 @@ export const TooltipOnText = () => {
     </div>
   );
 };
+
+const CustomComponent = forwardRef(({ text }, ref) => (
+  <div ref={ref} className="neeto-ui-shadow-lg p-10 rounded-md justify-center items-center neeto-ui-bg-black neeto-ui-text-white cursor-pointer">
+    {text}
+  </div>
+));
+
+export const TooltipOnCustomComponent = () => {
+  const CustomComponent = forwardRef(({ text }, ref) => (
+    <div ref={ref} className="shadow-lg p-10 rounded-md justify-center items-center neeto-ui-bg-black neeto-ui-text-white cursor-pointer">
+      {text}
+    </div>
+  ));
+  return (
+    <div className=" p-10 flex items-center justify-center space-x-6">
+      <Tooltip placement="top" content={"Tooltip"}>
+        <CustomComponent text="Custom Component" />
+      </Tooltip>
+    </div>
+  );
+};
+
+TooltipOnCustomComponent.parameters = {
+  docs: {description: { story: TooltipDocs }}
+}
