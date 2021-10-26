c84fad3dbeaf8cf812b7bc4b10bd7c0f4ae22709
karthiknmenon <menonk484@gmail.com>
2021-10-26 12:28:32 +0530

    Add date picker component
    - Added datePicker component from AntD. - Add DateRangePicker and DateInput to neetoUI. - Custom style DatePicker to neetoUI palette.

---
+++ example/src/Pages/DatePicker.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../Header";
+import { DatePicker } from "../../../lib/components";
+
+const Date = () => {
+  return (
+    <>
+      <Header title="Tags" />
+      <div className="p-6">
+        <div className="p-4 border border-indigo-500 border-dashed">
+          <div className="w-1/6 space-y-8">
+            <DatePicker type="date" />
+            <DatePicker type="range" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Date;
