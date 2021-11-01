24474d6498d810513df0f71c775f2894dc45e40f
Ajmal Noushad <ajufhn@gmail.com>
2021-11-01 10:16:04 +0530

    Added Documentation for AppSwitcher (#596)
    * Added Documentation for AppSwitcher
    
    - Fixed Rendered more hooks than during the previous render error caused due to if condition.
    - Added propTypes documentation
    - Removed unused prop (closeButton)
    - Added story with controls for AppSwitcher
    
    * Updated story
    
    - Removed unused imports

---
+++ stories/AppSwitcher.stories.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Sidebar from "../lib/components/layouts/Sidebar";
+import AppSwitcher from "../lib/components/layouts/AppSwitcher";
+import { NAV_LINKS } from "../example/src/constants";
+
+export default {
+  title: "Layouts/AppSwitcher",
+  component: AppSwitcher,
+  parameters: {
+    layout: "fullscreen",
+    docs: {
+      description: {
+        component: '`import { AppSwitcher } from "@bigbinary/neetoui/v2/layouts";`',
+      },
+    },
+  },
+};
+
+export const AppSwitcherStory = ({ onClose, isOpen, ...args }) => {
+  const [isAppSwitcherOpen, setIsAppSwitcherOpen] = useState(isOpen);
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
+
+  useEffect(() => {
+    setIsAppSwitcherOpen(isOpen);
+  }, [isOpen]);
+
+  return (
+    <Router>
+      <Sidebar
+        navLinks={NAV_LINKS.slice(3)}
+        onAppSwitcherToggle={() => setIsAppSwitcherOpen((isOpen) => !isOpen)}
+        isCollapsed={isSidebarCollapsed}
+        onCollapse={() => setIsSidebarCollapsed((collapsed) => !collapsed)}
+        showAppSwitcher
+        appName="neetoUI"
+      />
+      <AppSwitcher
+        isOpen={isAppSwitcherOpen}
+        isSidebarOpen={!isSidebarCollapsed}
+        onClose={() => setIsAppSwitcherOpen(false)}
+        {...args}
+      />
+    </Router>
+  );
+};
+
+AppSwitcherStory.storyName = "AppSwitcher";
+AppSwitcherStory.args = {
+  isOpen: true,
+  v2: true
+};
