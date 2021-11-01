de450912aad495a48f6f542a75d6b9533244a999
Ajmal Noushad <ajufhn@gmail.com>
2021-11-01 22:07:49 +0530

    Added `helpIconProps` prop for displaying help icon with label (#600)

--- stories/Label.stories.jsx
+++ stories/Label.stories.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 import Label from "../lib/components/Label";
+import Button from "../lib/components/Button";
 
 export default {
   title: "Components/Label",
@@ -13,6 +15,7 @@ export default {
       },
     },
   },
+  subcomponents: { Button }
 };
 
 const Template = ({ children, ...args }) => <Label {...args}>{children}</Label>;
@@ -26,4 +29,20 @@ export const Required = Template.bind({});
 Required.args = {
   children: "This is a required label",
   required: true,
+};
+
+export const WithHelpIcon = (args) => (
+  <BrowserRouter>
+    <Label {...args} />
+  </BrowserRouter>
+);
+WithHelpIcon.args = {
+  children: "This is a label with a help icon",
+  helpIconProps: {
+    href: "https://github.com/bigbinary/neeto-ui",
+    tooltipProps: {
+      content: "Help icon tooltip",
+      position: "bottom"
+    }
+  },
 };
