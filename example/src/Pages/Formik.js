0770375676399d588e64f3a03ba7bdb6cd873afe
Ajmal Noushad <ajufhn@gmail.com>
2021-11-25 00:32:28 +0530

    Fixed Formik Radio input issues (#641)
    * Fixed Formik Radio input issues
    
    - Added error styles
    - Added error text message
    - Updated Formik validation for Radio Input
    
    * Avoided overriding value prop for Item through parent.
    
    * Added support for initialValues
    
    - Added support for initialValues
    - Updated error styling method

--- example/src/Pages/Formik.js
+++ example/src/Pages/Formik.js
@@ -6,22 +6,23 @@ import {
   Radio as FormikRadio,
   Select as FormikSelect,
   Switch as FormikSwitch,
-  BlockNavigation
+  BlockNavigation,
 } from "../../../lib/components/formik";
-
 import Header from "../Header";
 
 const FormikElements = () => {
   const RADIO_OPTIONS = [
     {
       label: "Everyone",
-      value: true,
+      value: "everyone",
       id: "Everyone",
+      name: "options",
     },
     {
       label: "Logged in users",
-      value: false,
+      value: "loggedInUsers",
       id: "Logged in users",
+      name: "options",
     },
   ];
 
@@ -45,7 +46,7 @@ const FormikElements = () => {
           onSubmit={() => {}}
         >
           <Form className="w-2/6">
-            <BlockNavigation/>
+            <BlockNavigation />
             <FormikInput
               name="formikInput"
               type="text"
@@ -58,10 +59,18 @@ const FormikElements = () => {
             <FormikRadio
               name="formikRadio"
               label="Formik Radio"
-              options={RADIO_OPTIONS}
               className="mb-6"
               data-testid="formik-radio"
-            />
+            >
+              {RADIO_OPTIONS.map(({ name, label, value, id }) => (
+                <FormikRadio.Item
+                  key={id}
+                  name={name}
+                  label={label}
+                  value={value}
+                />
+              ))}
+            </FormikRadio>
             <FormikSelect
               name="formikSelect"
               label="Formik Select"
