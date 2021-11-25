f29de2931d68a571e22720da83e83db45d7be27c
Edwin Babu <edwinbbu@gmail.com>
2021-11-25 13:01:15 +0530

    Resolved conflicts

--- stories/Components/Radio.stories.jsx
+++ stories/Components/Radio.stories.jsx
@@ -15,20 +15,42 @@ export default {
   },
 };
 
-export const Options = () => {
+export const Options = (args) => {
   return (
-    <Radio label="Radio Options" className="space-y-4">
-      <Radio.Item label="option1" value="option1" />
-      <Radio.Item label="option2" value="option2" />
+    <Radio {...args}>
+      <Radio.Item name="options" label="option1" value="option1" />
+      <Radio.Item name="options" label="option2" value="option2" />
     </Radio>
   );
 };
 
-export const OptionsStacked = () => {
+Options.args = {
+  label: "Radio Options",
+};
+
+export const OptionsStacked = (args) => {
   return (
-    <Radio label="Radio Options Stacked" className="space-y-4" stacked>
-      <Radio.Item label="option1" value="option1" />
-      <Radio.Item label="option2" value="option2" />
+    <Radio {...args}>
+      <Radio.Item name="stackedOptions" label="option1" value="option1" />
+      <Radio.Item name="stackedOptions" label="option2" value="option2" />
+    </Radio>
+  );
+};
+
+OptionsStacked.args = {
+  label: "Radio Options Stacked",
+  stacked: true
+};
+
+export const ControlledRadio = (args) => {
+  const [value, setValue] = React.useState("");
+  return (
+    <Radio {...args} onChange={(e) => setValue(e.target.value)}>
+      <Radio.Item checked={value === "option1"} name="controlledOptions" label="option1" value="option1" />
+      <Radio.Item checked={value === "option2"} name="controlledOptions" label="option2" value="option2" />
+      <Radio.Item checked={value === "option3"} name="controlledOptions" label="option3" value="option3" />
+      <Radio.Item checked={value === "option4"} name="controlledOptions" label="option4" value="option4" />
+      <Radio.Item checked={value === "option5"} name="controlledOptions" label="option5" value="option5" />
     </Radio>
   );
 };
