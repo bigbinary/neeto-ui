d0a122017329dbedbcfc3e9d4145c3b9ced3a9ba
Ajmal Noushad <ajufhn@gmail.com>
2021-12-03 15:19:20 +0530

    Added propTypes documentation for Container and Scrollable
    - Added stories with different variants for Container and Scrollable
    - Added documentation on props for Container and Scrollable

---
+++ stories/Layouts/Container.stories.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import { Container, Header, Scrollable } from "../../lib/components/layouts";
+
+const description = `
+The container centers your content horizontally by providing a horizontal padding. It's the most basic layout element.
+
+\`import { Container } from "@bigbinary/neetoui/v2/layouts"\`
+`;
+
+export default {
+  title: "Layouts/Container",
+  description: "",
+  component: Container,
+  parameters: {
+    layout: "fullscreen",
+    docs: {
+      description: {
+        component: description,
+      },
+    },
+  },
+};
+
+export const ContainerStory = (args) => {
+  return (
+    <Container {...args}>
+      <div className="neeto-ui-bg-gray-100 w-full h-full flex-1" />
+    </Container>
+  );
+};
+ContainerStory.storyName = "Container";
+ContainerStory.decorators = [
+  (Story) => <div className="bg-green-200">{Story()}</div>,
+];
+
+const DummyCard = () => (
+  <div className="neeto-ui-bg-white w-full h-36 rounded-md p-5 space-y-2 shadow">
+    <div className="neeto-ui-bg-gray-200 w-1/4 h-8 flex-1 neeto rounded" />
+    <div className="neeto-ui-bg-gray-200 w-full h-4 flex-1 neeto rounded" />
+    <div className="neeto-ui-bg-gray-200 w-5/6 h-4 flex-1 neeto rounded" />
+    <div className="neeto-ui-bg-gray-200 w-3/4 h-4 flex-1 neeto rounded" />
+  </div>
+);
+
+export const WithHeaderAndScrollable = (args) => {
+  return (
+    <Container {...args}>
+      <Header title="Header" />
+      <Scrollable
+        className="w-full space-y-6 py-6 neeto-ui-bg-gray-300"
+        size="small"
+      >
+        {[...Array(6)].map((_, i) => (
+          <DummyCard key={i} />
+        ))}
+      </Scrollable>
+    </Container>
+  );
+};
+
+WithHeaderAndScrollable.args = {
+  isHeaderFixed: true,
+};
---
+++ stories/Layouts/Scrollable.stories.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+import {
+  Header,
+  SubHeader,
+  Scrollable,
+  Container,
+} from "../../lib/components/layouts";
+
+const description = `
+Scrollable is used when contents are overflowing the viewport and requires scrolling. It is primarily used along with Container.
+
+\`import { Scrollable } from "@bigbinary/neetoui/v2/layouts"\`
+`;
+
+export default {
+  title: "Layouts/Scrollable",
+  description: "",
+  component: Scrollable,
+  parameters: {
+    layout: "fullscreen",
+    docs: {
+      description: {
+        component: description,
+      },
+    },
+  },
+};
+
+const DummyCard = () => (
+  <div className="neeto-ui-bg-white w-full h-36 rounded-md p-5 space-y-2 shadow">
+    <div className="neeto-ui-bg-gray-200 w-1/4 h-8 flex-1 neeto rounded" />
+    <div className="neeto-ui-bg-gray-200 w-full h-4 flex-1 neeto rounded" />
+    <div className="neeto-ui-bg-gray-200 w-5/6 h-4 flex-1 neeto rounded" />
+    <div className="neeto-ui-bg-gray-200 w-3/4 h-4 flex-1 neeto rounded" />
+  </div>
+);
+
+export const ScrollableStory = (args) => {
+  return (
+    <Scrollable {...args} className="neeto-ui-bg-gray-100 p-6 space-y-6">
+      {[...Array(5)].map((_, i) => (
+        <DummyCard key={i} />
+      ))}
+    </Scrollable>
+  );
+};
+ScrollableStory.storyName = "Scrollable";
+
+export const WithHeader = (args) => {
+  return (
+    <Container isHeaderFixed>
+      <Header title="Header" />
+      <Scrollable
+        {...args}
+        className="w-full space-y-6 py-6 neeto-ui-bg-gray-300"
+      >
+        {[...Array(5)].map((_, i) => (
+          <DummyCard key={i} />
+        ))}
+      </Scrollable>
+    </Container>
+  );
+};
+
+WithHeader.args = {
+  size: "large",
+};
+
+export const WithHeaderAndSubHeader = (args) => {
+  return (
+    <Container isHeaderFixed>
+      <Header title="Header" />
+      <SubHeader
+        searchProps={{}}
+        disableButtonProps={{}}
+        deleteButtonProps={{}}
+      />
+      <Scrollable
+        {...args}
+        className="w-full space-y-6 py-6 neeto-ui-bg-gray-300"
+      >
+        {[...Array(6)].map((_, i) => (
+          <DummyCard key={i} />
+        ))}
+      </Scrollable>
+    </Container>
+  );
+};
+
+WithHeader.args = {
+  size: "small",
+};
