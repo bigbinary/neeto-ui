9f8f2d7034a8af89f0a246046ab1dc7267dcff9e
Goutham Subramanyam <gsn.goutham@gmail.com>
2021-12-31 01:49:42 +0530

    - Removed Text Action Dropdown - Updated Examples

--- example/src/Pages/ActionDropdown.js
+++ example/src/Pages/ActionDropdown.js
@@ -16,45 +16,56 @@ const ActionDropdowns = () => {
       <div className="p-6 space-y-6">
         <div className="flex flex-row flex-wrap items-center justify-start p-4 space-x-6 border border-indigo-500 border-dashed">
           <ActionDropdown
-            label="Primary ActionDropdown"
+            label="Primary"
             style="primary"
-            onClick={() => handleClick("Clicked primary ActionDropdown")}
+            onClick={() => handleClick("Clicked Primary ActionDropdown")}
           >
             {listItems.map((item, idx) => (
               <li key={idx}>{item}</li>
             ))}
           </ActionDropdown>
           <ActionDropdown
-            label="Secondary ActionDropdown"
+            label="Secondary"
             style="secondary"
-            onClick={() => handleClick("Clicked secondary ActionDropdown")}
+            onClick={() => handleClick("Clicked Secondary ActionDropdown")}
           >
             {listItems.map((item, idx) => (
               <li key={idx}>{item}</li>
             ))}
           </ActionDropdown>
           <ActionDropdown
-            label="Text ActionDropdown"
-            style="text"
-            onClick={() => handleClick("Clicked text ActionDropdown")}
-          >
-            {listItems.map((item, idx) => (
-              <li key={idx}>{item}</li>
-            ))}
-          </ActionDropdown>
-          <ActionDropdown
-            label="Disabled ActionDropdown"
+            label="Disabled"
+            onClick={() => handleClick("Clicked Disabled ActionDropdown")}
             disabled
-            onClick={() => handleClick("Clicked disabled ActionDropdown")}
           >
             {listItems.map((item, idx) => (
               <li key={idx}>{item}</li>
             ))}
           </ActionDropdown>
           <ActionDropdown
-            label="Large ActionDropdown"
             size="large"
-            onClick={() => handleClick("Clicked large ActionDropdown")}
+            label="Primary Large"
+            onClick={() => handleClick("Clicked Primary Large ActionDropdown")}
+          >
+            {listItems.map((item, idx) => (
+              <li key={idx}>{item}</li>
+            ))}
+          </ActionDropdown>
+          <ActionDropdown
+            size="large"
+            style="secondary"
+            label="Secondary Large"
+            onClick={() => handleClick("Clicked Secondary Large ActionDropdown")}
+          >
+            {listItems.map((item, idx) => (
+              <li key={idx}>{item}</li>
+            ))}
+          </ActionDropdown>
+          <ActionDropdown
+            size="large"
+            label="Disabled Large"
+            onClick={() => handleClick("Clicked Disabled Large ActionDropdown")}
+            disabled
           >
             {listItems.map((item, idx) => (
               <li key={idx}>{item}</li>
