6cfb8d05d7bff99842adec6de92905ade97dfa23
Goutham Subramanyam N <gsn.goutham@gmail.com>
2022-01-01 13:01:09 +0530

    Added ActionDropdown Story (#736)

---
+++ stories/Components/ActionDropdown.stories.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import ActionDropdown from "../../lib/components/ActionDropdown";
+
+export default {
+  title: "Components/ActionDropdown",
+  component: ActionDropdown,
+  parameters: {
+    layout: "padded",
+    docs: {
+      description: {
+        component: '`import { ActionDropdown } from "@bigbinary/neetoui/v2";`',
+      },
+    },
+  },
+};
+
+const listItems = ["Option 1", "Option 2", "Option 3"];
+
+const Template = (args) => {
+  return (
+    <ActionDropdown {...args}>
+      {listItems.map((item, idx) => (
+        <li key={idx}>{item}</li>
+      ))}
+    </ActionDropdown>
+  );
+};;
+
+export const PrimaryDropdown = Template.bind({});
+PrimaryDropdown.args = {
+  style: "primary",
+  label: "Primary",
+};
+
+export const SecondaryDropdown = Template.bind({});
+SecondaryDropdown.args = {
+  style: "secondary",
+  label: "Secondary",
+};
+
+export const DisabledDropdown = Template.bind({});
+DisabledDropdown.args = {
+  label: "Disabled",
+  disabled: true,
+};
+
+export const LargePrimaryDropdown = Template.bind({});
+LargePrimaryDropdown.args = {
+  size: "large",
+  style: "primary",
+  label: "Large Primary",
+};
+
+export const LargeSecondaryDropdown = Template.bind({});
+LargeSecondaryDropdown.args = {
+  size: "large",
+  style: "secondary",
+  label: "Large Secondary",
+};
+
+export const LargeDisabledDropdown = Template.bind({});
+LargeDisabledDropdown.args = {
+  size: "large",
+  label: "Disabled",
+  disabled: true,
+};
