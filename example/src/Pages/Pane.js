785d54cc9e7d75d38ad918a37c32104fa6b98985
Ajmal Noushad <ajufhn@gmail.com>
2022-01-06 12:48:34 +0530

    Fixed useOnClickOutside logic
    - Fixed useOnClickOutside logic
    - Added story for Pane with Modal and Alert

--- example/src/Pages/Pane.js
+++ example/src/Pages/Pane.js
@@ -3,15 +3,17 @@ import { Check } from "@bigbinary/neeto-icons";
 import {
   Button,
   Pane,
+  Modal
 } from "../../../lib/components";
 import Header from "../Header";
 
 const Panes = () => {
   const [showPane, setShowPane] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="w-full">
       <Header title="Pane" />
-      
+
       <div className="p-6 space-y-6">
         <div className="w-1/2 space-y-8">
           <div className="flex flex-row items-center justify-start space-x-6">
@@ -19,7 +21,7 @@ const Panes = () => {
           </div>
         </div>
       </div>
-      
+
       <Pane isOpen={showPane} onClose={() => setShowPane(false)}>
         <Pane.Header>
           <h2>User Info</h2>
@@ -30,6 +32,7 @@ const Panes = () => {
           formless, fearless wonders! With Zandor, their leader, and his wife,
           Tara, and son, Dorno, they team up to protect their planet from sinister
           invaders! All-strong! All-brave! All-heroes! They're The Herculoids!
+          <Button label="Open Modal" onClick={() => setShowModal(true)} />
         </Pane.Body>
         <Pane.Footer className="flex space-x-4">
           <Button
@@ -44,6 +47,31 @@ const Panes = () => {
           />
         </Pane.Footer>
       </Pane>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+        <Modal.Header>
+          <h2>Add Label</h2>
+        </Modal.Header>
+        <Modal.Body>
+          Somewhere out in space live The Herculoids! Zok, the laser-ray dragon!
+          Igoo, the giant rock ape! Tundro, the tremendous! Gloop and Gleep, the
+          formless, fearless wonders! With Zandor, their leader, and his wife,
+          Tara, and son, Dorno, they team up to protect their planet from
+          sinister invaders! All-strong! All-brave! All-heroes! They're The
+          Herculoids!
+        </Modal.Body>
+        <Modal.Footer className="space-x-4">
+          <Button
+            icon={Check}
+            label="Continue"
+            onClick={() => setShowModal(false)}
+          />
+          <Button
+            style="text"
+            label="Cancel"
+            onClick={() => setShowModal(false)}
+          />
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
--- stories/Overlays/Pane.stories.jsx
+++ stories/Overlays/Pane.stories.jsx
@@ -3,6 +3,8 @@ import { Check } from "@bigbinary/neeto-icons";
 
 import Button from "../../lib/components/Button";
 import Pane from "../../lib/components/Pane";
+import Modal from "../../lib/components/Modal";
+import Alert from "../../lib/components/Alert";
 import Typography from "../../lib/components/Typography";
 
 export default {
@@ -74,6 +76,99 @@ export const Panes = ({ isOpen, onClose, ...args }) => {
           />
         </Pane.Footer>
       </Pane>
+    </div>
+  );
+};
+
+export const PaneWithModalAndAlert = ({ isOpen, onClose, ...args }) => {
+  const [showPane, setShowPane] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  useEffect(() => {
+    setShowPane(isOpen);
+  }, [isOpen]);
+  const handleClose = () => {
+    setShowPane(false);
+    onClose();
+  };
+  return (
+    <div className="w-full">
+      <div className="space-y-6">
+        <div className="w-1/2 space-y-8">
+          <div className="flex flex-row items-center justify-start space-x-6">
+            <Button label="Show Pane" onClick={() => setShowPane(true)} />
+          </div>
+        </div>
+      </div>
+
+      <Pane isOpen={showPane} onClose={handleClose} {...args}>
+        <Pane.Header>
+          <Typography style="h2" weight="semibold">
+            Typography
+          </Typography>
+        </Pane.Header>
+        <Pane.Body className="space-y-4">
+          <Typography style="body2">
+            Somewhere out in space live The Herculoids! Zok, the laser-ray
+            dragon! Igoo, the giant rock ape! Tundro, the tremendous! Gloop and
+            Gleep, the formless, fearless wonders! With Zandor, their leader,
+            and his wife, Tara, and son, Dorno, they team up to protect their
+            planet from sinister invaders! All-strong! All-brave! All-heroes!
+            They're The Herculoids!
+          </Typography>
+          <div className="flex space-x-6">
+            <Button label="Show Modal" onClick={() => setShowModal(true)} />
+            <Button label="Show Alert" onClick={() => setShowAlert(true)} />
+          </div>
+        </Pane.Body>
+        <Pane.Footer className="flex items-center space-x-2">
+          <Button
+            icon={Check}
+            size="large"
+            label="Continue"
+            onClick={() => setShowPane(false)}
+          />
+          <Button
+            style="text"
+            size="large"
+            label="Cancel"
+            onClick={() => setShowPane(false)}
+          />
+        </Pane.Footer>
+      </Pane>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+        <Modal.Header>
+          <h2>Add Label</h2>
+        </Modal.Header>
+        <Modal.Body>
+          Somewhere out in space live The Herculoids! Zok, the laser-ray dragon!
+          Igoo, the giant rock ape! Tundro, the tremendous! Gloop and Gleep, the
+          formless, fearless wonders! With Zandor, their leader, and his wife,
+          Tara, and son, Dorno, they team up to protect their planet from
+          sinister invaders! All-strong! All-brave! All-heroes! They're The
+          Herculoids!
+        </Modal.Body>
+        <Modal.Footer className="space-x-4">
+          <Button
+            icon={Check}
+            label="Continue"
+            onClick={() => setShowModal(false)}
+          />
+          <Button
+            style="text"
+            label="Cancel"
+            onClick={() => setShowModal(false)}
+          />
+        </Modal.Footer>
+      </Modal>
+      <Alert
+        isOpen={showAlert}
+        onClose={() => setShowAlert(false)}
+        onSubmit={() => setShowAlert(false)}
+        message="Are you sure you want to continue? All of your unsaved changes will be
+          lost."
+        title="You have unsaved changes!"
+      />
     </div>
   );
 };
