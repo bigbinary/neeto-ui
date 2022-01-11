458c239aa203537d3914caf3181c6e8671987344
Amal Dinesh <amalkdinesh@gmail.com>
2022-01-11 19:03:26 +0530

    Bundle optimized (#752)
    * Updated neeto icons and removed bundle files from code
    
    * Time picker component optimized
    
    * Stopped execution of bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const PeerDepsExternalsPlugin = require("@bigbinary/peer-deps-externals-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 
 module.exports = {
@@ -8,7 +10,11 @@ module.exports = {
     layouts: "./lib/components/layouts/index.js",
     formik: "./lib/components/formik/index.js",
   },
-  plugins: [new MiniCssExtractPlugin(), new PeerDepsExternalsPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new PeerDepsExternalsPlugin(),
+    // new BundleAnalyzerPlugin(),
+  ],
   module: {
     rules: [
       {
