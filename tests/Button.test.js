36ab7eea9a0378c34e52f29e917583a94c4fe189
Ajmal Noushad <ajufhn@gmail.com>
2022-03-03 13:39:12 +0400

    Improved Button test coverage
    Added tests for:
    - tooltip
    - disabled state
    - loading state
    - button icon

--- tests/Button.test.js
+++ tests/Button.test.js
@@ -15,12 +15,39 @@ describe("Button", () => {
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
-  it("should not call onClick when disabled", () => {
+  it("should not call onClick on button click when disabled", () => {
     const onClick = jest.fn();
     const { getByText } = render(
       <Button label="Button" onClick={onClick} disabled />
     );
     fireEvent.click(getByText("Button"));
     expect(onClick).toHaveBeenCalledTimes(0);
+  });
+
+  it("should not call onClick on button click when loading", () => {
+    const onClick = jest.fn();
+    const { getByText } = render(
+      <Button label="Button" onClick={onClick} loading />
+    );
+    fireEvent.click(getByText("Button"));
+    expect(onClick).toHaveBeenCalledTimes(0);
+  });
+
+  it("should show tooltip when button is hovered", () => {
+    const { getByText } = render(
+      <Button label="Button" tooltipProps={{ content: "Tooltip" }} />
+    );
+    fireEvent.mouseEnter(getByText("Button"), { bubbles: true });
+    expect(getByText("Tooltip")).toBeInTheDocument();
+  });
+
+  it("should show icon when icon string is provided", () => {
+    const { getByTestId } = render(<Button icon="check" />);
+    expect(getByTestId("class-icon")).toBeInTheDocument();
+  });
+
+  it("should show icon when icon component is provided", () => {
+    const { getByTestId } = render(<Button icon={() => <svg data-testid="svg-icon" />} />);
+    expect(getByTestId("svg-icon")).toBeInTheDocument();
   });
 });
