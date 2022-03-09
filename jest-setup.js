785bf5ca2a7f35536e2fbf7fdad72d4d75985d34
Ajmal Noushad <ajufhn@gmail.com>
2022-03-09 12:13:48 +0400

    Update jest-setup.js

--- jest-setup.js
+++ jest-setup.js
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom";
 
 
-// Refer: https://github.com/framer/motion/issues/204
+// Fixes element.getTotalLength is not a function. Refer: https://github.com/framer/motion/issues/204
 if (!SVGElement.prototype.getTotalLength) {
   SVGElement.prototype.getTotalLength = () => 1;
 }
