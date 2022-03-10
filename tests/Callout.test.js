a6e1c326347fc2b19ecb0047e35a70070323390a
AJIL-PAUL <ajilpaul.268@gmail.com>
2022-03-10 19:05:26 +0530

    Updated unit test for Callout

--- tests/Callout.test.js
+++ tests/Callout.test.js
@@ -5,17 +5,16 @@ import { render } from "@testing-library/react";
 
 describe("Callout", () => {
   it("should render without error", () => {
-    const { getByText } = render(<Callout>Tesing Callout</Callout>);
+    const { getByText } = render(
+      <Callout>
+        <p>Tesing Callout</p>
+      </Callout>
+    );
     expect(getByText("Tesing Callout")).toBeInTheDocument();
   });
 
   it("should show icon when icon component is provided", () => {
     const { getByTestId } = render(<Callout icon={Check} />);
     expect(getByTestId("callout-icon")).toBeInTheDocument();
-  });
-
-  it("should not break component if icon is passed but nil", () => {
-    const { container } = render(<Callout icon={null} />);
-    expect(container).toMatchSnapshot();
   });
 });
