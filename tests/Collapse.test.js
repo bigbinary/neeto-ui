c4966bf65a0920910827f7873c1a7f1a4fd8956b
Ajmal Noushad <ajufhn@gmail.com>
2022-03-10 12:49:00 +0400

    Added tests for Collapse (#950)

---
+++ tests/Collapse.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Collapse from "../lib/components/Collapse";
+
+describe("Collapse", () => {
+  it("should render without error", () => {
+    const { getByText } = render(<Collapse open>
+      <p>Content</p>
+    </Collapse>);
+    expect(getByText("Content")).toBeInTheDocument();
+  });
+
+  it("should not display content when collapsed", () => {
+    const { queryByText } = render(<Collapse>
+      <p>Content</p>
+    </Collapse>);
+    expect(queryByText("Content")).not.toBeInTheDocument();
+  });
+});
