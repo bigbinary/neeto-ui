b7329ba12931d6e8b40d2197e5c904982db44768
Prasidh Prabudhan <prasidhprabudhan123@gmail.com>
2022-03-10 15:14:34 +0530

    Added test for heading and component overriding default tag

--- tests/Typography.test.js
+++ tests/Typography.test.js
@@ -7,4 +7,14 @@ describe("Typography", () => {
     const { getByText } = render(<Typography style="body1">Typography</Typography>);
     expect(getByText("Typography")).toBeInTheDocument();
   });
+
+  it("should render a heading when style is of heading type", () => {
+    const { getByRole } = render(<Typography style="h1">Typography</Typography>);
+    expect(getByRole("heading", { level: 1 })).toBeInTheDocument();
+  });
+
+  it("should override default tag when component prop is given", () => {
+    const { getByRole } = render(<Typography style="h1" component="h2">Typography</Typography>);
+    expect(getByRole("heading", { level: 2 })).toBeInTheDocument();
+  });
 });
