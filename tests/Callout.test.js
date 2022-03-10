1e22883c86734111c3572884f0c5984f48432f79
Ajil Paul <36255991+AJIL-PAUL@users.noreply.github.com>
2022-03-10 23:49:36 +0530

    Added unit test for Callout (#957)
    * Added unit test for Callout
    
    * Updated unit test for Callout

---
+++ tests/Callout.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Callout } from "../lib/components";
+import { Check } from "@bigbinary/neeto-icons";
+import { render } from "@testing-library/react";
+
+describe("Callout", () => {
+  it("should render without error", () => {
+    const { getByText } = render(
+      <Callout>
+        <p>Tesing Callout</p>
+      </Callout>
+    );
+    expect(getByText("Tesing Callout")).toBeInTheDocument();
+  });
+
+  it("should show icon when icon component is provided", () => {
+    const { getByTestId } = render(<Callout icon={Check} />);
+    expect(getByTestId("callout-icon")).toBeInTheDocument();
+  });
+});
