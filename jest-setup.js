b333b9f5124796ea2df0d9ee99d02475a97daa53
Ajmal Noushad <ajufhn@gmail.com>
2022-03-10 12:05:11 +0400

    Added tests for Alert component (#921)

--- jest-setup.js
+++ jest-setup.js
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom";
+
+
+// Fixes element.getTotalLength is not a function. Refer: https://github.com/framer/motion/issues/204
+if (!SVGElement.prototype.getTotalLength) {
+  SVGElement.prototype.getTotalLength = () => 1;
+}
