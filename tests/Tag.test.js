00477b8468d15024340632e02cc29c0b79098fa6
AJIL-PAUL <ajilpaul.268@gmail.com>
2022-03-10 19:08:07 +0530

    Removed style testing and snap from Tag unit test

--- tests/Tag.test.js
+++ tests/Tag.test.js
@@ -13,11 +13,6 @@ describe("Tag", () => {
     expect(getByTestId("class-icon")).toBeInTheDocument();
   });
 
-  it("should not break component if icon is passed but nil", () => {
-    const { container } = render(<Tag icon={null} />);
-    expect(container).toMatchSnapshot();
-  });
-
   it("should show indicator when indicatorColor is provided", () => {
     const { getByTestId } = render(<Tag indicatorColor="green" />);
     expect(getByTestId("tag-indicator")).toBeInTheDocument();
@@ -40,33 +35,5 @@ describe("Tag", () => {
     const { getByTestId } = render(<Tag onClose={onClose} disabled />);
     fireEvent.click(getByTestId("tag-close-button"));
     expect(onClose).toHaveBeenCalledTimes(0);
-  });
-
-  it("should contain neeto-ui-tag--size-large class if size is large", () => {
-    const { getByTestId } = render(<Tag size="large" />);
-    expect(getByTestId("tag-container")).toHaveClass(
-      "neeto-ui-tag--size-large"
-    );
-  });
-
-  it("should contain neeto-ui-tag--size-small class if size is small", () => {
-    const { getByTestId } = render(<Tag size="small" />);
-    expect(getByTestId("tag-container")).toHaveClass(
-      "neeto-ui-tag--size-small"
-    );
-  });
-
-  it("should contain neeto-ui-tag--style-outline class if style is outline", () => {
-    const { getByTestId } = render(<Tag style="outline" />);
-    expect(getByTestId("tag-container")).toHaveClass(
-      "neeto-ui-tag--style-outline"
-    );
-  });
-
-  it("should contain neeto-ui-tag--style-solid class if style is solid", () => {
-    const { getByTestId } = render(<Tag style="solid" />);
-    expect(getByTestId("tag-container")).toHaveClass(
-      "neeto-ui-tag--style-solid"
-    );
   });
 });
