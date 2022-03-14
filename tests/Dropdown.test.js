98de38662cd5ed4dc0d2699d23cea23c68670b5f
Ajmal Noushad <ajufhn@gmail.com>
2022-03-14 20:42:55 +0400

    Dropdown with Tippy (#892)

--- tests/Dropdown.test.js
+++ tests/Dropdown.test.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Dropdown } from "../lib/components";
-import { fireEvent, render, screen } from "@testing-library/react";
+import {  render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-const options = ["option 1", "option 2"].map(option => <li key={option}>{option}</li>)
+const options = ["option 1", "option 2"].map((option) => (
+  <li key={option}>{option}</li>
+));
 
 describe("Dropdown", () => {
   it("should render without error", () => {
-    const { getByText } = render(
-      <Dropdown
-        label="Dropdown"
-      />)
+    const { getByText } = render(<Dropdown label="Dropdown" />);
     expect(getByText("Dropdown")).toBeInTheDocument();
   });
 
@@ -23,152 +22,123 @@ describe("Dropdown", () => {
 
   it("should render all the options when clicking dropdown without error", async () => {
     const { getByText } = render(
-      <Dropdown label="Dropdown" >
-        {options}
-      </Dropdown>
-    )
-    userEvent.click(getByText("Dropdown"))
-    const listItems = await screen.findAllByText(/option/i)
+      <Dropdown label="Dropdown">{options}</Dropdown>
+    );
+    userEvent.click(getByText("Dropdown"));
+    const listItems = await screen.findAllByText(/option/i);
     expect(listItems.length).toBe(2);
   });
 
   it("should render all the options if isOpen is true", async () => {
     const { findAllByText } = render(
       <Dropdown label="Dropdown" isOpen>
-       {options}
+        {options}
       </Dropdown>
-    )
-    const listItems = await findAllByText(/option/i)
+    );
+    const listItems = await findAllByText(/option/i);
     expect(listItems.length).toBe(2);
   });
 
   it("should be disabled if disabled is true", () => {
-    const { getByRole } = render(
-      <Dropdown label="Dropdown" disabled />
-    );
+    const { getByRole } = render(<Dropdown label="Dropdown" disabled />);
     expect(getByRole("button")).toBeDisabled();
   });
 
   it("should close the dropdown on select if closeOnSelect is true", () => {
     const { getByText } = render(
-      <Dropdown label="Dropdown" closeOnSelect >
+      <Dropdown label="Dropdown" closeOnSelect>
         {options}
       </Dropdown>
-    )
-    userEvent.click(getByText("Dropdown"))
-    const listItem = getByText("option 1")
-    userEvent.click(listItem)
+    );
+    userEvent.click(getByText("Dropdown"));
+    const listItem = getByText("option 1");
+    userEvent.click(listItem);
     expect(screen.queryAllByRole("listitem")).toHaveLength(0);
   });
 
   it("should not close the dropdown on select if closeOnSelect is false", async () => {
     const { getByText } = render(
-      <Dropdown label="Dropdown" closeOnSelect={false} >
+      <Dropdown label="Dropdown" closeOnSelect={false}>
         {options}
       </Dropdown>
-    )
-    userEvent.click(getByText("Dropdown"))
-    const listItem = getByText("option 1")
-    userEvent.click(listItem)
-    const listItems = await screen.findAllByRole("listitem")
+    );
+    userEvent.click(getByText("Dropdown"));
+    const listItem = getByText("option 1");
+    userEvent.click(listItem);
+    const listItems = await screen.findAllByRole("listitem");
     expect(listItems).toHaveLength(2);
   });
 
   it("should close the dropdown when Esc key is pressed if closeOnEsc is true", () => {
     const { getByText } = render(
-      <Dropdown label="Dropdown" closeOnEsc >
+      <Dropdown label="Dropdown" closeOnEsc>
         {options}
       </Dropdown>
-    )
-    userEvent.click(getByText("Dropdown"))
+    );
+    userEvent.click(getByText("Dropdown"));
     userEvent.keyboard("{esc}");
     expect(screen.queryAllByRole("listitem")).toHaveLength(0);
   });
 
   it("should not close the dropdown when Esc key is pressed if closeOnEsc is false", async () => {
     const { getByText } = render(
-      <Dropdown label="Dropdown" closeOnEsc={false} >
+      <Dropdown label="Dropdown" closeOnEsc={false}>
         {options}
       </Dropdown>
-    )
-    userEvent.click(getByText("Dropdown"))
+    );
+    userEvent.click(getByText("Dropdown"));
     userEvent.keyboard("{esc}");
-    const listItems = await screen.findAllByRole("listitem")
+    const listItems = await screen.findAllByRole("listitem");
     expect(listItems).toHaveLength(2);
   });
 
   it("should close dropdown on clicking outside if closeOnOutsideClick is true", () => {
     const { getByText } = render(
-      <Dropdown label="Dropdown" closeOnOutsideClick >
+      <Dropdown label="Dropdown" closeOnOutsideClick>
         {options}
       </Dropdown>
-    )
-    userEvent.click(getByText("Dropdown"))
+    );
+    userEvent.click(getByText("Dropdown"));
     userEvent.click(document.body);
     expect(screen.queryAllByRole("listitem")).toHaveLength(0);
   });
 
   it("should not close on clicking outside if closeOnOutsideClick is false", async () => {
     const { getByText } = render(
-      <Dropdown label="Dropdown" closeOnOutsideClick={false} >
+      <Dropdown label="Dropdown" closeOnOutsideClick={false}>
         {options}
       </Dropdown>
-    )
-    userEvent.click(getByText("Dropdown"))
+    );
+    userEvent.click(getByText("Dropdown"));
     userEvent.click(document.body);
-    const listItems = await screen.findAllByRole("listitem")
+    const listItems = await screen.findAllByRole("listitem");
     expect(listItems).toHaveLength(2);
   });
 
   it("should open another dropdown on click trigger when it is multilevel ", async () => {
     const { getByText } = render(
-      <Dropdown label="Dropdown" isMultiLevel isOpen>
+      <Dropdown label="Dropdown" isOpen>
         {options}
-        <Dropdown
-          customTarget={<li>Another Dropdown</li>}
-        >
-          {options}
-        </Dropdown>
+        <Dropdown customTarget={<li>Another Dropdown</li>}>{options}</Dropdown>
       </Dropdown>
-    )
-    userEvent.click(getByText("Another Dropdown"))
-    const listItems =  await screen.findAllByRole("listitem")
+    );
+    userEvent.click(getByText("Another Dropdown"));
+    const listItems = await screen.findAllByRole("listitem");
     expect(listItems).toHaveLength(5);
   });
 
-  it("should open another dropdown on hover trigger when it is multilevel  ", async () => {
+  it("should open another dropdown on hover trigger when it is multilevel", async () => {
     const { getByText } = render(
-      <Dropdown label="Dropdown" isMultiLevel isOpen>
+      <Dropdown label="Dropdown" isOpen>
         {options}
-        <Dropdown
-          customTarget={<li>Another Dropdown</li>}
-          trigger="hover"
-        >
+        <Dropdown customTarget={<li>Another Dropdown</li>} trigger="hover">
           {options}
         </Dropdown>
       </Dropdown>
-    )
-    userEvent.hover(getByText("Another Dropdown"))
-    const listItems =  await screen.findAllByRole("listitem")
+    );
+    userEvent.hover(getByText("Another Dropdown"));
+    const listItems = await screen.findAllByRole("listitem");
     expect(listItems).toHaveLength(5);
-  });
-
-  it("should close the multilevel dropdown when mouse leaves", async () => {
-    const { getByText } = render(
-      <Dropdown label="Dropdown" isMultiLevel isOpen>
-        {options}
-        <Dropdown
-          customTarget={<li>Another Dropdown</li>}
-          trigger="hover"
-          isOpen
-        >
-          {options}
-        </Dropdown>
-      </Dropdown>
-    )
-    userEvent.unhover(getByText("Another Dropdown"))
-    const listItems =  await screen.findAllByRole("listitem")
-    expect(listItems).toHaveLength(3);
   });
 
   it("should call onClose when Dropdown is closed", () => {
@@ -177,8 +147,8 @@ describe("Dropdown", () => {
       <Dropdown label="Dropdown" closeOnOutsideClick onClose={onClose}>
         {options}
       </Dropdown>
-    )
-    userEvent.click(getByText("Dropdown"))
+    );
+    userEvent.click(getByText("Dropdown"));
     userEvent.click(document.body);
     expect(onClose).toHaveBeenCalledTimes(1);
   });
