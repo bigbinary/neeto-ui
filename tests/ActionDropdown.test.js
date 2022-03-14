d5c96a268ef4509f144c9bb1e6f1bbcf26a0ef7d
AJIL-PAUL <ajilpaul.268@gmail.com>
2022-03-14 15:01:48 +0530

    Added unit test for ActionDropdown

--- tests/ActionDropdown.test.js
+++ tests/ActionDropdown.test.js
@@ -1,28 +1,61 @@
 import React from "react";
 import { ActionDropdown } from "../lib/components";
-import { render, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("ActionDropdown", () => {
   it("should render without error", () => {
-    const { getByText } = render(<ActionDropdown label="ActionDropdown" />);
-    expect(getByText("ActionDropdown")).toBeInTheDocument();
+    render(
+      <ActionDropdown label="ActionDropdown">
+        <li>Option 1</li>
+        <li>Option 2</li>
+      </ActionDropdown>
+    );
+    expect(screen.getByText("ActionDropdown")).toBeInTheDocument();
   });
 
   it("should call onClick on button click", () => {
     const onClick = jest.fn();
-    const { getByText } = render(
-      <ActionDropdown label="ActionDropdown" onClick={onClick} />
+    render(
+      <ActionDropdown label="ActionDropdown" onClick={onClick}>
+        <li>Option 1</li>
+        <li>Option 2</li>
+      </ActionDropdown>
     );
-    fireEvent.click(getByText("ActionDropdown"));
+    const button = screen.getByTestId("action-dropdown-btn");
+    userEvent.click(button);
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
   it("should not call onClick on button click when disabled", () => {
     const onClick = jest.fn();
-    const { getByText } = render(
-      <ActionDropdown label="ActionDropdown" onClick={onClick} disabled />
+    render(
+      <ActionDropdown label="ActionDropdown" disabled>
+        <li>Option 1</li>
+        <li>Option 2</li>
+      </ActionDropdown>
     );
-    fireEvent.click(getByText("ActionDropdown"));
+    userEvent.click(screen.getByText("ActionDropdown"));
     expect(onClick).toHaveBeenCalledTimes(0);
+  });
+
+  it("should show options on clicking dropdown", async () => {
+    render(
+      <ActionDropdown
+        dropdownProps={{
+          buttonProps: {
+            "data-testid": "action-dropdown-dropdown",
+          },
+        }}
+        label="ActionDropdown"
+      >
+        <li>Option 1</li>
+        <li>Option 2</li>
+      </ActionDropdown>
+    );
+    const dropdown = screen.getByTestId("action-dropdown-dropdown");
+    userEvent.click(dropdown);
+    expect(await screen.findByText("Option 1")).toBeInTheDocument();
+    expect(await screen.findByText("Option 2")).toBeInTheDocument();
   });
 });
