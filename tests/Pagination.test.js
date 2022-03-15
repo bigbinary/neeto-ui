0550189ff7a28e610c0281e3a1fc76f2a9df5a7f
Prasidh Prabudhan <prasidhprabudhan123@gmail.com>
2022-03-15 20:09:46 +0530

    Added more tests for pagination

--- tests/Pagination.test.js
+++ tests/Pagination.test.js
@@ -4,7 +4,7 @@ import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 const currentPage = 3
-const count = 500
+const count = 1000
 const pageSize = 100
 
 describe("Pagination", () => {
@@ -47,6 +47,20 @@ describe("Pagination", () => {
     expect(navigate).toHaveBeenCalledTimes(1);
   });
 
+  it("should invoke navigate callback when any page number is clicked.", () => {
+    const navigate = jest.fn()
+    const { getByText } = render(
+      <Pagination
+        count={count}
+        pageNo={currentPage}
+        pageSize={pageSize}
+        navigate={navigate}
+      />
+    );
+    userEvent.click(getByText("4"));
+    expect(navigate).toHaveBeenCalledTimes(1);
+  });
+
   it("should not render anything when currentPage is 0", () => {
     const { container } = render(
       <Pagination
@@ -56,5 +70,53 @@ describe("Pagination", () => {
       />
     )
     expect(container.firstChild).toBeNull();
+  });
+
+  it("should render only left dots when there are more than 1 page between the extremes of left sibling and the page limit", () => {
+    const { getByTestId } = render(
+      <Pagination
+        count={count}
+        pageNo={7}
+        pageSize={pageSize}
+      />
+    )
+    const dots = getByTestId("dots")
+    expect(dots).toBeInTheDocument()
+  });
+
+  it("should render only right dots when there are more than 1 page between the extremes of right sibling and the page limit", () => {
+    const { getByTestId } = render(
+      <Pagination
+        count={count}
+        pageNo={3}
+        pageSize={pageSize}
+      />
+    )
+    const dots = getByTestId("dots")
+    expect(dots).toBeInTheDocument()
+  });
+
+  it("should render both left and right dots when there are more than 1 page between the extremes of both siblings and the page limit", () => {
+    const { getAllByTestId } = render(
+      <Pagination
+        count={count}
+        pageNo={5}
+        pageSize={pageSize}
+      />
+    )
+    const dots = getAllByTestId("dots")
+    expect(dots.length).toBe(2)
+  });
+
+  it("should not render any dots if the number of pages is less than the page numbers we want to show ", () => {
+    const { queryByTestId } = render(
+      <Pagination
+        count={500}
+        pageNo={currentPage}
+        pageSize={pageSize}
+      />
+    )
+    const dots = queryByTestId("dots")
+    expect(dots).toBeNull()
   });
 });
