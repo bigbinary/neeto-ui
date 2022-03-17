198d9e624f0cfe9e7458e8c63dea8951c6665893
Ajmal Noushad <ajufhn@gmail.com>
2022-03-17 18:42:26 +0530

    Fixed Avatar initials not showing on image error
    - Added tests to cover indicator
    - Added image load error

--- tests/Avatar.test.js
+++ tests/Avatar.test.js
@@ -1,30 +1,59 @@
 import React from "react";
 import { Avatar } from "../lib/components";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("Avatar", () => {
   it("should render without error", () => {
-    const { getByRole } = render(<Avatar />);
-    const avatarImg = getByRole('img');
+    render(<Avatar />);
+    const avatarImg = screen.getByRole("img");
+    fireEvent.load(avatarImg);
     expect(avatarImg).toBeInTheDocument();
   });
 
   it("should render initials  when username is given", () => {
-    const { getByText } = render(<Avatar user={{ name: 'neeto UI' }} />);
-    expect(getByText("NU")).toBeInTheDocument();
+    render(<Avatar user={{ name: "neeto UI" }} />);
+    expect(screen.getByText("NU")).toBeInTheDocument();
   });
 
   it("should not render initials  when username is not given", () => {
-    const { queryByTestId } = render(<Avatar />);
-    expect(queryByTestId("initials")).toBeNull();
+    render(<Avatar />);
+    expect(screen.queryByTestId("initials")).not.toBeInTheDocument();
   });
 
   it("should call onClick when the avatar is clicked", () => {
-    const onClick = jest.fn()
-    const { getByText } = render(<Avatar user={{ name: 'neeto UI' }} onClick={onClick} />);
-    const avatar = getByText("NU")
+    const onClick = jest.fn();
+    render(
+      <Avatar user={{ name: "neeto UI" }} onClick={onClick} />
+    );
+    const avatar = screen.getByText("NU");
     userEvent.click(avatar);
-    expect(onClick).toBeCalledTimes(1)
+    expect(onClick).toBeCalledTimes(1);
+  });
+
+  it("should render initials when image is invalid", () => {
+    render(
+      <Avatar
+        user={{
+          name: "John Doe",
+          imageUrl: "not-found.png",
+        }}
+      />
+    );
+    fireEvent.error(screen.getByRole("img"));
+    expect(screen.getByText("JD")).toBeInTheDocument();
+  });
+
+  it("should render indicator when status is defined", () => {
+    render(
+      <Avatar
+        user={{
+          name: "John Doe",
+          imageUrl: "not-found.png",
+        }}
+        status="online"
+      />
+    );
+    expect(screen.getByTestId("indicator")).toBeInTheDocument();
   });
 });
