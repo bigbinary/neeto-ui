a20a782d560bbb0f36bcd90fe5dd0e61c347e455
Prasidh Prabudhan <48003387+prasidhprabudhan@users.noreply.github.com>
2022-03-24 18:36:04 +0530

    Added tests for table (#1008)

--- jest-setup.js
+++ jest-setup.js
@@ -1,7 +1,22 @@
 import "@testing-library/jest-dom";
 
-
 // Fixes element.getTotalLength is not a function. Refer: https://github.com/framer/motion/issues/204
 if (!SVGElement.prototype.getTotalLength) {
   SVGElement.prototype.getTotalLength = () => 1;
 }
+
+//Fixes ReferenceError: ResizeObserver is not defined
+global.ResizeObserver = require('resize-observer-polyfill')
+
+//Fixes TypeError: window.matchMedia is not a function
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+    }
+  )),
+});
