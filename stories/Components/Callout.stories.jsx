ee104399ac7e48e71254c972e8404c5d3b7b85d7
Praveen Murali <itspraveenmurali@gmail.com>
2022-03-30 17:38:11 +0530

    Added All Variants story for Callout

--- stories/Components/Callout.stories.jsx
+++ stories/Components/Callout.stories.jsx
@@ -44,3 +44,16 @@ export const WarningCallout = () => {
 export const DangerCallout = () => {
   return <Callout style="danger" icon={CloseCircle}>This is a danger callout!</Callout>;
 };
+
+export const AllVariants = () => {
+  return <div className="flex flex-col gap-4">
+    <Callout style="success" icon={CheckCircle}>This is a success callout!</Callout>
+    <Callout style="info" icon={Info}>
+      This is an info callout!
+    </Callout>
+    <Callout style="warning" icon={Warning}>
+      This is a warning callout!
+    </Callout>
+    <Callout style="danger" icon={CloseCircle}>This is a danger callout!</Callout>
+  </div>;
+};
