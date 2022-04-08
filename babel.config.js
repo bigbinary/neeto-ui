efa7499d98658d461992d0a99785e0b6e1a91b5a
Ajmal Noushad <ajufhn@gmail.com>
2022-04-08 18:13:13 +0530

    Added tests for formik Input (#1061)

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,12 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: ["@babel/preset-env", "@babel/preset-react"],
+    env: {
+      test: {
+        plugins: [
+          "@babel/plugin-transform-runtime"
+        ]
+      }
+    }
   };
 };
---
+++ tests/formik/Input.test.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Formik, Form } from "formik";
+import { Input } from "../../lib/components/formik";
+import * as yup from "yup";
+
+
+const TestForm = ({ onSubmit }) => {
+  const handleSubmit = values => {
+    onSubmit(values);
+  };
+  return <>
+    <h1>Sign Up</h1>
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+      }}
+      validationSchema={yup.object().shape({
+        firstName: yup.string().required("First name is required"),
+        lastName: yup.string().required("Last name is required"),
+        email: yup.string().email("Invalid email address").required("Email address is required"),
+      })}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <Input name="firstName" label="First Name" />
+        <Input name="lastName" label="Last Name" />
+        <Input name="email" label="Email" type="email" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  </>;
+};
+
+describe("formik/Input", () => {
+  it("should render without error", () => {
+    render(
+      <Formik initialValues={{}} onSubmit={() => {}}>
+        <Form>
+          <Input label="Input label" name="test" />
+        </Form>
+      </Formik>
+    );
+    expect(screen.getByLabelText("Input label")).toBeInTheDocument();
+  });
+
+  it("should return entered values when used inside a formik form", async () => {
+    const onSubmit = jest.fn();
+    render(<TestForm onSubmit={onSubmit} />);
+    userEvent.type(screen.getByLabelText("First Name"), "John");
+    userEvent.type(screen.getByLabelText("Last Name"), "Doe");
+    userEvent.type(screen.getByLabelText("Email"), "john.doe@email.com");
+    userEvent.click(screen.getByText("Submit"));
+    await waitFor(() => expect(onSubmit).toHaveBeenCalledWith({
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@email.com"
+    }));
+  });
+
+  it("should display validation error when invalid input is provided", async () => {
+    const onSubmit = jest.fn();
+    render(<TestForm onSubmit={onSubmit} />);
+    userEvent.type(screen.getByLabelText("Email"), "john.doemail.com");
+    userEvent.click(screen.getByText("Submit"));
+    await waitFor(() => expect(screen.getByText("Invalid email address")).toBeInTheDocument());
+  });
+});
