7ddebf8a2328a620d3531cb229c222c05802a16f
Neha Jamwal <nehajamwal301995@gmail.com>
2022-04-13 22:55:31 +0530

    Minor modifications

--- tests/formik/Radio.test.js
+++ tests/formik/Radio.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  fireEvent,
   render,
   screen,
   waitFor,
@@ -10,18 +9,12 @@ import { Formik, Form } from "formik";
 import { Radio } from "../../lib/components/formik";
 import * as yup from "yup";
 
-const options = {
-  option1: { label: "Option 1", value: "option1" },
-  option2: { label: "Option 1", value: "option1" },
-};
-
 const TestRadioForm = ({ onSubmit }) => {
   const handleSubmit = (values) => {
     onSubmit(values);
   };
   return (
     <>
-      <h1>Sign Up</h1>
       <Formik
         initialValues={{
           options: "",
@@ -51,7 +44,7 @@ describe("formik/Radio", () => {
       render(
       <Formik initialValues={{}} onSubmit={() => {}}>
         <Form>
-          <Radio name="Radio"  onChange={() => {}} value="">
+          <Radio name="Radio">
             <Radio.Item label="Option 1" name="options" value="option1" />
           </Radio>
         </Form>
@@ -60,17 +53,13 @@ describe("formik/Radio", () => {
     expect(screen.getByLabelText("Option 1")).toBeInTheDocument();
   });
 
-  it("should return selected value when used inside a formik form", async () => {
+  it("should return selected option value when used inside a formik form", async () => {
     const onSubmit = jest.fn();
    const {getAllByRole} = render(<TestRadioForm onSubmit={onSubmit} />);
     const radio = getAllByRole("radio");
     userEvent.click(radio[0]);
-    expect(radio[0]).toBeChecked();
-    expect(radio[1]).not.toBeChecked();
     userEvent.click(radio[1]);
-    expect(radio[0]).not.toBeChecked();
-    expect(radio[1]).toBeChecked();
-    fireEvent.click(screen.getByText("Submit"));
+    userEvent.click(screen.getByText("Submit"));
     await waitFor(() =>
       expect(onSubmit).toHaveBeenCalledWith({options: 'option2'})
     );
@@ -78,10 +67,7 @@ describe("formik/Radio", () => {
 
   it("should display error when no option is selected", async () => {
     const onSubmit = jest.fn();
-    const {getAllByRole} =render(<TestRadioForm onSubmit={onSubmit} />);
-    const radio = getAllByRole("radio");
-    expect(radio[0]).not.toBeChecked();
-    expect(radio[1]).not.toBeChecked();
+    render(<TestRadioForm onSubmit={onSubmit} />);
     userEvent.click(screen.getByText("Submit"));
     await waitFor(() => expect(screen.getByText("selecting one option is required")).toBeInTheDocument());
   });
