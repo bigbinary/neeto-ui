5128299e0513c2e7e73830eab27e3b2d8cf8b7fa
Mohit Srivastava <mohitsrivastava67@gmail.com>
2022-04-13 18:36:51 +0530

    Added tests for formik/Select component

--- tests/formik/Select.test.js
+++ tests/formik/Select.test.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 import { Formik, Form } from "formik";
 import { Select as FormikSelect } from "../../lib/components/formik";
+import userEvent from "@testing-library/user-event";
 
 const SELECT_OPTIONS = [
   {
@@ -27,6 +28,7 @@ const SelectTest = ({ onSubmit }) => {
           className="mb-8"
           data-testid="formik-select"
         />
+        <button type="submit">Submit</button>
       </Form>
     </Formik>
   );
@@ -47,5 +49,58 @@ describe("formik/Select", () => {
     );
 
     expect(getByLabelText("Formik Select")).toBeInTheDocument();
+  });
+
+  it("should show available options when clicked", () => {
+    render(
+      <Formik initialValues={{}} onSubmit={() => {}}>
+        <Form>
+          <FormikSelect
+            name="formikSelect"
+            label="Formik Select"
+            options={SELECT_OPTIONS}
+          />
+        </Form>
+      </Formik>
+    );
+
+    const selectInput = screen.getByLabelText("Formik Select").closest("input");
+    userEvent.click(selectInput);
+    for (const { label } of SELECT_OPTIONS) {
+      expect(screen.getByText(label)).toBeInTheDocument();
+    }
+  });
+
+  it("should should show no options if the input deosn't match any options", () => {
+    render(
+      <Formik initialValues={{}} onSubmit={() => {}}>
+        <Form>
+          <FormikSelect
+            name="formikSelect"
+            label="Formik Select"
+            options={SELECT_OPTIONS}
+          />
+        </Form>
+      </Formik>
+    );
+
+    const selectInput = screen.getByLabelText("Formik Select").closest("input");
+    userEvent.type(selectInput, "Invalid Option");
+    expect(screen.getByText(/No options/i)).toBeInTheDocument();
+  });
+
+  it("should submit with the chosen option", async () => {
+    const onSubmit = jest.fn();
+    render(<SelectTest onSubmit={onSubmit} />);
+
+    const selectInput = screen.getByLabelText("Formik Select").closest("input");
+    userEvent.click(selectInput);
+    userEvent.click(screen.getByText(SELECT_OPTIONS[0].label));
+    userEvent.click(screen.getByText("Submit"));
+    await waitFor(() =>
+      expect(onSubmit).toHaveBeenCalledWith({
+        formikSelect: SELECT_OPTIONS[0],
+      })
+    );
   });
 });
