79485e81e5f80734c35e00fc2abaaef4aa108114
Neha Jamwal <58418171+nehajamwalR@users.noreply.github.com>
2022-04-18 17:26:25 +0530

    Adds Tests for formik checkbox (#1091)

---
+++ tests/formik/checkbox.test.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Formik, Form } from "formik";
+import { Checkbox } from "../../lib/components/formik";
+import * as yup from "yup";
+
+const TestCheckboxForm = ({ onSubmit }) => {
+  const handleSubmit = (values) => {
+    onSubmit(values);
+  };
+  return (
+    <>
+      <Formik
+        initialValues={{
+          formikCheckbox: false,
+        }}
+        validationSchema={yup.object().shape({
+          formikCheckbox: yup.boolean().oneOf([true],"Checking the formik checkbox is required"),
+        })}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <Checkbox name="formikCheckbox" label="Formik Checkbox" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
+
+describe("formik/Checkbox", () => {
+  it("should render without error", () => {
+    render(
+      <Formik initialValues={{}} onSubmit={() => {}}>
+        <Form>
+          <Checkbox name="checkbox" label="Checkbox" />
+        </Form>
+      </Formik>
+    );
+    expect(screen.getByLabelText("Checkbox")).toBeInTheDocument();
+  });
+
+  it("should return checked when used inside a formik form", async () => {
+    const onSubmit = jest.fn();
+    const { getByRole } = render(<TestCheckboxForm onSubmit={onSubmit} />);
+    const checkbox = getByRole("checkbox");
+    userEvent.click(checkbox);
+    userEvent.click(screen.getByText("Submit"));
+    await waitFor(() =>
+      expect(onSubmit).toHaveBeenCalledWith({formikCheckbox : true })
+    );
+  });
+
+  it("should display error when checkbox is not checked", async () => {
+    const onSubmit = jest.fn();
+    render(<TestCheckboxForm onSubmit={onSubmit} />);
+    userEvent.click(screen.getByText("Submit"));
+    await waitFor(() =>
+      expect(
+        screen.getByText("Checking the formik checkbox is required")
+      ).toBeInTheDocument()
+    );
+  });
+});
