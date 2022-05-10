c321c1e7a9c0c39c6d540f1e7ce782413fafb104
amaldinesh7 <amalkdinesh@gmail.com>
2022-05-10 15:47:02 +0530

    Fixed tags molecule stories

--- stories/Molecules/Tags.stories.jsx
+++ stories/Molecules/Tags.stories.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Tags from '../../lib/Molecules/Tags';
+import Tags from "../../lib/Molecules/Tags";
 
 export default {
   title: "Molecules/Tags",
@@ -13,74 +13,84 @@ export default {
       },
     },
   },
-}
+};
 
 const Template = (args) => {
-  const [allTags,setAllTags] = useState(args.allTags);
+  const [allTags, setAllTags] = useState(args.allTags);
   const [selectedTags, setSelectedTags] = useState(args.selectedTags);
 
-  const onTagCreate = tag => {
+  const onTagCreate = (tag) => {
     setAllTags([...allTags, { label: tag, value: tag }]);
     setSelectedTags([...selectedTags, { label: tag, value: tag }]);
-  }
+  };
 
-  const onTagSelect = tag =>
-    setSelectedTags([...selectedTags, tag]);
+  const onTagSelect = (tag) => setSelectedTags([...selectedTags, tag]);
 
+  const onTagDelete = (tag) =>
+    setSelectedTags(selectedTags.filter((ele) => ele.value !== tag.value));
 
-  const onTagDelete = tag => setSelectedTags(selectedTags.filter(ele => ele.value !== tag.value));
-
-  return <Tags
-    allTags={allTags}
-    selectedTags={selectedTags}
-    onTagCreate={onTagCreate}
-    onTagSelect={onTagSelect}
-    onTagDelete={onTagDelete}
-    tagProps={args.tagProps}
-    buttonProps={args.buttonProps}
-    selectProps={args.selectProps}
-  />
-}
+  return (
+    <Tags
+      allTags={allTags}
+      selectedTags={selectedTags}
+      onTagCreate={onTagCreate}
+      onTagSelect={onTagSelect}
+      onTagDelete={onTagDelete}
+      tagProps={args.tagProps}
+      buttonProps={args.buttonProps}
+      selectProps={args.selectProps}
+    />
+  );
+};
 
 export const Default = Template.bind();
 Default.args = {
-  allTags: [{
-    label: 'Tag1',
-    value: 'tag1'
-  },{
-    label: 'Tag2',
-    value: 'tag2'
-  },{
-    label: 'Tag3',
-    value: 'tag3'
-  }],
-  selectedTags: [{
-    label: 'Tag1',
-    value: 'tag1'
-  }]
-}
+  allTags: [
+    {
+      label: "Tag1",
+      value: "tag1",
+    },
+    {
+      label: "Tag2",
+      value: "tag2",
+    },
+    {
+      label: "Tag3",
+      value: "tag3",
+    },
+  ],
+  selectedTags: [
+    {
+      label: "Tag1",
+      value: "tag1",
+    },
+  ],
+};
 
 export const TagWithColor = Template.bind();
 TagWithColor.args = {
-  allTags: [{
-    label: 'Tag1',
-    value: 'tag1'
-  },{
-    label: 'Tag2',
-    value: 'tag2'
-  },{
-    label: 'Tag3',
-    value: 'tag3'
-  }],
-  selectedTags: [{
-    label: 'Tag1',
-    value: 'tag1'
-  }],
+  allTags: [
+    {
+      label: "Tag1",
+      value: "tag1",
+    },
+    {
+      label: "Tag2",
+      value: "tag2",
+    },
+    {
+      label: "Tag3",
+      value: "tag3",
+    },
+  ],
+  selectedTags: [
+    {
+      label: "Tag1",
+      value: "tag1",
+    },
+  ],
   tagProps: {
     color: "gray",
-    style: "outline"
+    style: "outline",
   },
-  buttonProps: {
-    style: "secondary"
-  }
-}
+};
