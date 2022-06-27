586fd3e038180c16d923f2c2f2e4bc7b431af4ab
amaldinesh7 <amalkdinesh@gmail.com>
2022-06-27 13:09:42 +0530

    Added esm module for components bundle

--- rollup.config.js
+++ rollup.config.js
@@ -18,9 +18,16 @@ const plugins = [
   babel({
     exclude: "node_modules/**",
     presets: ["@babel/preset-env", "@babel/preset-react"],
-    plugins: ["@babel/plugin-transform-runtime", ["import", {
-      libraryName: "antd", libraryDirectory: "lib"
-    }]],
+    plugins: [
+      "@babel/plugin-transform-runtime",
+      [
+        "import",
+        {
+          libraryName: "antd",
+          libraryDirectory: "lib",
+        },
+      ],
+    ],
     babelHelpers: "runtime",
   }),
   resolve({
@@ -29,49 +36,63 @@ const plugins = [
     moduleDirectories: ["node_modules"],
   }),
   commonjs({
-    include: /\**node_modules\**/
+    include: /\**node_modules\**/,
   }),
   json(),
-  terser({ compress: { evaluate: false } })
+  terser({ compress: { evaluate: false } }),
 ];
 
-export default [{
-  input: "./lib/components/index.js",
-  output: {
-    file: "index.js",
-    format: "esm",
-    sourcemap: false,
-    assetFileNames: "[name][extname]",
+export default [
+  {
+    input: "./lib/components/index.js",
+    output: [
+      {
+        file: "index.esm.js",
+        format: "esm",
+        sourcemap: false,
+        assetFileNames: "[name][extname]",
+      },
+      {
+        file: "index.js",
+        format: "umd",
+        name: "neetoui",
+        sourcemap: false,
+        assetFileNames: "[name][extname]",
+      },
+    ],
+    plugins: [
+      ...plugins,
+      styles({
+        extensions: [".css", ".scss", ".min.css"],
+        mode: ["extract", "index.css"],
+      }),
+    ],
   },
-  plugins: [
-    ...plugins,
-    styles({
-      extensions: [".css", ".scss", ".min.css"],
-      mode: ["extract", "index.css"],
-    }),
-  ],
-}, {
-  input: "./lib/components/layouts/index.js",
-  output: {
-    file: "layouts.js",
-    format: "esm",
-    sourcemap: false,
+  {
+    input: "./lib/components/layouts/index.js",
+    output: {
+      file: "layouts.js",
+      format: "esm",
+      sourcemap: false,
+    },
+    plugins,
   },
-  plugins,
-}, {
-  input: "./lib/components/formik/index.js",
-  output:  {
-    file: "formik.js",
-    format: "esm",
-    sourcemap: false,
+  {
+    input: "./lib/components/formik/index.js",
+    output: {
+      file: "formik.js",
+      format: "esm",
+      sourcemap: false,
+    },
+    plugins,
   },
-  plugins
-}, {
-  input: "./lib/molecules/index.js",
-  output:  {
-    file: "molecules.js",
-    format: "esm",
-    sourcemap: false,
+  {
+    input: "./lib/molecules/index.js",
+    output: {
+      file: "molecules.js",
+      format: "esm",
+      sourcemap: false,
+    },
+    plugins,
   },
-  plugins
-}];
+];
