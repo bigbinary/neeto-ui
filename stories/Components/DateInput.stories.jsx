efb3c81ee2f21d99ae04b5909d029141e0c9d1fb
Tajuddin <khantaj65@gmail.com>
2022-07-05 19:36:13 +0530

    Updated Date with Custom target stories

--- stories/Components/DateInput.stories.jsx
+++ stories/Components/DateInput.stories.jsx
@@ -1,6 +1,5 @@
 import { Modal, Typography, Pane } from "../../lib/components";
 import React from "react";
-
 import isChromatic from "chromatic/isChromatic";
 import DatePicker from "../../lib/components/DatePicker";
 import TimePicker from "../../lib/components/TimePicker";
@@ -181,39 +180,83 @@ TimePickerWithDefaultValue.parameters = {
   },
 };
 
-export const DatePickerWithCustomTarget = (args) => {
+export const DatePickerWithCustomTargetAndLabel = (args) => {
   return (
-    <div className="space-y-3">
-      <DatePicker customTarget={<Button icon={Calendar} />} {...args} />
-      <DatePicker customTarget={<Button label="Select date Range" />} defaultValue={
-        isChromatic()
-          ? [
-            dayjs(new Date(1999, 7, 16)),
-            dayjs(new Date(1999, 7, 16)).add(7, "day"),
-          ]
-          : [dayjs(), dayjs().add(7, "day")]
-      } type="range" {...args} />
-
-      <TimePicker
-        customTarget={
-          <Button
-            label="Select Time"
-            style="secondary"
-            icon={Clock}
-            iconPosition="left"
-          />
-        }
-        {...args}
-      />
-    </div>
+    <DatePicker customTarget={(dateObj) => {
+      const customLabel = dateObj?.toLocaleString() ?? "Select Date";
+      return <Button icon={Calendar}
+        label={customLabel} />;
+    }} {...args} />
   );
 };
-DatePickerWithCustomTarget.storyName = "Date/Time Picker with Custom Target";
+DatePickerWithCustomTargetAndLabel.storyName = "Date Picker with Custom Target and and dynamic Label";
+DatePickerWithCustomTargetAndLabel.parameters = {
+  docs: {
+    description: {
+      story:
+        "`customTarget` prop is used to replace the default input field with any custom element, Date object can be recieved in the customTarget function if you want to use it",
+    },
+  },
+};
+
+export const DatePickerWithCustomTarget = (args) => {
+  return (
+    <DatePicker customTarget={<Button label="Select Date" icon={Calendar} />} {...args} />
+  );
+};
+DatePickerWithCustomTarget.storyName = "Date Picker with Custom Target";
 DatePickerWithCustomTarget.parameters = {
   docs: {
     description: {
       story:
-        "`customTarget` prop is used to replace the default input field with any custom element",
+        "`customTarget` prop is used to replace the default input field with any custom element.",
+    },
+  },
+};
+
+export const TimePickerWithCustomTarget = (args) => {
+  return (
+    <TimePicker
+      defaultValue={
+        isChromatic() ? dayjs(new Date(1999, 7, 16, 5, 32)) : dayjs()
+      }
+      customTarget={(timeObj) => {
+        const customLabel = timeObj?.toLocaleString() ?? "Select Time";
+        return <Button icon={Clock}
+          label={customLabel} />;
+      }}
+      {...args}
+    />
+  );
+};
+TimePickerWithCustomTarget.storyName = "Time Picker with Custom Target and dynamic Label";
+TimePickerWithCustomTarget.parameters = {
+  docs: {
+    description: {
+      story:
+        "`customTarget` prop is used to replace the default input field with any custom element, Date object can be recieved in the customTarget function if you want to use it",
+    },
+  },
+};
+
+export const RangePickerWithCustomTarget = (args) => {
+  return (
+    <div className="space-y-3">
+      <DatePicker type="range" customTarget={(dateObj) => {
+        const customLabel = `Starts On: ${dateObj?.[0]?.toLocaleString() ?? "---"} - Ends On: ${dateObj?.[1]?.toLocaleString() ?? "---"}`;
+        return <Button icon={Calendar}
+          label={customLabel} />;
+      }} {...args} />
+
+    </div>
+  );
+};
+RangePickerWithCustomTarget.storyName = "Range Picker with Custom Target and value";
+RangePickerWithCustomTarget.parameters = {
+  docs: {
+    description: {
+      story:
+        "`customTarget` prop is used to replace the default input field with any custom element. `DateObj` can be recieved as prop to use in the custom Target it contains start and end date in `[startdateObj, endDateObj]`",
     },
   },
 };
