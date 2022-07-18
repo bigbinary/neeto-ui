3a4edc2be3010050f4aaac9e9ae1e1f5772e0cd4
Praveen Murali <itspraveenmurali@gmail.com>
2022-07-18 09:00:51 +0530

    Accordion fix

--- stories/Components/Accordion.stories.jsx
+++ stories/Components/Accordion.stories.jsx
@@ -15,6 +15,10 @@ export default {
         component: '`import { Accordion } from "@bigbinary/neetoui";`',
       },
     },
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/zhdsnPzXzr264x1WUeVdmA/02-Components?node-id=104%3A15",
+    },
   },
 };
 
