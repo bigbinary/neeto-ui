30fd2bf0f907b8a0039bbb12d9f695f6c3543fdb
Praveen Murali <itspraveenmurali@gmail.com>
2022-07-19 18:46:04 +0530

    Minor UI fixes

--- stories/Overlays/Pane.stories.jsx
+++ stories/Overlays/Pane.stories.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Check } from "@bigbinary/neeto-icons";
 
 import Button from "../../lib/components/Button";
