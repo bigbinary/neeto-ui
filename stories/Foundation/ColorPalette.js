4970ac2e3cdb48bc657a8ccd69aab9952d01872b
Ajmal Noushad <ajufhn@gmail.com>
2022-08-24 22:01:20 +0530

    Add support to switch color palette based on themes

--- stories/Foundation/ColorPalette.js
+++ stories/Foundation/ColorPalette.js
@@ -3,152 +3,620 @@ const colorPalette = [
     name: "$neeto-ui-white",
     value: "255, 255, 255",
     compiledValue: "255, 255, 255",
+    hexValue: "#ffffff",
   },
   {
     name: "$neeto-ui-black",
     value: "27, 31, 35",
     compiledValue: "27, 31, 35",
+    hexValue: "#1b1f23",
   },
   {
     name: "$neeto-ui-gray-800",
     value: "47, 57, 65",
     compiledValue: "47, 57, 65",
+    hexValue: "#2f3b45",
   },
   {
     name: "$neeto-ui-gray-700",
     value: "73, 84, 92",
     compiledValue: "73, 84, 92",
+    hexValue: "#485460",
   },
   {
     name: "$neeto-ui-gray-600",
     value: "104, 115, 125",
     compiledValue: "104, 115, 125",
+    hexValue: "#68757d",
   },
   {
     name: "$neeto-ui-gray-500",
     value: "135, 146, 157",
     compiledValue: "135, 146, 157",
+    hexValue: "#8b929b",
   },
   {
     name: "$neeto-ui-gray-400",
     value: "194, 200, 204",
     compiledValue: "194, 200, 204",
+    hexValue: "#c4c8cc",
   },
   {
     name: "$neeto-ui-gray-300",
     value: "216, 220, 222",
     compiledValue: "216, 220, 222",
+    hexValue: "#d8dcde",
   },
   {
     name: "$neeto-ui-gray-200",
     value: "233, 235, 237",
     compiledValue: "233, 235, 237",
+    hexValue: "#e9ebed",
   },
   {
     name: "$neeto-ui-gray-100",
     value: "248, 249, 249",
     compiledValue: "248, 249, 249",
+    hexValue: "#f8f9f9",
   },
   {
     name: "$neeto-ui-primary-800",
     value: "45, 54, 212",
     compiledValue: "45, 54, 212",
+    hexValue: "#2d36d4",
   },
   {
     name: "$neeto-ui-primary-600",
     value: "54, 66, 223",
     compiledValue: "54, 66, 223",
+    hexValue: "#3642df",
   },
   {
     name: "$neeto-ui-primary-500",
     value: "69, 88, 249",
     compiledValue: "69, 88, 249",
+    hexValue: "#4b58f9",
   },
   {
     name: "$neeto-ui-primary-100",
     value: "235, 236, 254",
     compiledValue: "235, 236, 254",
+    hexValue: "#ebecfe",
   },
   {
     name: "$neeto-ui-success-800",
     value: "16, 113, 84",
     compiledValue: "16, 113, 84",
+    hexValue: "#101d54",
   },
   {
     name: "$neeto-ui-success-600",
     value: "18, 128, 92",
     compiledValue: "18, 128, 92",
+    hexValue: "#122058",
   },
   {
     name: "$neeto-ui-success-500",
     value: "38, 142, 108",
     compiledValue: "38, 142, 108",
+    hexValue: "#264866",
   },
   {
     name: "$neeto-ui-success-100",
     value: "225, 243, 238",
     compiledValue: "225, 243, 238",
+    hexValue: "#e1f3ee",
   },
   {
     name: "$neeto-ui-error-800",
     value: "187, 18, 26",
     compiledValue: "187, 18, 26",
+    hexValue: "#bb121a",
   },
   {
     name: "$neeto-ui-error-600",
     value: "201, 37, 45",
     compiledValue: "201, 37, 45",
+    hexValue: "#c92329",
   },
   {
     name: "$neeto-ui-error-500",
     value: "215, 55, 63",
     compiledValue: "215, 55, 63",
+    hexValue: "#d73739",
   },
   {
     name: "$neeto-ui-error-100",
     value: "254, 236, 240",
     compiledValue: "254, 236, 240",
+    hexValue: "#fecce0",
   },
   {
     name: "$neeto-ui-info-800",
     value: "9, 90, 186",
     compiledValue: "9, 90, 186",
+    hexValue: "#095ab6",
   },
   {
     name: "$neeto-ui-info-600",
     value: "13, 102, 208",
     compiledValue: "13, 102, 208",
+    hexValue: "#1366d0",
   },
   {
     name: "$neeto-ui-info-500",
     value: "20, 115, 230",
     compiledValue: "20, 115, 230",
+    hexValue: "#1473e6",
   },
   {
     name: "$neeto-ui-info-100",
     value: "226, 242, 255",
     compiledValue: "226, 242, 255",
+    hexValue: "#e2f2ff",
   },
   {
     name: "$neeto-ui-warning-800",
     value: "189, 100, 13",
     compiledValue: "189, 100, 13",
+    hexValue: "#bd640d",
   },
   {
     name: "$neeto-ui-warning-600",
     value: "203, 111, 16",
     compiledValue: "203, 111, 16",
+    hexValue: "#cb6f10",
   },
   {
     name: "$neeto-ui-warning-500",
     value: "218, 123, 17",
     compiledValue: "218, 123, 17",
+    hexValue: "#d87b11",
   },
   {
     name: "$neeto-ui-warning-100",
     value: "251, 242, 225",
     compiledValue: "251, 242, 225",
+    hexValue: "#f9f2e1",
   },
 ];
+
+export const colorPaletteMap = {
+  base: {
+    name: "BASE",
+    colors: [
+      {
+        name: "neeto-ui-white",
+        light: {
+          value: "255, 255, 255",
+          compiledValue: "255, 255, 255",
+          hexValue: "#ffffff",
+        },
+        dark: {
+          value: "18, 18, 18",
+          compiledValue: "18, 18, 18",
+          hexValue: "#121212",
+        },
+        text: "black",
+      },
+      {
+        name: "neeto-ui-black",
+        light: {
+          value: "27, 31, 35",
+          compiledValue: "27, 31, 35",
+          hexValue: "#1b1f23",
+        },
+        dark: {
+          value: "224, 224, 224",
+          compiledValue: "224, 224, 224",
+          hexValue: "#e0e0e0",
+        },
+      },
+    ],
+  },
+  gray: {
+    title: "GRAY",
+    colors: [
+      {
+        name: "neeto-ui-gray-800",
+        light: {
+          value: "47, 57, 65",
+          compiledValue: "47, 57, 65",
+          hexValue: "#2f3b45",
+        },
+        dark: {
+          value: "200, 200, 200",
+          compiledValue: "200, 200, 200",
+          hexValue: "#c8c8c8",
+        },
+      },
+      {
+        name: "neeto-ui-gray-700",
+        light: {
+          value: "73, 84, 92",
+          compiledValue: "73, 84, 92",
+          hexValue: "#485460",
+        },
+        dark: {
+          value: "162, 162, 162",
+          compiledValue: "162, 162, 162",
+          hexValue: "#a2a2a2",
+        },
+      },
+      {
+        name: "neeto-ui-gray-600",
+        light: {
+          value: "104, 115, 125",
+          compiledValue: "104, 115, 125",
+          hexValue: "#68757d",
+        },
+        dark: {
+          value: "124, 124, 124",
+          compiledValue: "124, 124, 124",
+          hexValue: "#7c7c7c",
+        },
+      },
+      {
+        name: "neeto-ui-gray-500",
+        light: {
+          value: "135, 146, 157",
+          compiledValue: "135, 146, 157",
+          hexValue: "#8b929b",
+        },
+        dark: {
+          value: "92, 92, 92",
+          compiledValue: "92, 92, 92",
+          hexValue: "#5c5c5c",
+        },
+      },
+      {
+        name: "neeto-ui-gray-400",
+        text: "black",
+        light: {
+          value: "194, 200, 204",
+          compiledValue: "194, 200, 204",
+          hexValue: "#c4c8cc",
+        },
+        dark: {
+          value: "73, 73, 73",
+          compiledValue: "73, 73, 73",
+          hexValue: "#494949",
+        },
+      },
+      {
+        name: "neeto-ui-gray-300",
+        text: "black",
+        light: {
+          value: "216, 220, 222",
+          compiledValue: "216, 220, 222",
+          hexValue: "#d8dcde",
+        },
+        dark: {
+          value: "57, 57, 57",
+          compiledValue: "57, 57, 57",
+          hexValue: "#393939",
+        },
+      },
+      {
+        name: "neeto-ui-gray-200",
+        text: "black",
+        light: {
+          value: "233, 235, 237",
+          compiledValue: "233, 235, 237",
+          hexValue: "#e9ebed",
+        },
+        dark: {
+          value: "44, 44, 44",
+          compiledValue: "44, 44, 44",
+          hexValue: "#2c2c2c",
+        },
+      },
+      {
+        name: "neeto-ui-gray-100",
+        text: "black",
+        light: {
+          value: "248, 249, 249",
+          compiledValue: "248, 249, 249",
+          hexValue: "#f8f9f9",
+        },
+        dark: {
+          value: "27, 27, 27",
+          compiledValue: "27, 27, 27",
+          hexValue: "#1b1b1b",
+        },
+      },
+    ],
+  },
+  primary: {
+    title: "PRIMARY",
+    colors: [
+      {
+        name: "neeto-ui-primary-800",
+        light: {
+          value: "45, 54, 212",
+          compiledValue: "45, 54, 212",
+          hexValue: "#2d36d4",
+        },
+        dark: {
+          value: "209, 213, 254",
+          compiledValue: "209, 213, 254",
+          hexValue: "#d1d5fe",
+        },
+      },
+      {
+        name: "neeto-ui-primary-600",
+        light: {
+          value: "54, 66, 223",
+          compiledValue: "54, 66, 223",
+          hexValue: "#3642df",
+        },
+        dark: {
+          value: "166, 170, 254",
+          compiledValue: "166, 170, 254",
+          hexValue: "#a6aafe",
+        },
+      },
+      {
+        name: "neeto-ui-primary-500",
+        light: {
+          value: "69, 88, 249",
+          compiledValue: "69, 88, 249",
+          hexValue: "#4b58f9",
+        },
+        dark: {
+          value: "137, 147, 254",
+          compiledValue: "137, 147, 254",
+          hexValue: "#879ffa",
+        },
+      },
+      {
+        name: "neeto-ui-primary-100",
+        text: "black",
+        light: {
+          value: "235, 236, 254",
+          compiledValue: "235, 236, 254",
+          hexValue: "#ebecfe",
+        },
+        dark: {
+          value: "27, 28, 37",
+          compiledValue: "27, 28, 37",
+          hexValue: "#1b1c27",
+        },
+      },
+    ],
+  },
+  success: {
+    title: "SUCCESS",
+    colors: [
+      {
+        name: "neeto-ui-success-800",
+        light: {
+          value: "16, 113, 84",
+          compiledValue: "16, 113, 84",
+          hexValue: "#101d54",
+        },
+        dark: {
+          value: "57, 185, 144",
+          compiledValue: "57, 185, 144",
+          hexValue: "#39b18c",
+        },
+      },
+      {
+        name: "neeto-ui-success-600",
+        light: {
+          value: "18, 128, 92",
+          compiledValue: "18, 128, 92",
+          hexValue: "#122058",
+        },
+        dark: {
+          value: "51, 171, 132",
+          compiledValue: "51, 171, 132",
+          hexValue: "#33ab84",
+        },
+      },
+      {
+        name: "neeto-ui-success-500",
+        light: {
+          value: "38, 142, 108",
+          compiledValue: "38, 142, 108",
+          hexValue: "#264866",
+        },
+        dark: {
+          value: "45, 157, 120",
+          compiledValue: "45, 157, 120",
+          hexValue: "#33ab84",
+        },
+      },
+      {
+        name: "neeto-ui-success-100",
+        text: "black",
+        light: {
+          value: "225, 243, 238",
+          compiledValue: "225, 243, 238",
+          hexValue: "#e1f3ee",
+        },
+        dark: {
+          value: "23, 85, 65",
+          compiledValue: "23, 85, 65",
+          hexValue: "#1b1c27",
+        },
+      },
+    ],
+  },
+  error: {
+    title: "ERROR",
+    colors: [
+      {
+        name: "neeto-ui-error-800",
+        light: {
+          value: "187, 18, 26",
+          compiledValue: "187, 18, 26",
+          hexValue: "#bb121a",
+        },
+        dark: {
+          value: "247, 109, 116",
+          compiledValue: "247, 109, 116",
+          hexValue: "#f7696e",
+        },
+      },
+      {
+        name: "neeto-ui-error-600",
+        light: {
+          value: "201, 37, 45",
+          compiledValue: "201, 37, 45",
+          hexValue: "#c92329",
+        },
+        dark: {
+          value: "236, 91, 98",
+          compiledValue: "236, 91, 98",
+          hexValue: "#ec5f62",
+        },
+      },
+      {
+        name: "neeto-ui-error-500",
+        light: {
+          value: "215, 55, 63",
+          compiledValue: "215, 55, 63",
+          hexValue: "#d73739",
+        },
+        dark: {
+          value: "227, 72, 80",
+          compiledValue: "227, 72, 80",
+          hexValue: "#e34740",
+        },
+      },
+      {
+        name: "neeto-ui-error-100",
+        text: "black",
+        light: {
+          value: "254, 236, 240",
+          compiledValue: "254, 236, 240",
+          hexValue: "#fecce0",
+        },
+        dark: {
+          value: "129, 33, 38",
+          compiledValue: "129, 33, 38",
+          hexValue: "#812926",
+        },
+      },
+    ],
+  },
+  warning: {
+    title: "WARNING",
+    colors: [
+      {
+        name: "neeto-ui-warning-800",
+        light: {
+          value: "189, 100, 13",
+          compiledValue: "189, 100, 13",
+          hexValue: "#bd640d",
+        },
+        dark: {
+          value: "249, 164, 63",
+          compiledValue: "249, 164, 63",
+          hexValue: "#f9a43f",
+        }
+      },
+      {
+        name: "neeto-ui-warning-600",
+        light: {
+          value: "203, 111, 16",
+          compiledValue: "203, 111, 16",
+          hexValue: "#cb6f10",
+        },
+        dark: {
+          value: "242, 148, 35",
+          compiledValue: "242, 148, 35",
+          hexValue: "#f2a833",
+        }
+      },
+      {
+        name: "neeto-ui-warning-500",
+        light: {
+          value: "218, 123, 17",
+          compiledValue: "218, 123, 17",
+          hexValue: "#d87b11",
+        },
+        dark: {
+          value: "230, 134, 25",
+          compiledValue: "230, 134, 25",
+          hexValue: "#e88e19",
+        }
+      },
+      {
+        name: "neeto-ui-warning-100",
+        text: "black",
+        light: {
+          value: "251, 242, 225",
+          compiledValue: "251, 242, 225",
+          hexValue: "#f9f2e1",
+        },
+        dark: {
+          value: "131, 74, 11",
+          compiledValue: "131, 74, 11",
+          hexValue: "#8d4a0b",
+        }
+      },
+    ],
+  },
+  info: {
+    title: "INFO",
+    colors: [
+      {
+        name: "neeto-ui-info-800",
+        light: {
+          value: "9, 90, 186",
+          compiledValue: "9, 90, 186",
+          hexValue: "#095ab6",
+        },
+        dark: {
+          value: "75, 156, 245",
+          compiledValue: "75, 156, 245",
+          hexValue: "#4b9ae5",
+        },
+      },
+      {
+        name: "neeto-ui-info-600",
+        light: {
+          value: "13, 102, 208",
+          compiledValue: "13, 102, 208",
+          hexValue: "#0d66d0",
+        },
+        dark: {
+          value: "55, 142, 240",
+          compiledValue: "55, 142, 240",
+          hexValue: "#3796f0",
+        },
+      },
+      {
+        name: "neeto-ui-info-500",
+        light: {
+          value: "20, 115, 230",
+          compiledValue: "20, 115, 230",
+          hexValue: "#1473e6",
+        },
+        dark: {
+          value: "38, 128, 235",
+          compiledValue: "38, 128, 235",
+          hexValue: "#2680eb",
+        },
+      },
+      {
+        name: "neeto-ui-info-100",
+        text: "black",
+        light: {
+          value: "226, 242, 255",
+          compiledValue: "226, 242, 255",
+          hexValue: "#e2f2ff",
+        },
+        dark: {
+          value: "11, 69, 137",
+          compiledValue: "11, 69, 137",
+          hexValue: "#0b4589",
+        },
+      },
+    ],
+  },
+};
 
 export default colorPalette;
---
+++ stories/Foundation/ColorStory.jsx
@@ -0,0 +1,56 @@
+import React, { Fragment, useState } from "react";
+
+import classnames from "classnames";
+import Radio from "../../lib/components/Radio";
+import { colorPaletteMap } from "./ColorPalette";
+
+const ColorStory = () => {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <div
+      className={classnames(
+        "demo-color-swatch-wrapper p-10 rounded border neeto-ui-border-gray-300",
+        {
+          "neeto-ui-theme--dark": theme === "dark",
+        }
+      )}
+    >
+      <Radio
+        className="flex justify-end items-center mb-8 w-full"
+        onChange={(event) => setTheme(event.target.value)}
+        value={theme}
+      >
+        <Radio.Item label="Light theme" name="theme" value="light" />
+        <Radio.Item label="Dark theme" name="theme" value="dark" />
+      </Radio>
+      {Object.entries(colorPaletteMap).map(([key, { title, colors }]) => (
+        <Fragment key={key}>
+          {console.log('colors', colors)}
+          <h5 className="mb-2">{title}</h5>
+          <div className="demo-color-swatch-wrapper__row mb-10 flex flex-wrap gap-5">
+            {colors.map(({ name, [theme]: { value, hexValue }, text = "white" }) => (
+              <div
+                key={name}
+                className={classnames(
+                  "demo-color-swatch border neeto-ui-border-gray-300",
+                  name.replace("neeto-ui", "neeto-ui-bg"),
+                  {
+                    "neeto-ui-text-black": text === "black",
+                    "neeto-ui-text-white": text === "white",
+                  }
+                )}
+              >
+                <code>--{name}</code>
+                <code>{value}</code>
+                <code>{hexValue}</code>
+              </div>
+            ))}
+          </div>
+        </Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default ColorStory;
