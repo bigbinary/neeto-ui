987f1ead8682ce9f2a4fc11f7f2fe14daf8d7153
amaldinesh7 <amalkdinesh@gmail.com>
2022-08-30 18:57:42 +0530

    Updated character count logic in textarea

--- stories/Components/Textarea.stories.jsx
+++ stories/Components/Textarea.stories.jsx
@@ -14,7 +14,8 @@ export default {
     },
     design: {
       type: "figma",
-      url: "https://www.figma.com/file/zhdsnPzXzr264x1WUeVdmA/02-Components?node-id=104%3A13",
+      url:
+        "https://www.figma.com/file/zhdsnPzXzr264x1WUeVdmA/02-Components?node-id=104%3A13",
     },
   },
 };
@@ -39,29 +40,69 @@ export const Controlled = () => {
 };
 
 export const Required = () => (
-  <Textarea label="Required Textarea" placeholder="Enter text" required={true} />
+  <Textarea
+    label="Required Textarea"
+    placeholder="Enter text"
+    required={true}
+  />
 );
 
 export const Disabled = () => (
-  <Textarea label="Disabled Textarea" placeholder="Enter text" disabled={true} />
+  <Textarea
+    label="Disabled Textarea"
+    placeholder="Enter text"
+    disabled={true}
+  />
 );
 
 export const Error = () => (
-  <Textarea label="Error" placeholder="Enter text" error="Provide valid email" />
+  <Textarea
+    label="Error"
+    placeholder="Enter text"
+    error="Provide valid email"
+  />
 );
 
 export const HelpText = () => (
-  <Textarea label="Name" placeholder="Enter text" helpText="This is help text props to the component." />
+  <Textarea
+    label="Name"
+    placeholder="Enter text"
+    helpText="This is help text props to the component."
+  />
 );
 
 export const NakedInput = () => (
-  <Textarea label="Naked Textarea Field" placeholder="Enter text" nakedTextarea={true} />
+  <Textarea
+    label="Naked Textarea Field"
+    placeholder="Enter text"
+    nakedTextarea={true}
+  />
 );
 
 export const TextareaWithMaxLength = () => (
-  <Textarea label="Textarea with max length" placeholder="Enter text" maxLength={10} />
-);
-
-export const TextareaWithMaxLengthWithoutLabel = () => (
-  <Textarea placeholder="Enter text" maxLength={10} />
+  <div className="flex flex-col space-y-6">
+    <Textarea
+      label="Textarea with max length"
+      placeholder="Enter text"
+      maxLength={10}
+    />
+    <Textarea
+      label="Textarea with max length"
+      placeholder="Enter text"
+      value="Sample I"
+      maxLength={10}
+    />
+    <Textarea
+      label="Textarea with max length"
+      placeholder="Enter text"
+      value="Sample In"
+      maxLength={10}
+    />
+    <Textarea
+      label="Textarea with max length"
+      placeholder="Enter text"
+      value="Sample Input"
+      maxLength={10}
+    />
+  </div>
 );
--- tests/Textarea.test.js
+++ tests/Textarea.test.js
@@ -26,7 +26,9 @@ describe("Textarea", () => {
   });
 
   it("should display helpText", () => {
-    const { getByText } = render(<Textarea id="text" label="Textarea" helpText="Help text" />);
+    const { getByText } = render(
+      <Textarea id="text" label="Textarea" helpText="Help text" />
+    );
     expect(getByText("Help text")).toBeInTheDocument();
   });
 
@@ -41,11 +43,8 @@ describe("Textarea", () => {
     const { getByLabelText, getByText } = render(
       <Textarea id="text" label="Textarea" maxLength={5} />
     );
-    expect(getByText(/0(.*)\/(.*)5/)).toBeInTheDocument();
-    expect(getByLabelText("Textarea")).toHaveAttribute("maxLength", "5");
-
     userEvent.type(getByLabelText("Textarea"), "Testing maxLength");
-    expect(getByText(/5(.*)\/(.*)5/)).toBeInTheDocument();
-    expect(getByLabelText("Textarea")).toHaveValue("Testi");
+    expect(getByText(/17(.*)\/(.*)5/)).toBeInTheDocument();
+    expect(getByLabelText("Textarea")).toHaveValue("Testing maxLength");
   });
 });
