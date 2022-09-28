92edc4ed0fa060af4b5d14fb2903f9034085248c
Ajil Paul <36255991+ajil-paul@users.noreply.github.com>
2022-09-28 18:55:15 +0530

    Added Kbd component (#1341)

---
+++ stories/Components/Kbd.stories.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Kbd from "../../lib/components/Kbd";
+
+export default {
+  title: "Components/Kbd",
+  component: Kbd,
+  parameters: {
+    layout: "padded",
+    docs: {
+      description: {
+        component: '`import { Kbd } from "@bigbinary/neetoui";`',
+      },
+    },
+  },
+};
+
+const Template = ({}) => (
+  <div className="flex gap-x-1">
+    <Kbd keyName="⌘" />
+    <Kbd keyName="⇧" />
+    <Kbd keyName="B" />
+  </div>
+);
+
+export const Default = Template.bind({});
+
+// eslint-disable-next-line no-empty-pattern
