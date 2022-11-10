b84d641917847761fc98d9b96ebbf6db59cf50c4
karthiknmenon <menonk484@gmail.com>
2022-11-10 16:53:10 +0530

    Add overlayManager for Modal and Pane

--- stories/Overlays/Pane.stories.jsx
+++ stories/Overlays/Pane.stories.jsx
@@ -176,3 +176,114 @@ export const PaneWithModalAndAlert = () => {
   );
 };
 PaneWithModalAndAlert.storyName = "Pane with Modal and Alert";
+
+export const MultiplePanes = () => {
+  const [showPane, setShowPane] = useState(false);
+  const [showPane2, setShowPane2] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div className="w-full">
+      <div className="space-y-6">
+        <div className="w-1/2 space-y-8">
+          <div className="flex flex-row items-center justify-start space-x-2">
+            <Button label="Show Pane" onClick={() => setShowPane(true)} />
+          </div>
+        </div>
+      </div>
+
+      <Pane isOpen={showPane} onClose={() => setShowPane(false)}>
+        <Pane.Header>
+          <Typography style="h2" weight="semibold">
+            Typography
+          </Typography>
+        </Pane.Header>
+        <Pane.Body className="space-y-4">
+          <Typography style="body2">
+            Somewhere out in space live the Herculoids! Zok, the laser-ray
+            dragon! Igoo, the giant rock ape! Tundro, the tremendous! Gloop and
+            Gleep, the formless, fearless wonders! With Zandor, their leader,
+            and his wife, Tara, and son, Dorno, they team up to protect their
+            planet from sinister invaders! All-strong! All-brave! All-heroes!
+            They're the Herculoids!
+          </Typography>
+          <div className="flex space-x-2">
+            <Button label="Show modal" onClick={() => setShowModal(true)} />
+            <Button label="Show alert" onClick={() => setShowAlert(true)} />
+          </div>
+        </Pane.Body>
+        <Pane.Footer className="flex items-center space-x-2">
+          <Button
+            icon={Check}
+            label="Continue"
+            onClick={() => setShowPane2(true)}
+          />
+          <Button
+            style="text"
+            label="Cancel"
+            onClick={() => setShowPane(false)}
+          />
+        </Pane.Footer>
+      </Pane>
+      <Pane isOpen={showPane2} onClose={() => setShowPane2(false)}>
+        <Pane.Header>
+          <Typography style="h2" weight="semibold">
+            Typography
+          </Typography>
+        </Pane.Header>
+        <Pane.Body className="space-y-4">
+          <Typography style="body2">
+            Somewhere out in space live the Herculoids! Zok, the laser-ray
+            dragon! Igoo, the giant rock ape! Tundro, the tremendous! Gloop and
+            Gleep, the formless, fearless wonders! With Zandor, their leader,
+            and his wife, Tara, and son, Dorno, they team up to protect their
+            planet from sinister invaders! All-strong! All-brave! All-heroes!
+            They're the Herculoids!
+          </Typography>
+          <div className="flex space-x-2">
+            <Button label="Show modal" onClick={() => setShowModal(true)} />
+            <Button label="Show alert" onClick={() => setShowAlert(true)} />
+          </div>
+        </Pane.Body>
+        <Pane.Footer className="flex items-center space-x-2">
+          <Button
+            icon={Check}
+            label="Continue"
+            onClick={() => setShowModal(true)}
+          />
+          <Button
+            style="text"
+            label="Cancel"
+            onClick={() => setShowPane2(false)}
+          />
+        </Pane.Footer>
+      </Pane>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+        <Modal.Header>
+          <h2>Add label</h2>
+        </Modal.Header>
+        <Modal.Body>
+          <Typography style="body2">
+            Somewhere out in space live the Herculoids! Zok, the laser-ray
+            dragon! Igoo, the giant rock ape! Tundro, the tremendous! Gloop and
+            Gleep, the formless, fearless wonders! With Zandor, their leader,
+          </Typography>
+        </Modal.Body>
+        <Modal.Footer className="space-x-2">
+          <Button
+            icon={Check}
+            label="Continue"
+            onClick={() => setShowModal(false)}
+          />
+          <Button
+            style="text"
+            label="Cancel"
+            onClick={() => setShowModal(false)}
+          />
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+PaneWithModalAndAlert.storyName = "Pane with Modal and Alert";
