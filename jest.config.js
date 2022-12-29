85766421e15b5948847c061cc2ee7747e48a27c7
Amaljith K <85148587+Amaljith-K@users.noreply.github.com>
2022-12-29 18:40:06 +0530

    Fixed aliases for render deployment and unit tests (#1500)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: amaldinesh7 <amalkdinesh@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = () => {
     verbose: true,
     moduleNameMapper: {
       "^.+\\.(css|less|scss)$": "identity-obj-proxy",
-      "^@bigbinary/neetoui/(.*)$": path.resolve(__dirname, "$1"),
+      "^@bigbinary/neetoui/(.*)$": path.resolve(__dirname, "lib", "$1"),
     },
     transformIgnorePatterns: [
       "/node_modules/(?!(@babel|react-router-nav-prompt|@bigbinary/neeto-icons|rc-picker|rc-util))",
