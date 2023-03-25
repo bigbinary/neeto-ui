255e9e07d9e231df121150ffa19f97fda7ffab85
Abhay V Ashokan <abhayvashokan@gmail.com>
2023-03-25 13:16:44 +0530

    Disabled @bigbinary/neeto/prefix-neeto-ui-import-alias ESlint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = mergeDeepLeft(
       "@bigbinary/neeto/neetocommons-tips": "off",
       "@bigbinary/neeto/no-blacklisted-imports": "off",
       "@bigbinary/neeto/no-dangling-constants": "off",
+      "@bigbinary/neeto/prefix-neeto-ui-import-alias": "off",
     },
   },
   defaultConfig
