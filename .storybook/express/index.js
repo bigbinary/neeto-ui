ed2e5d4245004518b31972d7106c23bd45d843fd
Calvin Chiramal <calvin.chiramal@bigbinary.com>
2023-05-26 17:15:19 +0530

    Removed logic for adding commit hash

--- .storybook/express/index.js
+++ .storybook/express/index.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const path = require("path");
-const { execSync } = require("child_process");
+// const { execSync } = require("child_process");
 
 const server = express();
 const port = 8080;
 
-const commitHash = execSync("git rev-parse HEAD").toString().trim();
+// const commitHash = execSync("git rev-parse HEAD").toString().trim();
 
-const generateUrlWithHash = req => {
-  const parsedUrl = new URL(req.originalUrl, `http://${req.headers.host}`);
-  parsedUrl.searchParams.set("hash", commitHash);
+// const generateUrlWithHash = req => {
+//   const parsedUrl = new URL(req.originalUrl, `http://${req.headers.host}`);
+//   parsedUrl.searchParams.set("hash", commitHash);
 
-  return parsedUrl.toString();
-};
+//   return parsedUrl.toString();
+// };
 
-const redirectWithCommitHash = (req, res, next) => {
-  // Only matches "/" with or without query params appended. The second condition is to avoid redirection loop.
-  if (req.path === "/" && req.query.hash !== commitHash) {
-    const urlWithHash = generateUrlWithHash(req);
-    res.redirect(urlWithHash);
-  } else {
-    next();
-  }
-};
+// const redirectWithCommitHash = (req, res, next) => {
+//   // Only matches "/" with or without query params appended. The second condition is to avoid redirection loop.
+//   if (req.path === "/" && req.query.hash !== commitHash) {
+//     const urlWithHash = generateUrlWithHash(req);
+//     res.redirect(urlWithHash);
+//   } else {
+//     next();
+//   }
+// };
 
-server.use(redirectWithCommitHash);
+// server.use(redirectWithCommitHash);
 // Serve static files from the specified folder
 server.use(express.static(path.join(__dirname, "../../storybook-static")));
 
