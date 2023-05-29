5214ea497efc50068572f9af2f6690626b243670
Calvin Wilson <calvin.chiramal@bigbinary.com>
2023-05-29 20:22:59 +0530

    Redirected user to storybook root url (#1712)

---
+++ .storybook/express/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const { execSync } = require("child_process");
+
+const server = express();
+const port = 3000;
+
+const commitHash = execSync("git rev-parse HEAD").toString().trim();
+
+const generateUrlWithHash = req => {
+  const parsedUrl = new URL(req.originalUrl, `http://${req.headers.host}`);
+  parsedUrl.searchParams.set("hash", commitHash);
+
+  return parsedUrl.toString();
+};
+
+const redirectWithCommitHash = (req, res, next) => {
+  // Only matches "/" with or without query params appended. The second condition is to avoid redirection loop.
+  if (req.path === "/" && req.query.hash !== commitHash) {
+    const urlWithHash = generateUrlWithHash(req);
+    res.redirect(urlWithHash);
+  } else {
+    next();
+  }
+};
+
+server.use(redirectWithCommitHash);
+// Serve static files from the specified folder
+server.use(express.static(path.join(__dirname, "../../storybook-static")));
+
+// Start the server
+server.listen(port, () =>
+  console.log(`JSON Server is running on port ${port}`)
+);
