81627db417d27e459b1ec7a24aad65a801392eb3
Sooraj J <sooraj.j@bigbinary.com>
2023-06-26 19:56:44 +0530

    Refactored transformObjectToDotNotation fucntion

--- src/components/formik/Form/ScrollToErrorField/utils.js
+++ src/components/formik/Form/ScrollToErrorField/utils.js
@@ -3,14 +3,14 @@ import { is } from "ramda";
 const transformObjectToDotNotation = (object, prefix = "") => {
   const result = [];
 
-  Object.keys(object).forEach(key => {
-    const value = object[key];
-    if (value && is(Object, value)) {
-      const nextPrefix = prefix ? `${prefix}.${key}` : key;
-      result.push(...transformObjectToDotNotation(value, nextPrefix));
-    } else if (value) {
+  Object.entries(object).forEach(([key, value]) => {
+    if (value) {
       const nextKey = prefix ? `${prefix}.${key}` : key;
-      result.push(nextKey);
+      if (is(Object, value)) {
+        result.push(...transformObjectToDotNotation(value, nextKey));
+      } else {
+        result.push(nextKey);
+      }
     }
   });
 
