46aecbc0b69233a08b48a9de85a19bd4b7c304ba
Praveen Murali <itspraveenmurali@gmail.com>
2023-07-18 20:42:22 +0530

    Removed empty box from no data component (#1751)
    * Removed empty box from no data component
    
    * updated image render function

--- src/components/NoData/constants.js
+++
@@ -1,2 +0,0 @@
-export const NO_DATA_IMAGE_URL =
-  "https://d2v7kzglnr2dnh.cloudfront.net/others/no-data.svg";
--- src/components/NoData/utils.jsx
+++ src/components/NoData/utils.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 
-import { NO_DATA_IMAGE_URL } from "./constants";
-
-export const renderImage = image => {
-  if (image) {
-    return typeof image === "string" ? (
-      <img data-cy="no-data-image" src={image} />
-    ) : (
-      image || React.Fragment
-    );
-  }
-
-  return <img data-cy="no-data-image" src={NO_DATA_IMAGE_URL} />;
-};
+export const renderImage = image =>
+  typeof image === "string" ? (
+    <img data-cy="no-data-image" src={image} />
+  ) : (
+    image
+  );
