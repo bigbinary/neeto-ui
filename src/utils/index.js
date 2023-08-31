74b876737823487ae2c62b270d73f711551195bc
Krishnapriya S <krishnapriya.s@bigbinary.com>
2023-08-31 20:04:59 +0530

    Added type check in hyphenize

--- src/utils/index.js
+++ src/utils/index.js
@@ -28,7 +28,7 @@ export const noop = () => {};
 export const hyphenize = string => {
   const fallbackString = "nui";
 
-  if (string && string.replace) {
+  if (string && typeof string === "string" && string.replace) {
     return string
       .replace(/[\s_]/g, "-")
       .replace(/([a-z])([A-Z])/g, "$1-$2")
