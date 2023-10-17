08d775794f0a9d44196cd8336f428f5cb3b644c1
Akhil <akhil.xavier@bigbinary.com>
2023-10-17 19:45:59 +0530

    Add types

--- src/components/Tree/index.jsx
+++ src/components/Tree/index.jsx
@@ -1,47 +1,44 @@
 import React from "react";
 
 import { Tree as TreeComponent } from "antd";
+import classnames from "classnames";
 import PropTypes from "prop-types";
-
-import { noop } from "utils";
 
 import SwitcherIcon from "./SwitcherIcon";
 
-const Tree = ({
-  defaultCheckedKeys = [],
-  defaultExpandedKeys = [],
-  defaultSelectedKeys = [],
-  onCheck = noop,
-  treeData = [],
-  onSelect = noop,
-  showLine = true,
-  checkable = false,
-  draggable = false,
-  ...otherProps
-}) => (
+const Tree = props => (
   <TreeComponent
-    checkable={checkable}
-    className="neeto-ui-tree draggable-tree"
-    defaultCheckedKeys={defaultCheckedKeys}
-    defaultExpandedKeys={defaultExpandedKeys}
-    defaultSelectedKeys={defaultSelectedKeys}
-    draggable={draggable}
-    showLine={showLine}
     switcherIcon={SwitcherIcon}
-    treeData={treeData}
-    onCheck={onCheck}
-    onSelect={onSelect}
-    {...otherProps}
+    {...props}
+    className={classnames("neeto-ui-tree draggable-tree", props.className)}
   />
 );
 
-Tree.displayName = "Tree";
-
 Tree.propTypes = {
+  /**
+   * Whether to allow dropping on the node
+   */
+  allowDrop: PropTypes.bool,
+  /**
+   * Whether to automatically expand a parent treeNode
+   */
+  autoExpandParent: PropTypes.bool,
+  /**
+   * Whether treeNode fill remaining horizontal space
+   */
+  blockNode: PropTypes.bool,
   /**
    * Add a Checkbox before the treeNodes.
    */
   checkable: PropTypes.bool,
+  /**
+   * Specifies the keys of the checked treeNodes
+   */
+  checkedKeys: PropTypes.arrayOf(PropTypes.string),
+  /**
+   * Check treeNode precisely; parent treeNode and children treeNodes are not associated
+   */
+  checkStrictly: PropTypes.bool,
   /**
    * Specifies the keys of the default checked treeNodes.
    */
@@ -58,6 +55,10 @@ Tree.propTypes = {
    * Callback function for when the onCheck event occurs.
    */
   onCheck: PropTypes.func,
+  /**
+   * Whether disabled the tree
+   */
+  disabled: PropTypes.bool,
   /**
    * The treeNodes data Array, if set it then you need not to construct children TreeNode. (key should be unique across the whole array).
    */
@@ -102,6 +103,50 @@ Tree.propTypes = {
    * Callback function for when the onDrop event occurs
    */
   onDrop: PropTypes.func,
+  /**
+   * Defines a function to filter (highlight) treeNodes. When the function returns true, the corresponding treeNode will be highlighted
+   */
+  filterTreeNode: PropTypes.func,
+  /**
+   * Load data asynchronously
+   */
+  loadData: PropTypes.func,
+  /**
+   *  Set loaded tree nodes. Need work with loadData
+   */
+  loadedKeys: PropTypes.arrayOf(PropTypes.string),
+  /**
+   * Whether can be selected
+   */
+  selectable: PropTypes.bool,
+  /**
+   * Specifies the keys of the selected treeNodes, multiple selection needs to set multiple to true
+   */
+  selectedKeys: PropTypes.arrayOf(PropTypes.string),
+  /**
+   * Allows selecting multiple treeNodes
+   */
+  multiple: PropTypes.bool,
+  /**
+   * Customize tree node title render
+   */
+  titleRender: PropTypes.func,
+  /**
+   * Disable virtual scroll when set to false
+   */
+  virtual: PropTypes.bool,
+  /**
+   * Callback function for when a treeNode is expanded or collapsed
+   */
+  onExpand: PropTypes.func,
+  /**
+   * Callback function for when a treeNode is loaded
+   */
+  onLoad: PropTypes.func,
+  /**
+   * Customize collapse/expand icon of tree node
+   */
+  switcherIcon: PropTypes.node,
 };
 
 export default Tree;
