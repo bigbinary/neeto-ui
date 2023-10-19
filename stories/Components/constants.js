1c50e0e9a5f99237ebaa43c492b873e8c73cb213
Akhil Xavier <45327726+AkhilXavier95@users.noreply.github.com>
2023-10-19 19:17:12 +0530

    Added document for event bubbling and capturing for the `Dropdown` component. (#1892)
    * #1811 - add document for event bubbling and capturing
    
    * Update event bubbling example
    
    * #1811 - Update the example code

--- stories/Components/constants.js
+++ stories/Components/constants.js
@@ -36,3 +36,25 @@ export const PALETTE_PICKER_CODE = `const OnlyPalettePicker = () => {
     </div>
   );
 };`;
+
+export const EVENT_BUBBLING_CAPTURING = `const EventBubblingAndCapturing = () => {
+  const { Menu, MenuItem, Divider } = Dropdown;
+  const listItems = ["Action", "Another action", "Something else here"];
+
+  return (
+    <div
+    className="neeto-ui-rounded neeto-ui-shadow-md h-40 w-1/2 cursor-pointer border-2 border-solid p-5"
+    onClick={() => alert("Clicked on the card")}
+  >
+    <div className="w-10" onClick={event => event.stopPropagation()}>
+      <Dropdown label="Dropdown" {...args}>
+        <Menu>
+          <MenuItem.Button isActive>Active</MenuItem.Button>
+          <MenuItem.Button>Disabled</MenuItem.Button>
+          <MenuItem.Button style="danger">Delete</MenuItem.Button>
+        </Menu>
+      </Dropdown>
+    </div>
+  </div>
+  );
+};`;
