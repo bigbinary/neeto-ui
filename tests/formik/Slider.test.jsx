9b00d327233268e66c1e8a423cd3d6a4f4b0d022
Neenu Chacko <45137335+neenu-chacko@users.noreply.github.com>
2023-10-20 20:04:07 +0530

    Updated test

--- tests/formik/Slider.test.jsx
+++ tests/formik/Slider.test.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 import { render } from "@testing-library/react";
+import { Formik, Form } from "formik";
 
-import { Slider } from "components";
+import { Slider } from "components/formik";
 
 describe("Slider", () => {
   it("renders without errors", () => {
-    const { container } = render(<Slider name="slider" />);
+    const onSubmit = jest.fn();
+    const { container } = render(
+      <Formik initialValues={{ slider: 0 }} onSubmit={onSubmit}>
+        <Form>
+          <Slider name="slider" />
+        </Form>
+      </Formik>
+    );
     expect(container).toBeInTheDocument();
   });
 });
