8aa616a5f7c458ada6000572197699cf5dcf2e69
Goutham Subramanyam <gsn.goutham@gmail.com>
2023-10-23 18:46:44 +0530

    Added overrides using ConfigProvider and improved the UI

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,11 +1,11 @@
 import React, { useId } from "react";
 
-import { Slider as AntdSlider } from "antd";
+import { Slider as AntdSlider, ConfigProvider } from "antd";
 import PropTypes from "prop-types";
 
 import Label from "components/Label";
 import Typography from "components/Typography";
-import { hyphenize, noop } from "utils";
+import { ANT_DESIGN_GLOBAL_TOKEN_OVERRIDES, hyphenize, noop } from "utils";
 
 import { NEETO_UI_PRIMARY_500 } from "./constants";
 
@@ -28,49 +28,71 @@ const Slider = ({
   const helpTextId = `helpText_${id}`;
 
   return (
-    <div className="neeto-ui-slider__wrapper">
-      <div className="neeto-ui-slider__label-wrapper">
-        {label && (
-          <Label
-            data-cy={`${hyphenize(label)}-slider-label`}
-            htmlFor={id}
-            required={required}
-            {...labelProps}
+    <ConfigProvider
+      theme={{
+        token: { ...ANT_DESIGN_GLOBAL_TOKEN_OVERRIDES },
+        components: {
+          Slider: {
+            dotActiveBorderColor: "rgb(var(--neeto-ui-primary-500))",
+            dotBorderColor: "rgb(var(--neeto-ui-gray-500))",
+            handleActiveColor: "rgb(var(--neeto-ui-primary-600))",
+            handleColor: "rgb(var(--neeto-ui-primary-500))",
+            railBg: "rgb(var(--neeto-ui-gray-200))",
+            railHoverBg: "rgb(var(--neeto-ui-gray-300))",
+            trackBg: "rgb(var(--neeto-ui-primary-500))",
+            trackBgDisabled: "rgb(var(--neeto-ui-gray-100))",
+            trackHoverBg: "rgb(var(--neeto-ui-primary-600))",
+
+            // Global overrides
+            colorFillContentHover: "rgb(var(--neeto-ui-gray-600))",
+          },
+        },
+      }}
+    >
+      <div className="neeto-ui-input__wrapper">
+        <div className="neeto-ui-input__label-wrapper">
+          {label && (
+            <Label
+              data-cy={`${hyphenize(label)}-slider-label`}
+              htmlFor={id}
+              required={required}
+              {...labelProps}
+            >
+              {label}
+            </Label>
+          )}
+        </div>
+        <AntdSlider
+          {...{ max, min, value, defaultValue, onChange }}
+          className="neeto-ui-w-full"
+          handleStyle={{ backgroundColor: NEETO_UI_PRIMARY_500 }}
+          tooltip={{ formatter: null }}
+          trackStyle={{ backgroundColor: NEETO_UI_PRIMARY_500 }}
+          {...otherProps}
+          id={id}
+        />
+        {!!error && (
+          <Typography
+            className="neeto-ui-input__error"
+            data-cy={`${hyphenize(label)}-input-error`}
+            id={errorId}
+            style="body3"
           >
-            {label}
-          </Label>
+            {error}
+          </Typography>
+        )}
+        {helpText && (
+          <Typography
+            className="neeto-ui-input__help-text"
+            data-cy={`${hyphenize(label)}-input-help`}
+            id={helpTextId}
+            style="body3"
+          >
+            {helpText}
+          </Typography>
         )}
       </div>
-      <AntdSlider
-        {...{ max, min, value, defaultValue, onChange }}
-        className="w-full"
-        handleStyle={{ backgroundColor: NEETO_UI_PRIMARY_500 }}
-        tooltip={{ formatter: null }}
-        trackStyle={{ backgroundColor: NEETO_UI_PRIMARY_500 }}
-        {...otherProps}
-        id={id}
-      />
-      {!!error && (
-        <Typography
-          className="neeto-ui-input__error"
-          data-cy={`${hyphenize(label)}-input-error`}
-          id={errorId}
-          style="body3"
-        >
-          {error}
-        </Typography>
-      )}
-      {helpText && (
-        <Typography
-          className="neeto-ui-input__help-text"
-          data-cy={`${hyphenize(label)}-input-help`}
-          id={helpTextId}
-          style="body3"
-        >
-          {helpText}
-        </Typography>
-      )}
-    </div>
+    </ConfigProvider>
   );
 };
 
