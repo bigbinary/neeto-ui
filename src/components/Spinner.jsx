5d67ae8d0685b33fd3d1002bc92f5454d5543c7b
Goutham Subramanyam N <gsn.goutham@gmail.com>
2023-10-23 20:33:16 +0530

    Enhanced the customisability of Spinner component (#1903)
    * Enhanced the customisability of Spinner component
    
    * Added size prop for Spinner
    
    * Updated size prop as per review comments

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,40 +3,43 @@ import React from "react";
 import classnames from "classnames";
 import PropTypes from "prop-types";
 
-const SPINNER_THEMES = {
-  dark: "dark",
-  light: "light",
-};
+const SPINNER_THEMES = { dark: "dark", light: "light" };
+const SPINNER_SIZES = { small: "small", medium: "medium" };
 
-const Spinner = ({ theme = "dark", className = "" }) => {
-  const spinnerItemClassName = classnames(
-    "neeto-ui-spinner__item",
-    {
-      "neeto-ui-bg-gray-800": theme === "dark",
-    },
-    {
-      "neeto-ui-bg-white": theme === "light",
-    }
-  );
-
-  return (
-    <span
-      className={classnames("neeto-ui-spinner", [className])}
-      data-testid="spinner"
-    >
-      <i className={spinnerItemClassName} />
-      <i className={spinnerItemClassName} />
-      <i className={spinnerItemClassName} />
-      <i className={spinnerItemClassName} />
-    </span>
-  );
-};
+const Spinner = ({
+  theme = "dark",
+  size = SPINNER_SIZES.medium,
+  className = "",
+}) => (
+  <span
+    data-testid="spinner"
+    className={classnames(
+      "neeto-ui-spinner",
+      {
+        "neeto-ui-spinner--dark": theme === "dark",
+        "neeto-ui-spinner--light": theme === "light",
+        "neeto-ui-spinner--size-small": size === SPINNER_SIZES.small,
+        "neeto-ui-spinner--size-medium": size === SPINNER_SIZES.medium,
+      },
+      [className]
+    )}
+  >
+    <i className="neeto-ui-spinner__item" />
+    <i className="neeto-ui-spinner__item" />
+    <i className="neeto-ui-spinner__item" />
+    <i className="neeto-ui-spinner__item" />
+  </span>
+);
 
 Spinner.propTypes = {
   /**
    * To specify the theme of the Spinner.
    */
   theme: PropTypes.oneOf(Object.values(SPINNER_THEMES)),
+  /**
+   * To set the size of the spinner
+   */
+  size: PropTypes.oneOf(Object.values(SPINNER_SIZES)),
   /**
    * Provide external classnames to Spinner component.
    */
