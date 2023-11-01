aea0386d5a78033bf7bc1ae0a36ad607af11a910
Goutham Subramanyam <gsn.goutham@gmail.com>
2023-11-02 03:20:20 +0530

    Enhanced the customisability of Multi Email Input component

--- src/components/MultiEmailInput/index.jsx
+++ src/components/MultiEmailInput/index.jsx
@@ -123,8 +123,8 @@ const MultiEmailInput = forwardRef(
       !!filterInvalidEmails && value.length > getValidEmailsCount(value);
 
     return (
-      <div className="neeto-ui-flex neeto-ui-flex-col neeto-ui-email-input">
-        <div className="neeto-ui-flex neeto-ui-justify-between neeto-ui-email-input__title-row">
+      <div className="neeto-ui-input__wrapper neeto-ui-email-input__wrapper">
+        <div className="neeto-ui-email-input__label-wrapper">
           {label && (
             <Label
               className="neeto-ui-email-input__label"
@@ -161,7 +161,7 @@ const MultiEmailInput = forwardRef(
           value={value}
           visibleEmailsCount={visibleEmailsCount}
           className={classnames(
-            "neeto-ui-react-select__container neeto-ui-email-input__select",
+            "neeto-ui-react-select__container neeto-ui-react-select__container--medium neeto-ui-email-input__select",
             { "neeto-ui-react-select__container--error": !!error }
           )}
           styles={{
@@ -180,37 +180,38 @@ const MultiEmailInput = forwardRef(
           {...otherProps}
           {...overrideProps}
         />
-        <div className="neeto-ui-email-input__bottom-info">
-          {!!error && (
-            <Typography
-              className="neeto-ui-input__error"
-              data-cy={`${hyphenize(label)}-input-error`}
-              style="body3"
-            >
-              {error}
-              {isFilterEmailsLinkVisible && (
-                <span
-                  className="neeto-ui-typography neeto-ui-text-body3 neeto-ui-font-semibold cursor-pointer"
-                  onClick={handleFilterEmails}
-                >
-                  &nbsp;
-                  {filterInvalidEmails.label
-                    ? filterInvalidEmails.label
-                    : "Click here to remove invalid emails."}
-                </span>
-              )}
-            </Typography>
-          )}
-          {!!helpText && (
-            <Typography
-              className="neeto-ui-input__help-text"
-              data-cy={`${hyphenize(label)}-input-help`}
-              style="body3"
-            >
-              {helpText}
-            </Typography>
-          )}
-        </div>
+        {!!error && (
+          <Typography
+            className="neeto-ui-input__error"
+            data-cy={`${hyphenize(label)}-input-error`}
+            style="body3"
+          >
+            {error}
+            {isFilterEmailsLinkVisible && (
+              <Typography
+                className="cursor-pointer"
+                component="span"
+                style="body3"
+                weight="semibold"
+                onClick={handleFilterEmails}
+              >
+                &nbsp;
+                {filterInvalidEmails.label
+                  ? filterInvalidEmails.label
+                  : "Click here to remove invalid emails."}
+              </Typography>
+            )}
+          </Typography>
+        )}
+        {!!helpText && (
+          <Typography
+            className="neeto-ui-input__help-text"
+            data-cy={`${hyphenize(label)}-input-help`}
+            style="body3"
+          >
+            {helpText}
+          </Typography>
+        )}
       </div>
     );
   }
