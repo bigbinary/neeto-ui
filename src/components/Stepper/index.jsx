5d3ed7c6afc9e598214178016af75667a706510c
Goutham Subramanyam <gsn.goutham@gmail.com>
2023-11-03 03:13:54 +0530

    Added stepper component with examples

--- src/components/Stepper/index.jsx
+++ src/components/Stepper/index.jsx
@@ -1,5 +1,46 @@
 import React from "react";
 
-const Stepper = ({}) => <div>Stepper</div>;
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const Stepper = ({ steps, activeIndex, setActiveIndex }) => (
+  <div className="neeto-ui-stepper__wrapper">
+    {steps.map(({ id, label }, index) => (
+      <div className="neeto-ui-stepper-item__wrapper" key={id}>
+        <button
+          className={classnames("neeto-ui-stepper-item", {
+            "neeto-ui-stepper-item--active": index === activeIndex,
+            "neeto-ui-stepper-item--done": index < activeIndex,
+          })}
+          onClick={() => setActiveIndex(index)}
+        >
+          <span className="neeto-ui-stepper-item__stage">
+            <span>{id}</span>
+          </span>
+          <span className="neeto-ui-stepper-item__label" data-text={label}>
+            {label}
+          </span>
+        </button>
+      </div>
+    ))}
+  </div>
+);
+
+Stepper.propTypes = {
+  /**
+   * To provide the array of steps
+   */
+  steps: PropTypes.arrayOf(
+    PropTypes.shape({ id: PropTypes.string, label: PropTypes.string })
+  ),
+  /**
+   * To specify the active step
+   */
+  activeIndex: PropTypes.number,
+  /**
+   * To specify the function to set the active step
+   */
+  setActiveIndex: PropTypes.func,
+};
 
 export default Stepper;
--- stories/Components/Stepper.stories.jsx
+++ stories/Components/Stepper.stories.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Stepper from "components/Stepper";
 
@@ -13,11 +13,49 @@ const metadata = {
   },
 };
 
-const Default = args => <Stepper {...args} />;
+const STEPS = [
+  { id: 1, label: "Connect your calendar" },
+  { id: 2, label: "Connect conferencing app" },
+  { id: 3, label: "Set availability" },
+];
 
-Default.args = {};
+const STEPS_MAX = [
+  { id: 1, label: "Installation" },
+  { id: 2, label: "Customize widget" },
+  { id: 3, label: "Invite users" },
+  { id: 4, label: "Connect mailbox" },
+  { id: 5, label: "Connect" },
+];
+
+const Default = args => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <Stepper
+      activeIndex={activeIndex}
+      setActiveIndex={setActiveIndex}
+      {...args}
+    />
+  );
+};
+
+const Variant = args => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <Stepper
+      activeIndex={activeIndex}
+      setActiveIndex={setActiveIndex}
+      steps={STEPS_MAX}
+      {...args}
+    />
+  );
+};
+
+Default.args = { steps: STEPS };
 
 Default.storyName = "Stepper";
+Variant.storyName = "Stepper with 5 steps";
 
-export { Default };
+export { Default, Variant };
 export default metadata;
