bbe69048569bfa385d50e49e15654a3f3d6697d8
vijay2136 <imvijay2136@gmail.com>
2023-11-07 17:12:19 +0530

    Fixed jest config

--- tests/Pagination.test.jsx
+++ tests/Pagination.test.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
 
 import { Pagination } from "components";
+import { getQueryParams } from "utils";
 
 const currentPage = 3;
 const count = 1000;
@@ -12,7 +14,7 @@ const pageSize = 100;
 describe("Pagination", () => {
   it("should render current page number without error", () => {
     const { getByText } = render(
-      <Pagination count={count} pageNo={currentPage} pageSize={pageSize} />
+      <Pagination {...{ count, pageSize }} pageNo={currentPage} />
     );
     expect(getByText(currentPage)).toBeInTheDocument();
   });
@@ -20,12 +22,7 @@ describe("Pagination", () => {
   it("should invoke navigate callback when the left navigate button is clicked.", () => {
     const navigate = jest.fn();
     const { getByTestId } = render(
-      <Pagination
-        count={count}
-        navigate={navigate}
-        pageNo={currentPage}
-        pageSize={pageSize}
-      />
+      <Pagination {...{ count, navigate, pageSize }} pageNo={currentPage} />
     );
     userEvent.click(getByTestId("left-navigate-button"));
     expect(navigate).toHaveBeenCalledTimes(1);
@@ -35,12 +32,7 @@ describe("Pagination", () => {
   it("should invoke navigate callback when the right navigate button is clicked.", () => {
     const navigate = jest.fn();
     const { getByTestId } = render(
-      <Pagination
-        count={count}
-        navigate={navigate}
-        pageNo={currentPage}
-        pageSize={pageSize}
-      />
+      <Pagination {...{ count, navigate, pageSize }} pageNo={currentPage} />
     );
     userEvent.click(getByTestId("right-navigate-button"));
     expect(navigate).toHaveBeenCalledTimes(1);
@@ -50,12 +42,7 @@ describe("Pagination", () => {
   it("should invoke navigate callback when any page number is clicked.", () => {
     const navigate = jest.fn();
     const { getByText } = render(
-      <Pagination
-        count={count}
-        navigate={navigate}
-        pageNo={currentPage}
-        pageSize={pageSize}
-      />
+      <Pagination {...{ count, navigate, pageSize }} pageNo={currentPage} />
     );
     userEvent.click(getByText("4"));
     expect(navigate).toHaveBeenCalledTimes(1);
@@ -64,14 +51,14 @@ describe("Pagination", () => {
 
   it("should not render anything when currentPage is 0", () => {
     const { container } = render(
-      <Pagination count={count} pageNo={0} pageSize={pageSize} />
+      <Pagination {...{ count, pageSize }} pageNo={0} />
     );
     expect(container.firstChild).toBeNull();
   });
 
   it("should render only left dots when there are more than 1 page between the extremes of left sibling and the page limit", () => {
     const { getByTestId } = render(
-      <Pagination count={count} pageNo={7} pageSize={pageSize} />
+      <Pagination {...{ count, pageSize }} pageNo={7} />
     );
     const dots = getByTestId("dots");
     expect(dots).toBeInTheDocument();
@@ -79,7 +66,7 @@ describe("Pagination", () => {
 
   it("should render only right dots when there are more than 1 page between the extremes of right sibling and the page limit", () => {
     const { getByTestId } = render(
-      <Pagination count={count} pageNo={3} pageSize={pageSize} />
+      <Pagination {...{ count, pageSize }} pageNo={3} />
     );
     const dots = getByTestId("dots");
     expect(dots).toBeInTheDocument();
@@ -87,7 +74,7 @@ describe("Pagination", () => {
 
   it("should render both left and right dots when there are more than 1 page between the extremes of both siblings and the page limit", () => {
     const { getAllByTestId } = render(
-      <Pagination count={count} pageNo={5} pageSize={pageSize} />
+      <Pagination {...{ count, pageSize }} pageNo={5} />
     );
     const dots = getAllByTestId("dots");
     expect(dots.length).toBe(2);
@@ -95,9 +82,20 @@ describe("Pagination", () => {
 
   it("should not render any dots if the number of pages is less than the page numbers we want to show ", () => {
     const { queryByTestId } = render(
-      <Pagination count={500} pageNo={currentPage} pageSize={pageSize} />
+      <Pagination {...{ pageSize }} count={500} pageNo={currentPage} />
     );
     const dots = queryByTestId("dots");
     expect(dots).toBeNull();
+  });
+
+  it("should set query params when any page number is clicked provided that navigate fn is not passed", () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Pagination {...{ count, pageSize }} pageNo={currentPage} />
+      </BrowserRouter>
+    );
+    userEvent.click(getByText("4"));
+    const queryParams = getQueryParams();
+    expect(queryParams).toEqual({ page: "4", page_size: "100" });
   });
 });
