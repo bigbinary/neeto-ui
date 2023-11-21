172e0f5995440c3ad4f3a338f244f30eb3e7c27f
Goutham Subramanyam <gsn.goutham@gmail.com>
2023-11-21 21:06:50 +0530

    Enhanced the customisability of TimePickerInput component

--- stories/Components/TimePickerInput.stories.jsx
+++ stories/Components/TimePickerInput.stories.jsx
@@ -19,5 +19,27 @@ Default.args = { label: "Time picker" };
 
 Default.storyName = "Time Picker Input";
 
-export { Default };
+const Sizes = args => (
+  <div className="flex w-full flex-col gap-3">
+    <TimePickerInput
+      {...args}
+      label="Small"
+      placeholder="Time Input placeholder"
+      size="small"
+    />
+    <TimePickerInput
+      {...args}
+      label="Medium"
+      placeholder="Time Input placeholder"
+    />
+    <TimePickerInput
+      {...args}
+      label="Large"
+      placeholder="Time Input placeholder"
+      size="large"
+    />
+  </div>
+);
+
+export { Default, Sizes };
 export default metadata;
