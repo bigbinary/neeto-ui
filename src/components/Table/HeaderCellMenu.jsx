89509dd8b0473a6e4ebb45f2fcce5849451871cd
Deepak Jose <deepakjosp@gmail.com>
2023-11-22 14:28:35 +0530

    #1992 - Added support to show columnInfo if present in the header menu.

--- src/components/Table/HeaderCellMenu.jsx
+++ src/components/Table/HeaderCellMenu.jsx
@@ -1,43 +1,79 @@
-import React from "react";
+import React, { useRef } from "react";
 
+import { isPresent } from "neetocist";
 import { CheckCircle, MenuVertical } from "neetoicons";
 
-import { Dropdown } from "components";
+import { Dropdown, Popover, Typography } from "components";
 
 const { Menu, MenuItem } = Dropdown;
 
-const HeaderCellMenu = ({ onSort, columnKey, sortedInfo }) => (
-  <div onClick={event => event.stopPropagation()}>
-    <Dropdown
-      className="flex"
-      icon={MenuVertical}
-      strategy="fixed"
-      buttonProps={{
-        className: "min-h-0 flex-shrink-0",
-        style: "text",
-        size: "small",
-      }}
-    >
-      <Menu>
-        <MenuItem.Button
-          className="flex items-center justify-between"
-          onClick={() => onSort(columnKey, "ascend")}
-        >
-          <span>Ascending</span>
-          {sortedInfo.order === "ascend" &&
-            sortedInfo.columnKey === columnKey && <CheckCircle size={14} />}
-        </MenuItem.Button>
-        <MenuItem.Button
-          className="flex items-center justify-between"
-          onClick={() => onSort(columnKey, "descend")}
-        >
-          <span>Desceding</span>
-          {sortedInfo.order === "descend" &&
-            sortedInfo.columnKey === columnKey && <CheckCircle size={14} />}
-        </MenuItem.Button>
-      </Menu>
-    </Dropdown>
-  </div>
-);
+const HeaderCellMenu = ({
+  onSort,
+  columnKey,
+  sortedInfo,
+  isSortable,
+  columnDescription,
+}) => {
+  const columnInfoButtonReference = useRef();
+
+  return (
+    <div onClick={event => event.stopPropagation()}>
+      <Dropdown
+        className="flex"
+        icon={MenuVertical}
+        strategy="fixed"
+        buttonProps={{
+          className: "min-h-0 flex-shrink-0",
+          style: "text",
+          size: "medium",
+        }}
+      >
+        <Menu>
+          {isSortable && (
+            <>
+              <MenuItem.Button
+                className="flex items-center justify-between"
+                onClick={() => onSort(columnKey, "ascend")}
+              >
+                <span>Ascending</span>
+                {sortedInfo.order === "ascend" &&
+                  sortedInfo.columnKey === columnKey && (
+                    <CheckCircle size={14} />
+                  )}
+              </MenuItem.Button>
+              <MenuItem.Button
+                className="flex items-center justify-between"
+                onClick={() => onSort(columnKey, "descend")}
+              >
+                <span>Desceding</span>
+                {sortedInfo.order === "descend" &&
+                  sortedInfo.columnKey === columnKey && (
+                    <CheckCircle size={14} />
+                  )}
+              </MenuItem.Button>
+            </>
+          )}
+          {isPresent(columnDescription) && (
+            <>
+              <MenuItem.Button ref={columnInfoButtonReference}>
+                Column info
+              </MenuItem.Button>
+              <Popover position="right" reference={columnInfoButtonReference}>
+                <Typography
+                  className="w-72 normal-case"
+                  lineHeight="normal"
+                  style="body2"
+                  weight="normal"
+                >
+                  {columnDescription}
+                </Typography>
+              </Popover>
+            </>
+          )}
+        </Menu>
+      </Dropdown>
+    </div>
+  );
+};
 
 export default HeaderCellMenu;
