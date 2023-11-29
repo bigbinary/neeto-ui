f962c8e90533d2c8ce004d59b8af532199b60aaa
ittyjose <ittyjose@gmail.com>
2023-11-29 13:36:44 +0530

    mved hover icon to timepicker component

--- src/components/TimePickerInput/HoverIcon.jsx
+++ src/components/TimePickerInput/HoverIcon.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 
 import { Clock, Close } from "neetoicons";
-import PropTypes from "prop-types";
 
-const HoverIcon = ({ time }) => {
+const HoverIcon = () => {
   const [hovered, setHovered] = useState(false);
 
   return (
@@ -11,16 +10,9 @@ const HoverIcon = ({ time }) => {
       onMouseLeave={() => setHovered(false)}
       onMouseOver={() => setHovered(true)}
     >
-      {hovered && time ? <Close size={16} /> : <Clock size={16} />}
+      {hovered ? <Close size={16} /> : <Clock size={16} />}
     </div>
   );
-};
-
-HoverIcon.propTypes = {
-  /**
-   * To specify the time value.
-   */
-  time: PropTypes.string,
 };
 
 export default HoverIcon;
