ba6aff7a820751d73a2c770c67701b2d80d2d553
Suraj Kumar Choudhury <suraj.choudhury@bigbinary.com>
2024-01-02 23:08:26 +0530

    add test for onTimeInputBlur

--- tests/TimePickerInput.test.jsx
+++ tests/TimePickerInput.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 
 import { TimePickerInput } from "components";
 
@@ -10,5 +10,13 @@ describe("TimePicker", () => {
   it("should render without error", () => {
     render(<TimePickerInput label="Select Time" />);
     expect(getByText("Select Time")).toBeInTheDocument();
+  });
+
+  it("should trigger onBlur on losing focus", async () => {
+    const onTimeInputBlur = jest.fn(() => true);
+    render(<TimePickerInput label="Select Time" onBlur={onTimeInputBlur} />);
+    const timePickerInputs = screen.getAllByRole("spinbutton");
+    await fireEvent.blur(timePickerInputs[0]);
+    expect(onTimeInputBlur).toHaveBeenCalled();
   });
 });
