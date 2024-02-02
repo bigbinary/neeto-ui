b18c1925afc0124089e034fa052db75ce3b42a72
Farhan ck <farhan.latheef1@gmail.com>
2024-02-02 16:11:35 +0530

    fix lint errors

--- tests/DateTimePicker.test.jsx
+++ tests/DateTimePicker.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import dayjs from "dayjs";
 
 import DateTimePicker from "components/DateTimePicker";
