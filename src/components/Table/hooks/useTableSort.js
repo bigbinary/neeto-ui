dec8c2045647a7b04e771037f8420c05de5b5731
Farhan ck <farhan.latheef1@gmail.com>
2024-03-04 16:04:19 +0530

    Table: update sortedInfo from queryParams

--- src/components/Table/hooks/useTableSort.js
+++ src/components/Table/hooks/useTableSort.js
@@ -1,16 +1,40 @@
-import { camelToSnakeCase } from "neetocist";
+import { useState } from "react";
+
+import { camelToSnakeCase, isPresent, snakeToCamelCase } from "neetocist";
 import { mergeLeft } from "ramda";
 import { useHistory } from "react-router-dom";
 
 import { useQueryParams } from "hooks";
 import { buildUrl } from "utils";
 
-import { URL_SORT_ORDERS } from "../constants";
+import { URL_SORT_ORDERS, TABLE_SORT_ORDERS } from "../constants";
+
+const getSortInfoFromQueryParams = queryParams => {
+  const sortedInfo = {};
+  if (
+    isPresent(queryParams.sort_by) &&
+    isPresent(queryParams.order_by) &&
+    isPresent(TABLE_SORT_ORDERS[queryParams.order_by])
+  ) {
+    sortedInfo.field = snakeToCamelCase(queryParams.sort_by);
+    sortedInfo.order = TABLE_SORT_ORDERS[queryParams.order_by];
+  }
+
+  return sortedInfo;
+};
 
 const useTableSort = () => {
   const queryParams = useQueryParams();
+  const [sortedInfo, setSortedInfo] = useState(
+    getSortInfoFromQueryParams(queryParams)
+  );
 
   const history = useHistory();
+
+  const tt = {
+    field: queryParams.sort_by,
+    order: TABLE_SORT_ORDERS[queryParams.order_by],
+  };
 
   const handleTableChange = (pagination, sorter) => {
     const params = {
@@ -23,7 +47,7 @@ const useTableSort = () => {
     history.push(buildUrl(pathname, mergeLeft(params, queryParams)));
   };
 
-  return { handleTableChange };
+  return { handleTableChange, sortedInfo, setSortedInfo };
 };
 
 export default useTableSort;
