29849fa24b65a2ab39c3b255f0a4e094adb76094
vijay2136 <imvijay2136@gmail.com>
2024-03-27 11:52:59 +0530

    Code improvements

--- src/components/formik/ActionBlock.jsx
+++ src/components/formik/ActionBlock.jsx
@@ -23,7 +23,7 @@ const ActionBlock = ({
   const isSubmitting = isFormSubmitting ?? isFormikSubmitting;
 
   return (
-    <div className={classnames(["flex items-center gap-x-2", className])}>
+    <div className={classnames(["neeto-ui-action-block__wrapper", className])}>
       <SubmitButton
         data-cy="save-changes-button"
         data-test-id="save-changes-button"
