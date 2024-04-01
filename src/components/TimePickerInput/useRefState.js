756fa68a23a291c091a9d7961a36a66db49be403
Farhan ck <farhan.latheef1@gmail.com>
2024-04-01 06:00:28 +0530

    lint fix

--- src/components/TimePickerInput/useRefState.js
+++ src/components/TimePickerInput/useRefState.js
@@ -2,6 +2,7 @@ import { useState, useRef } from "react";
 
 // A wrapper hook around useState, that updates a ref as well, useful when dealing directly with dom event listeners.
 const useRefState = val => {
+  // eslint-disable-next-line react/hook-use-state
   const [value, _setValue] = useState(val);
   const valueRef = useRef(value);
 
