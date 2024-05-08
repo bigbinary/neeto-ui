c27bc7c431dec33bebe2d86de3253579d918b060
Deepak Jose <deepakjosp@gmail.com>
2024-05-08 20:45:19 +0530

    Fixed failing test.

--- tests/Avatar.test.jsx
+++ tests/Avatar.test.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import { Avatar } from "components";
 
 describe("Avatar", () => {
   it("should render without error", () => {
-    render(<Avatar />);
-    const avatarImg = screen.getByRole("img");
-    fireEvent.load(avatarImg);
-    expect(avatarImg).toBeInTheDocument();
+    render(<Avatar user={{ name: "neeto UI" }} />);
+    const avatarFallback = screen.getByText("ne");
+    expect(avatarFallback).toBeInTheDocument();
   });
 
   it("should call onClick when the avatar is clicked", async () => {
     const onClick = jest.fn();
-    render(<Avatar user={{ name: "neeto UI" }} onClick={onClick} />);
+    render(<Avatar {...{ onClick }} user={{ name: "neeto UI" }} />);
     const avatar = screen.getByTestId("avatar");
     await userEvent.click(avatar);
     expect(onClick).toBeCalledTimes(1);
@@ -25,10 +24,7 @@ describe("Avatar", () => {
     render(
       <Avatar
         status="online"
-        user={{
-          name: "John Doe",
-          imageUrl: "not-found.png",
-        }}
+        user={{ name: "John Doe", imageUrl: "not-found.png" }}
       />
     );
     expect(screen.getByTestId("indicator")).toBeInTheDocument();
