25c0b2e1ae900ca2f2075d5cb47189e4ddec8c34
Gaagul Gigi <gaagulgigi@gmail.com>
2024-07-12 01:13:39 +0530

    Minor updates

--- src/components/DatePicker/Today.jsx
+++ src/components/DatePicker/Today.jsx
@@ -1,17 +1,22 @@
+/* eslint-disable @bigbinary/neeto/no-dumb-components-with-use-translation */
 import React from "react";
 
-import { withT } from "neetocommons/react-utils";
+import { useTranslation } from "react-i18next";
 
-const Today = withT(({ t, onClick }) => (
-  <div className="text-center">
-    <button
-      {...{ onClick }}
-      className="neeto-ui-rounded-md hover:neeto-ui-bg-gray-200 px-2 py-1 text-xs font-medium transition duration-300 ease-in-out"
-      data-cy="year-month-mode-today"
-    >
-      {t("neetoui.datePicker.today")}
-    </button>
-  </div>
-));
+const Today = ({ onClick }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="text-center">
+      <button
+        {...{ onClick }}
+        className="neeto-ui-rounded-md hover:neeto-ui-bg-gray-200 px-2 py-1 text-xs font-medium transition duration-300 ease-in-out"
+        data-cy="year-month-mode-today"
+      >
+        {t("neetoui.datePicker.today")}
+      </button>
+    </div>
+  );
+};
 
 export default Today;
