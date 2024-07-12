1dc451b06077613bed4b227b820b2ec77fe61572
Gaagul Gigi <gaagulgigi@gmail.com>
2024-07-13 00:31:41 +0530

    Added support for spanish and french

--- src/components/TranslationProvider/i18n.js
+++ src/components/TranslationProvider/i18n.js
@@ -1,13 +1,19 @@
 /* eslint-disable @bigbinary/neeto/file-name-and-export-name-standards */
+/* eslint-disable import/extensions */
 
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-// eslint-disable-next-line import/extensions
 import enTranslations from "src/translations/en.json";
+import esTranslations from "src/translations/es.json";
+import frTranslations from "src/translations/fr.json";
 
 i18n.use(initReactI18next).init({
-  resources: { en: { translation: enTranslations } },
+  resources: {
+    en: { translation: enTranslations },
+    es: { translation: esTranslations },
+    fr: { translation: frTranslations },
+  },
   lang: "en",
   fallbackLng: "en",
   interpolation: { escapeValue: false },
