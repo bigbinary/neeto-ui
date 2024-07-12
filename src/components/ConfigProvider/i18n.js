46d01f476be7e266ecde5ab31169587fd512ee2e
Gaagul Gigi <gaagulgigi@gmail.com>
2024-07-12 22:39:14 +0530

    Updated new provider

--- src/components/ConfigProvider/i18n.js
+++ src/components/ConfigProvider/i18n.js
@@ -3,6 +3,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+// eslint-disable-next-line import/extensions
 import enTranslations from "src/translations/en.json";
 
 i18n.use(initReactI18next).init({
