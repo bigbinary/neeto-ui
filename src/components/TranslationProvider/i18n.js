03a1fe14c0b7069c0fec4900950e6b4058a3dd70
Gaagul Gigi <70290286+gaagul@users.noreply.github.com>
2024-07-15 12:15:27 +0530

    Added additional languages and Updated story

--- src/components/TranslationProvider/i18n.js
+++ src/components/TranslationProvider/i18n.js
@@ -4,9 +4,13 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import deTranslations from "src/translations/de.json";
 import enTranslations from "src/translations/en.json";
 import esTranslations from "src/translations/es.json";
 import frTranslations from "src/translations/fr.json";
+import nlTranslations from "src/translations/nl.json";
+import plTranslations from "src/translations/pl.json";
+import ptTranslations from "src/translations/pt.json";
 
 export const initializeI18n = () => {
   i18n.use(initReactI18next).init({
@@ -14,6 +18,10 @@ export const initializeI18n = () => {
       en: { translation: enTranslations },
       es: { translation: esTranslations },
       fr: { translation: frTranslations },
+      de: { translation: deTranslations },
+      nl: { translation: nlTranslations },
+      pl: { translation: plTranslations },
+      pt: { translation: ptTranslations },
     },
     lang: "en",
     fallbackLng: "en",
