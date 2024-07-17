66136732e404eaa34d630f6fe3d6e5025f7d8add
Deepak Jose <deepakjosp@gmail.com>
2024-07-17 19:39:24 +0530

    Updated FormikTextarea component to use status value to show error if present and reset it on change.

--- src/formik/Textarea.jsx
+++ src/formik/Textarea.jsx
@@ -1,18 +1,31 @@
 import React, { forwardRef } from "react";
 
-import { Field } from "formik";
+import { Field, getIn } from "formik";
 import PropTypes from "prop-types";
 
 import Textarea from "components/Textarea";
 
 const FormikTextarea = forwardRef(({ name, ...rest }, ref) => (
   <Field {...{ name }}>
-    {({ field, meta }) => (
-      <Textarea
-        error={meta.touched ? meta.error : ""}
-        {...{ ref, ...field, ...rest }}
-      />
-    )}
+    {({ field, meta, form }) => {
+      const { status, setStatus } = form;
+      const fieldStatus = getIn(status, name);
+
+      const fieldProps = {
+        ...field,
+        onChange: e => {
+          setStatus({ ...status, [name]: null });
+          field.onChange(e);
+        },
+      };
+
+      return (
+        <Textarea
+          error={meta.touched ? meta.error || fieldStatus : ""}
+          {...{ ref, ...fieldProps, ...rest }}
+        />
+      );
+    }}
   </Field>
 ));
 
