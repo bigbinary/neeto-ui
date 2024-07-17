fea6645b6af11ab4df17d550d8792f14dbe5f045
Deepak Jose <deepakjosp@gmail.com>
2024-07-17 19:20:25 +0530

    Updated FormikSlider component to use status value to show error if present and reset it on change.

--- src/formik/Slider.jsx
+++ src/formik/Slider.jsx
@@ -1,18 +1,24 @@
 import React, { forwardRef } from "react";
 
-import { useField } from "formik";
+import { getIn, useField, useFormikContext } from "formik";
 
 import Slider from "components/Slider";
 
 const FormikSlider = forwardRef(({ name, ...otherProps }, ref) => {
   const [field, meta, { setValue, setTouched }] = useField(name);
+  const { status, setStatus } = useFormikContext();
+
+  const fieldStatus = getIn(status, name);
 
   return (
     <Slider
-      error={meta.error || ""}
+      error={meta.error || fieldStatus || ""}
       {...{ ref, ...field, name }}
       onBlur={() => setTouched(true)}
-      onChange={value => setValue(value)}
+      onChange={value => {
+        setStatus({ ...status, [name]: null });
+        setValue(value);
+      }}
       {...otherProps}
     />
   );
