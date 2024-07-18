4564f4583b041021a70dd002ee12fb6571502766
Deepak Jose <deepakjosp@gmail.com>
2024-07-18 10:03:39 +0530

    Updated FormikRadio component to use status value to show error if present and reset it on change.

--- src/formik/Radio.jsx
+++ src/formik/Radio.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 
-import { useField, useFormikContext } from "formik";
+import { useField, useFormikContext, getIn } from "formik";
 
 import Radio from "components/Radio";
 
 const RadioGroup = ({ label, name, className = "", ...props }) => {
-  const { setFieldValue } = useFormikContext();
+  const { setFieldValue, status, setStatus } = useFormikContext();
   const [field, meta] = useField({ name });
+
+  const fieldStatus = getIn(status, name);
+
+  const fieldProps = {
+    ...field,
+    onChange: event => {
+      setStatus({ ...status, [name]: null });
+      setFieldValue(name, event.target.value);
+    },
+  };
 
   return (
     <Radio
-      error={meta.touched ? meta.error : ""}
-      onChange={event => setFieldValue(name, event.target.value)}
-      {...{ className, label, ...field, ...props }}
+      error={meta.touched ? meta.error || fieldStatus : ""}
+      {...{ className, label, ...fieldProps, ...props }}
     />
   );
 };
