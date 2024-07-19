627d511bb1bf34f2ce9d1e68b499dbe0af282196
Joseph Mathew <josephmathew900@gmail.com>
2024-07-19 07:23:41 +0530

    Removed TimePickerInput and DateTimePicker components

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,6 @@ import Callout from "./Callout";
 import Checkbox from "./Checkbox";
 import ColorPicker from "./ColorPicker";
 import DatePicker from "./DatePicker";
-import DateTimePicker from "./DateTimePicker";
 import Dropdown from "./Dropdown";
 import Input from "./Input";
 import Kbd from "./Kbd";
@@ -30,7 +29,6 @@ import Table from "./Table";
 import Tag from "./Tag";
 import Textarea from "./Textarea";
 import TimePicker from "./TimePicker";
-import TimePickerInput from "./TimePickerInput";
 import Toastr from "./Toastr";
 import Tooltip from "./Tooltip";
 import TranslationProvider from "./TranslationProvider";
@@ -50,7 +48,6 @@ export {
   Checkbox,
   ColorPicker,
   DatePicker,
-  DateTimePicker,
   Dropdown,
   MultiEmailInput,
   Input,
@@ -73,7 +70,6 @@ export {
   Tag,
   Textarea,
   TimePicker,
-  TimePickerInput,
   Toastr,
   Tooltip,
   Tree,
