37ef09222d69e0449464675a35fff7d5d650c1c8
Deepak Jose <deepakjosp@gmail.com>
2024-07-22 17:23:39 +0530

    Fixed the incorrect usage if dissoc in formik fields.

--- src/formik/Textarea.jsx
+++ src/formik/Textarea.jsx
@@ -9,13 +9,13 @@ import Textarea from "components/Textarea";
 const FormikTextarea = forwardRef(({ name, ...rest }, ref) => (
   <Field {...{ name }}>
     {({ field, meta, form }) => {
-      const { status, setStatus } = form;
+      const { status = {}, setStatus } = form;
       const fieldStatus = getIn(status, name);
 
       const fieldProps = {
         ...field,
         onChange: e => {
-          setStatus(dissoc(name));
+          setStatus(dissoc(name, status));
           field.onChange(e);
         },
       };
