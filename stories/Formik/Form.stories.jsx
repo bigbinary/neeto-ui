c7d04af4b31b68acf2fe07dc1e191f114efa08bf
Deepak Jose <deepakjosp@gmail.com>
2024-07-26 10:06:22 +0530

    Added the server side validation errors story with a demo.

--- stories/Formik/Form.stories.jsx
+++ stories/Formik/Form.stories.jsx
@@ -6,6 +6,7 @@ import { Input, Button } from "formikcomponents";
 import Form from "formikcomponents/Form";
 
 import FormStoriesDocs from "!raw-loader!./FormStories.mdx";
+import ServerValidationErrorDocs from "!raw-loader!./ServerValidationErrors.mdx";
 
 const metadata = {
   title: "Formik/Form",
@@ -45,6 +46,50 @@ const FormikStory = args => (
 FormikStory.storyName = "Form";
 FormikStory.args = { scrollToErrorField: true };
 
-export { FormikStory };
+const HandlingServerValidationErrors = args => {
+  const handleSubmit = (_, actions) =>
+    new Promise(resolve => {
+      setTimeout(() => {
+        actions.setStatus({ username: "Username is already taken!" });
+        actions.setSubmitting(false);
+        resolve();
+      }, 2000);
+    });
+
+  return (
+    <Form
+      formikProps={{
+        initialValues: { firstName: "", lastName: "", email: "" },
+        validationSchema: yup.object().shape({
+          firstName: yup.string().required(),
+          username: yup.string().required(),
+        }),
+        onSubmit: handleSubmit,
+      }}
+      {...args}
+    >
+      <div className="flex h-auto w-full flex-col items-start gap-4 p-6">
+        <Input required className="w-80" label="First name" name="firstName" />
+        <Input required className="w-80" label="Last name" name="lastName" />
+        <Input required className="w-80" label="Username" name="username" />
+        <Button
+          className="w-20"
+          disabled={false}
+          label="Submit"
+          type="submit"
+        />
+      </div>
+    </Form>
+  );
+};
+
+HandlingServerValidationErrors.storyName =
+  "Handling server side validation errors";
+
+HandlingServerValidationErrors.parameters = {
+  docs: { description: { story: ServerValidationErrorDocs } },
+};
+
+export { FormikStory, HandlingServerValidationErrors };
 
 export default metadata;
