4e97c8d3dd4e398d39e323d0288cb5faa2d16ea3
Deepak Jose <deepak.jose@bigbinary.com>
2024-08-09 19:41:02 +0530

    Creates a playground for neetoUI in the storybook. (#2282)
    * Removed the addon @storybook/addon-postcss because of the post css version mismatch and the plugin was unsupported, added monaco editor and react-live packages.
    
    * Created a story with the live editor and added necessary documentation.
    
    * Cleaned up the CSS and UI.
    
    * Added documentation about the usage of react hooks in the editor.
    
    * Renamed the story to LivePlayground

---
+++ stories/Introduction/LivePlayground.stories.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+import { LiveProvider, LivePreview } from "react-live";
+import MonacoEditor from "react-monaco-editor";
+
+import * as atoms from "atoms";
+import * as components from "components";
+import * as FormikComponents from "formikcomponents";
+import * as hooks from "hooks";
+
+import LivePlaygroundDocs from "!raw-loader!./LivePlaygroundDocs/LivePlaygroundUsage.mdx";
+
+const LivePlayground = () => {
+  const [code, setCode] = useState("\n");
+
+  return (
+    <div className="flex h-full flex-col">
+      <div className="w-full flex-grow">
+        <MonacoEditor
+          height="300px"
+          language="jsx"
+          theme="vs-dark"
+          value={code}
+          onChange={setCode}
+        />
+      </div>
+      <h3 className="p-4">Preview</h3>
+      <div
+        className="min-h-96 m-4 mt-0 flex-grow overflow-y-auto border-2 p-4"
+        style={{ minHeight: "300px" }}
+      >
+        <LiveProvider
+          {...{ code }}
+          scope={{ ...components, ...atoms, ...hooks, FormikComponents, React }}
+        >
+          <LivePreview />
+        </LiveProvider>
+      </div>
+    </div>
+  );
+};
+
+const metadata = {
+  title: "Live Playground",
+  component: LivePlayground,
+  parameters: { docs: { description: { component: LivePlaygroundDocs } } },
+};
+
+export default metadata;
+
+export { LivePlayground };
