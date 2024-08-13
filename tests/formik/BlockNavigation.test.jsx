b28806ad5020eac1c65ee53a1a1e2c1b3f928df9
Deepak Jose <deepakjosp@gmail.com>
2024-08-13 14:53:11 +0530

    Added test to verify the save and continue feature in the BlockNavigation.

--- tests/formik/BlockNavigation.test.jsx
+++ tests/formik/BlockNavigation.test.jsx
@@ -11,7 +11,7 @@ import { Input } from "components";
 import BlockNavigation from "formikcomponents/BlockNavigation";
 import Form from "formikcomponents/Form";
 
-const TestComponent = () => <div>test page</div>;
+const TestComponent = () => <div>Test page</div>;
 const mockSubmit = jest.fn();
 const TestForm = ({ isDirty }) => (
   <>
@@ -53,14 +53,14 @@ describe("formik/BlockNavigation", () => {
     render(<TestBlockNavigation />);
 
     expect(screen.getByRole("textbox")).toBeInTheDocument();
-    expect(screen.queryByText(/test page/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Test page/i)).not.toBeInTheDocument();
   });
 
   it("should allow navigation when form is empty", async () => {
     render(<TestBlockNavigation />);
 
     await userEvent.click(screen.getByRole("link"));
-    expect(screen.getByText(/test page/i)).toBeInTheDocument();
+    expect(screen.getByText(/Test page/i)).toBeInTheDocument();
   });
 
   it("should not allow navigation when form isn't empty", async () => {
@@ -70,14 +70,14 @@ describe("formik/BlockNavigation", () => {
     await userEvent.type(input, "test");
     await userEvent.click(screen.getByRole("link"));
 
-    expect(screen.queryByText(/test page/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Test page/i)).not.toBeInTheDocument();
   });
 
   it("should not allow navigation if isDirty prop is true", async () => {
     render(<TestBlockNavigation isDirty />);
 
     await userEvent.click(screen.getByRole("link"));
-    expect(screen.queryByText(/test page/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Test page/i)).not.toBeInTheDocument();
   });
 
   it("should display an Alert modal with Continue and Cancel buttons", async () => {
@@ -115,9 +115,9 @@ describe("formik/BlockNavigation", () => {
 
     const input = screen.getByRole("textbox");
     await userEvent.type(input, repeat("{backspace}", "test".length).join(""));
-    await userEvent.type(input, "testing discard changes");
+    await userEvent.type(input, "Testing discard changes");
 
-    expect(input.value).toBe("testing discard changes");
+    expect(input.value).toBe("Testing discard changes");
 
     await userEvent.click(screen.getByRole("link"));
 
@@ -127,7 +127,28 @@ describe("formik/BlockNavigation", () => {
     await userEvent.click(continueButton);
 
     await waitFor(() => expect(continueButton).not.toBeInTheDocument());
-    expect(screen.getByText(/test page/i)).toBeInTheDocument();
+    expect(screen.getByText(/Test page/i)).toBeInTheDocument();
     expect(mockSubmit).not.toHaveBeenCalled();
+  });
+
+  it("should allow navigation and save the form if the Save and continue button is clicked", async () => {
+    render(<TestBlockNavigation isDirty />);
+
+    const input = screen.getByRole("textbox");
+    await userEvent.type(input, repeat("{backspace}", "test".length).join(""));
+    await userEvent.type(input, "Testing save changes");
+
+    expect(input.value).toBe("Testing save changes");
+
+    await userEvent.click(screen.getByRole("link"));
+
+    const saveButton = screen.getByRole("button", {
+      name: "Save and continue",
+    });
+    await userEvent.click(saveButton);
+
+    await waitFor(() => expect(saveButton).not.toBeInTheDocument());
+    expect(screen.getByText(/Test page/i)).toBeInTheDocument();
+    expect(mockSubmit).toBeCalledTimes(1);
   });
 });
