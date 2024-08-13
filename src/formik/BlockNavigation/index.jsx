7e7b20fef24e5d4e3a4eaffa72ef9b817139b359
Deepak Jose <deepakjosp@gmail.com>
2024-08-13 12:45:15 +0530

    moved block navigation component to a folder and added handlers to discard changes, save changes and close the alert.

--- src/formik/BlockNavigation/index.jsx
+++ src/formik/BlockNavigation/index.jsx
@@ -3,14 +3,12 @@ import React from "react";
 
 import { useFormikContext } from "formik";
 import PropTypes from "prop-types";
-import { useTranslation } from "react-i18next";
 
-import Alert from "components/Alert";
 import { useNavPrompt } from "hooks";
-import { getLocale } from "utils";
+
+import Alert from "./Alert";
 
 const BlockNavigation = ({ isDirty = false, ...otherProps }) => {
-  const { t, i18n } = useTranslation();
   const formikContext = useFormikContext();
   const shouldBlock =
     isDirty || (Boolean(formikContext) && Boolean(formikContext.dirty));
@@ -19,23 +17,27 @@ const BlockNavigation = ({ isDirty = false, ...otherProps }) => {
     shouldBlock,
   });
 
-  const continueAction = () => {
+  const handleDiscardChanges = () => {
     if (formikContext) formikContext.resetForm();
+    hidePrompt();
     continueNavigation();
+  };
+
+  const handleSaveChanges = () => {
+    if (formikContext.isValid) {
+      formikContext.submitForm();
+      continueNavigation();
+    } else formikContext.setTouched(formikContext.errors);
+
+    hidePrompt();
   };
 
   return (
     <Alert
       isOpen={isBlocked}
-      message={getLocale(i18n, t, "neetoui.blockNavigation.alertMessage")}
-      title={getLocale(i18n, t, "neetoui.blockNavigation.alertTitle")}
-      submitButtonLabel={getLocale(
-        i18n,
-        t,
-        "neetoui.blockNavigation.submitButtonLabel"
-      )}
       onClose={hidePrompt}
-      onSubmit={continueAction}
+      onDiscardChanges={handleDiscardChanges}
+      onSaveChanges={handleSaveChanges}
       {...otherProps}
     />
   );
