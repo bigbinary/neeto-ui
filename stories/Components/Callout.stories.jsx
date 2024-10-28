6f49fe175d77f3bb98cd06880f5a25ac0362b515
Yash Jaiswal <jais.yash01@gmail.com>
2024-10-28 20:02:59 +0530

    Fixed Callout docs

--- stories/Components/Callout.stories.jsx
+++ stories/Components/Callout.stories.jsx
@@ -4,8 +4,6 @@ import { Warning, CloseCircle, CheckCircle, Info } from "neetoicons";
 
 import Callout from "components/Callout";
 
-import { icons } from "../constants";
-
 import CalloutCSSCustomization from "!raw-loader!./CalloutStoriesDocs/CalloutCSSCustomization.mdx";
 import CalloutDocs from "!raw-loader!./CalloutStoriesDocs/CalloutDocs.mdx";
 
@@ -16,7 +14,39 @@ const metadata = {
     layout: "padded",
     docs: { description: { component: CalloutDocs } },
   },
-  argTypes: { icon: { options: Object.keys(icons), mapping: icons } },
+  argTypes: {
+    icon: {
+      description:
+        "To specify the icon to be used in Callout component. By default, icons are based on the style of the Callout component. Passing false will hide the icon.",
+      control: "object",
+      table: { type: { summary: "elementType" } },
+    },
+    style: {
+      description: "To specify the style of Callout component.",
+      control: "radio",
+      options: Object.values({
+        info: "info",
+        warning: "warning",
+        danger: "danger",
+        success: "success",
+      }),
+      table: {
+        type: { summary: "string" },
+        defaultValue: { summary: "info" },
+      },
+    },
+    className: {
+      description: "To provide external classnames to Callout component.",
+      control: "text",
+      table: { type: { summary: "string" } },
+    },
+    children: {
+      description:
+        "To specify the content to be rendered inside the Callout component.",
+      control: "object",
+      table: { type: { summary: "node" } },
+    },
+  },
 };
 
 const Default = args => <Callout {...args}>This is a Callout!</Callout>;
