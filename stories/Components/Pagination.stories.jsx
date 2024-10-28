5c4e4ba471545047073e2ab50bdd90fff3a7dd05
Yash Jaiswal <jais.yash01@gmail.com>
2024-10-28 20:14:44 +0530

    Fixed Pagination docs

--- stories/Components/Pagination.stories.jsx
+++ stories/Components/Pagination.stories.jsx
@@ -18,6 +18,48 @@ const metadata = {
       url: "https://www.figma.com/file/zhdsnPzXzr264x1WUeVdmA/02-Components?node-id=1070%3A3918",
     },
   },
+  argTypes: {
+    count: {
+      description: "To specify the total number of items.",
+      control: "number",
+      table: {
+        type: { summary: "number" },
+        defaultValue: { summary: "0" },
+      },
+      required: true,
+    },
+    pageNo: {
+      description: "To specify the current page number.",
+      control: "number",
+      table: { type: { summary: "number" } },
+    },
+    navigate: {
+      description:
+        "To specify the callback which will be invoked when the navigate buttons are clicked. If not provided, the component will update pagination information in the URL query parameters.",
+      control: "function",
+      table: { type: { summary: "func" } },
+    },
+    pageSize: {
+      description: "To specify the size of a single page.",
+      control: "number",
+      table: { type: { summary: "number" } },
+      required: true,
+    },
+    siblingCount: {
+      description:
+        "To specify the number of siblings to be shown before and after the current page number.",
+      control: "number",
+      table: {
+        type: { summary: "number" },
+        defaultValue: { summary: "1" },
+      },
+    },
+    className: {
+      description: "To provide external className to the Pagination component.",
+      control: "text",
+      table: { type: { summary: "string" } },
+    },
+  },
 };
 
 const Template = args => (
