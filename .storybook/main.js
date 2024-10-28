2ed376298d332ea36fd8b1576f1d7f27ee81babe
Yash Jaiswal <jais.yash01@gmail.com>
2024-10-28 09:55:32 +0530

    Removed remark-gfm package

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,5 @@
 import MonacoWebpackPlugin from "monaco-editor-webpack-plugin";
 import path from "path";
-import remarkGfm from "remark-gfm";
 import { mergeDeepLeft } from "ramda";
 
 import commonResolve from "@bigbinary/neeto-commons-frontend/configs/nanos/webpack/resolve.js";
@@ -11,28 +10,14 @@ const rootResolve = mergeDeepLeft(projectResolve, commonResolve);
 const config = {
   staticDirs: ["./public"],
 
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
 
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-scss",
-    "@storybook/addon-postcss",
     "@storybook/addon-console",
     "storybook-dark-mode",
-    {
-      name: "@storybook/addon-docs",
-      options: {
-        mdxPluginOptions: {
-          mdxCompileOptions: {
-            remarkPlugins: [remarkGfm],
-          },
-        },
-      },
-    },
   ],
 
   webpackFinal: async config => {
