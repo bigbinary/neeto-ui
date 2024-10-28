b534f751954d12cd3518c79bdac3652f07ad6333
Yash Jaiswal <jais.yash01@gmail.com>
2024-10-28 20:24:41 +0530

    Fixed ProgressBar docs

--- stories/Components/ProgressBar.stories.jsx
+++ stories/Components/ProgressBar.stories.jsx
@@ -12,6 +12,26 @@ const metadata = {
     layout: "padded",
     docs: { description: { component: ProgressBarDocs } },
   },
+  argTypes: {
+    progressPercentage: {
+      description:
+        "To specify the value to be used as the width of the progress bar.",
+      control: "number",
+      table: { type: { summary: "number" } },
+    },
+    progressValue: {
+      description:
+        "To specify the progress value to be displayed on the progress bar as text.",
+      control: "text",
+      table: { type: { summary: "string" } },
+    },
+    className: {
+      description:
+        "To specify external classnames as overrides to the ProgressBar component.",
+      control: "text",
+      table: { type: { summary: "string" } },
+    },
+  },
 };
 
 const Default = args => <ProgressBar {...args} />;
