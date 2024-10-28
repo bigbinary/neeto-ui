2cfd6d716f560fbceac4d2b7c22455828e0517c1
Yash Jaiswal <jais.yash01@gmail.com>
2024-10-28 09:57:28 +0530

    Changes to render Introductory files in new storybook version

---
+++ stories/Introduction/Changelog.stories.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Changelog = () => <div>Changelog</div>;
+
+const metadata = {
+  title: "Changelog",
+  component: Changelog,
+  tags: ["!autodocs"],
+};
+
+export default metadata;
+
+export { Changelog };
