98e498934117f8449da59feccf9d6bc953faaa5e
Yash Jaiswal <jais.yash01@gmail.com>
2024-10-29 18:49:55 +0530

    Deleted Checkbox mdx's files folder and added description in stories file

--- stories/Components/Checkbox.stories.jsx
+++ stories/Components/Checkbox.stories.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 import Checkbox from "components/Checkbox";
 
-import CheckboxCSSCustomization from "!raw-loader!./CheckboxStoriesDocs/CheckboxCSSCustomization.mdx";
-import CheckboxDocs from "!raw-loader!./CheckboxStoriesDocs/CheckboxDocs.mdx";
+const description = `
+\`import { Checkbox } from "@bigbinary/neetoui";\`
+
+\`Checkbox\` allows users to select multiple items from a list or mark a single
+item as selected with a simple click.
+`;
 
 const metadata = {
   title: "Components/Checkbox",
   component: Checkbox,
   parameters: {
     layout: "padded",
-    docs: { description: { component: CheckboxDocs } },
+    docs: { description: { component: description } },
     design: {
       type: "figma",
       url: "https://www.figma.com/file/zhdsnPzXzr264x1WUeVdmA/02-Components?node-id=104%3A4",
@@ -88,6 +92,57 @@ Error.args = {
   label: "Checkbox with error",
   error: "Error message",
 };
+
+const CheckboxCSSCustomization = `
+Starting from v6, neeto-ui supports enhanced customization of components using
+CSS variables. These are the variables that are being used in the \`Checkbox\`
+component.
+
+\`\`\`css
+--neeto-ui-checkbox-size: 16px;
+--neeto-ui-checkbox-color: rgb(var(--neeto-ui-primary-500));
+--neeto-ui-checkbox-border-width: 2px;
+--neeto-ui-checkbox-border-color: rgb(var(--neeto-ui-gray-400));
+--neeto-ui-checkbox-border-radius: var(--neeto-ui-rounded-sm);
+
+// Disabled
+--neeto-ui-checkbox-disabled-opacity: 0.5;
+
+// Hover
+--neeto-ui-checkbox-hover-border-color: rgb(var(--neeto-ui-gray-500));
+
+// Focus
+--neeto-ui-checkbox-focus-outline: transparent;
+--neeto-ui-checkbox-focus-outline-offset: 0px;
+
+// Focus Visible
+--neeto-ui-checkbox-focus-visible-outline: 3px solid rgba(var(--neeto-ui-primary-500), 50%);
+--neeto-ui-checkbox-focus-visible-outline-offset: 1px;
+--neeto-ui-checkbox-focus-visible-box-shadow: none;
+
+// Checked
+--neeto-ui-checkbox-checked-border-color: rgb(var(--neeto-ui-primary-500));
+
+// Margin
+--neeto-ui-checkbox-label-margin: 8px;
+
+// Label
+--neeto-ui-checkbox-label-line-height: 1.2;
+\`\`\`
+
+You can use these variables to customize the component to your liking. Here is
+an example:
+
+\`\`\`css
+.neetix-checkbox {
+  --neeto-ui-checkbox-color: rgb(var(--neeto-ui-gray-800));
+  --neeto-ui-checkbox-focus-visible-outline: 3px solid rgba(var(--neeto-ui-gray-800), 50%);
+  --neeto-ui-checkbox-checked-border-color: rgb(var(--neeto-ui-gray-800));
+}
+\`\`\`
+
+#### Output
+`;
 
 const CSSCustomization = args => <Checkbox {...args} />;
 
