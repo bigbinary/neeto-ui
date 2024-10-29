f42abfa4321677e710a14d91492b59f3e5b08cc0
Yash Jaiswal <jais.yash01@gmail.com>
2024-10-29 18:34:18 +0530

    Deleted Accordian mdx's files folder

--- stories/Components/Accordion.stories.jsx
+++ stories/Components/Accordion.stories.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 import Accordion from "components/Accordion";
 
-import AccordionCSSCustomization from "!raw-loader!./AccordionStoriesDocs/AccordionCSSCustomization.mdx";
-import AccordionDocs from "!raw-loader!./AccordionStoriesDocs/AccordionDocs.mdx";
+const description = `
+\`import { Accordion } from "@bigbinary/neetoui";\`
+
+\`Accordion\` allows users to expand or collapse sections of content organized in
+a vertical stack to reveal or hide additional information.
+
+\`Accordion\` component has one subcomponent:
+
+- \`Accordion.Item\`: Represents an individual section within an accordion that can be expanded or collapsed.
+`;
 
 const metadata = {
   title: "Components/Accordion",
@@ -11,7 +19,7 @@ const metadata = {
   subcomponents: { "Accordion.Item": Accordion.Item },
   parameters: {
     layout: "padded",
-    docs: { description: { component: AccordionDocs } },
+    docs: { description: { component: description } },
     design: {
       type: "figma",
       url: "https://www.figma.com/file/zhdsnPzXzr264x1WUeVdmA/02-Components?node-id=104%3A15",
@@ -157,6 +165,68 @@ const AccordionWithCustomizedIcon = args => (
 
 AccordionWithCustomizedIcon.storyName = "Accordion with customized icon";
 AccordionWithCustomizedIcon.parameters = { layout: "default" };
+
+const AccordionCSSCustomization = `
+Starting from v6, neeto-ui supports enhanced customization of components using
+CSS variables. These are the variables that are being used in the \`Accordion\`
+component.
+
+\`\`\`css
+// Outer wrapper
+--neeto-ui-accordion-outer-wrapper-padding-x: 0px;
+--neeto-ui-accordion-outer-wrapper-padding-y: 0px;
+--neeto-ui-accordion-outer-wrapper-border-radius: 0px;
+--neeto-ui-accordion-outer-wrapper-bg-color: transparent;
+
+// Wrapper
+--neeto-ui-accordion-wrapper-border-width: 1px;
+--neeto-ui-accordion-wrapper-border-color: rgb(var(--neeto-ui-gray-300));
+--neeto-ui-accordion-wrapper-border-radius: 0px;
+
+// Item
+--neeto-ui-accordion-item-padding-x: 8px;
+--neeto-ui-accordion-item-padding-y: 16px;
+--neeto-ui-accordion-item-font-size: var(--neeto-ui-text-base);
+--neeto-ui-accordion-item-font-weight: var(--neeto-ui-font-medium);
+--neeto-ui-accordion-item-line-height: 16px;
+--neeto-ui-accordion-item-color: rgb(var(--neeto-ui-gray-800));
+--neeto-ui-accordion-item-bg-color: transparent;
+
+// Drop
+--neeto-ui-accordion-drop-padding-x: 8px;
+--neeto-ui-accordion-drop-padding-top: 4px;
+--neeto-ui-accordion-drop-padding-bottom: 16px;
+--neeto-ui-accordion-drop-color: rgb(var(--neeto-ui-gray-800));
+--neeto-ui-accordion-drop-margin-bottom: 0px;
+
+// Item - Open
+--neeto-ui-accordion-item-open-color: rgb(var(--neeto-ui-black));
+
+// Item - Hover
+--neeto-ui-accordion-item-hover-color: rgb(var(--neeto-ui-black));
+
+// Item - Focus Visible
+--neeto-ui-accordion-item-focus-visible-color: rgb(var(--neeto-ui-black));
+--neeto-ui-accordion-item-focus-visible-outline: 3px solid rgba(var(--neeto-ui-primary-500), 50%);
+--neeto-ui-accordion-item-focus-visible-outline-offset: 1px;
+--neeto-ui-accordion-item-focus-visible-box-shadow: none;
+\`\`\`
+
+You can use these variables to customize the component to your liking. Here is
+an example:
+
+\`\`\`css
+.neetix-accordion {
+  --neeto-ui-accordion-item-padding-x: 0px;
+  --neeto-ui-accordion-drop-padding-x: 0px;
+  --neeto-ui-accordion-item-color: rgb(var(--neeto-ui-primary-500));
+  --neeto-ui-accordion-item-hover-color: rgb(var(--neeto-ui-primary-600));
+  --neeto-ui-accordion-item-open-color: rgb(var(--neeto-ui-primary-800));
+}
+\`\`\`
+
+#### Output
+`;
 
 const CSSCustomization = args => (
   <Accordion {...args}>
