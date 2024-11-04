1e1a6738905ac015b77f70c7a7ee7e4826fd7f83
Yash Jaiswal <jais.yash01@gmail.com>
2024-11-04 10:06:13 +0530

    Deleted KBD mdx's files folder and added description in stories file

--- stories/Components/Kbd.stories.jsx
+++ stories/Components/Kbd.stories.jsx
@@ -2,15 +2,20 @@ import React from "react";
 
 import Kbd from "components/Kbd";
 
-import KbdCSSCustomization from "!raw-loader!./KbdStoriesDocs/KbdCSSCustomization.mdx";
-import KbdDocs from "!raw-loader!./KbdStoriesDocs/KbdDocs.mdx";
+const description = `
+\`import { Kbd } from "@bigbinary/neetoui";\`
+
+\`Kbd\` represents user keyboard input within a document. It's typically used to
+display keyboard shortcuts, key presses, or other keyboard-related information
+in a web page's content.
+`;
 
 const metadata = {
   title: "Components/Kbd",
   component: Kbd,
   parameters: {
     layout: "padded",
-    docs: { description: { component: KbdDocs } },
+    docs: { description: { component: description } },
   },
   argTypes: {
     keyName: {
@@ -56,6 +61,39 @@ const CSSCustomization = args => <Kbd {...args} />;
 CSSCustomization.storyName = "Kbd CSS Customization";
 
 CSSCustomization.args = { keyName: "⌘", className: "neetix-kbd" };
+
+const KbdCSSCustomization = `
+Starting from v6, neeto-ui supports enhanced customization of components using
+CSS variables. These are the variables that are being used in the \`Kbd\`
+component.
+
+\`\`\`css
+--neeto-ui-kbd-font-size: var(--neeto-ui-text-sm);
+--neeto-ui-kbd-font-weight: var(--neeto-ui-font-semibold);
+--neeto-ui-kbd-color: rgb(var(--neeto-ui-gray-700));
+--neeto-ui-kbd-bg-color: rgb(var(--neeto-ui-gray-200));
+--neeto-ui-kbd-border-radius: var(--neeto-ui-rounded);
+--neeto-ui-kbd-min-width: 24px;
+--neeto-ui-kbd-height: 24px;
+--neeto-ui-kbd-padding: 4px;
+\`\`\`
+
+You can use these variables to customize the component to your liking. Here is
+an example:
+
+\`\`\`css
+.neetix-kbd {
+  --neeto-ui-kbd-font-size: var(--neeto-ui-text-base);
+  --neeto-ui-kbd-color: rgb(var(--neeto-ui-primary-800));
+  --neeto-ui-kbd-bg-color: rgb(var(--neeto-ui-primary-100));
+  --neeto-ui-kbd-border-radius: var(--neeto-ui-rounded-sm);
+  --neeto-ui-kbd-min-width: 32px;
+  --neeto-ui-kbd-height: 32px;
+}
+\`\`\`
+
+#### Output
+`;
 
 CSSCustomization.parameters = {
   docs: { description: { story: KbdCSSCustomization } },
