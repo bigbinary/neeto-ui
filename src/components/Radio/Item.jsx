b1425f6294a9a54f723fc9bd84a02478cb6ccc63
Sojan Varghese <sojanvarghese@Sojans-MacBook-Pro.local>
2024-11-16 00:53:52 +0530

    Added dataCy prop to radio  item component

--- src/components/Radio/Item.jsx
+++ src/components/Radio/Item.jsx
@@ -12,6 +12,7 @@ const Item = ({
   label = "",
   className = "",
   labelProps,
+  dataCy,
   ...otherProps
 }) => {
   const id = useId(otherProps.id);
@@ -21,13 +22,13 @@ const Item = ({
       <input
         {...{ id, name }}
         className="neeto-ui-radio"
-        data-cy={`${hyphenize(label)}-radio-input`}
+        data-cy={`${hyphenize(dataCy ?? label)}-radio-input`}
         type="radio"
         {...otherProps}
       />
       {label && (
         <Label
-          data-cy={`${hyphenize(label)}-radio-label`}
+          data-cy={`${hyphenize(dataCy ?? label)}-radio-label`}
           htmlFor={id}
           {...labelProps}
         >
