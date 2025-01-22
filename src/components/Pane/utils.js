174044daa89b4d6afe78f84b47e7b9f0007d1dcb
Joseph Mathew <josephmathew900@gmail.com>
2025-01-22 20:20:19 +0530

    Fixed TypeError - Cannot read properties of null (reading 'style') in Pane component (#2413)

--- src/components/Pane/utils.js
+++ src/components/Pane/utils.js
@@ -4,15 +4,15 @@ export const getHeader = paneWrapperRef =>
   paneWrapperRef.current.querySelector(".neeto-ui-pane__header");
 
 export const updateHeaderHeight = (header, paneWrapperRef) => {
-  const headerHeight = header.offsetHeight;
+  const headerHeight = header?.offsetHeight;
 
   if (headerHeight > DEFAULT_PANE_HEADER_HEIGHT) {
-    paneWrapperRef.current.style.setProperty(
+    paneWrapperRef.current?.style.setProperty(
       "--neeto-ui-pane-header-height",
       `${headerHeight}px`
     );
   } else {
-    paneWrapperRef.current.style.removeProperty(
+    paneWrapperRef.current?.style.removeProperty(
       "--neeto-ui-pane-header-height"
     );
   }
